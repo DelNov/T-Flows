6d8ba9dff5e045ab2a7db3fd1b29c71ba565bd61
Niceno <bojan@niceno.ch>
2018-06-05 13:44:00 +0200

    Should gather statistics for all models now.

--- Sources/Process/Calculate_Mean.f90
+++ Sources/Process/Calculate_Mean.f90
@@ -16,53 +16,71 @@
   type(Grid_Type) :: grid
   integer         :: n0, n1
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: c, n
+  integer :: c, n
 !==============================================================================!
 
-  n=n1-n0
+  if(turbulence_statistics .eq. NO) return
 
-  if(n  > -1) then
+  n = n1 - n0
+
+  if(n > -1) then
+
     do c = -grid % n_bnd_cells, grid % n_cells
 
-      !-----------------!
-      !   Mean values   !
-      !-----------------!
-      U % mean(c) = ( U % mean(c) * (1.*n) + U % n(c) ) / (1.*(n+1))
-      V % mean(c) = ( V % mean(c) * (1.*n) + V % n(c) ) / (1.*(n+1))
-      W % mean(c) = ( W % mean(c) * (1.*n) + W % n(c) ) / (1.*(n+1))
-      P % mean(c) = ( P % mean(c) * (1.*n) + P % n(c) ) / (1.*(n+1))
+      if(turbulence_model .eq. SMAGORINSKY .or.  &
+         turbulence_model .eq. DYNAMIC     .or.  &
+         turbulence_model .eq. WALE        .or.  &
+         turbulence_model .eq. DNS) then
 
-      !------------------------!
-      !   Fluctuating values   !
-      !------------------------!
-      uu % mean(c) = ( uu % mean(c)*(1.*n) + U % n(c) * U % n(c) ) & 
-                   / (1.*(n+1))
-      vv % mean(c) = ( vv % mean(c)*(1.*n) + V % n(c) * V % n(c) ) & 
-                   / (1.*(n+1))
-      ww % mean(c) = ( ww % mean(c)*(1.*n) + W % n(c) * W % n(c) ) & 
-                   / (1.*(n+1))
+        u % mean(c) = (u % mean(c) * (1.*n) + u % n(c)) / (1.*(n+1))
+        v % mean(c) = (v % mean(c) * (1.*n) + v % n(c)) / (1.*(n+1))
+        w % mean(c) = (w % mean(c) * (1.*n) + w % n(c)) / (1.*(n+1))
+        p % mean(c) = (p % mean(c) * (1.*n) + p % n(c)) / (1.*(n+1))
 
-      uv % mean(c) = ( uv % mean(c)*(1.*n) + U % n(c) * V % n(c) ) & 
-                   / (1.*(n+1))
-      uw % mean(c) = ( uw % mean(c)*(1.*n) + U % n(c) * W % n(c) ) & 
-                   / (1.*(n+1))
-      vw % mean(c) = ( vw % mean(c)*(1.*n) + V % n(c) * W % n(c) ) & 
-                   / (1.*(n+1))
+        uu % mean(c) = (uu % mean(c)*(1.*n) + u % n(c) * u % n(c)) / (1.*(n+1))
+        vv % mean(c) = (vv % mean(c)*(1.*n) + v % n(c) * v % n(c)) / (1.*(n+1))
+        ww % mean(c) = (ww % mean(c)*(1.*n) + w % n(c) * w % n(c)) / (1.*(n+1))
 
-      ! VISt_mean(c) = ( VISt_mean(c)*(1.*n) + VISt(c) ) & 
-      !              / (1.*(n+1))
+        uv % mean(c) = (uv % mean(c)*(1.*n) + u % n(c) * v % n(c)) / (1.*(n+1))
+        uw % mean(c) = (uw % mean(c)*(1.*n) + u % n(c) * w % n(c)) / (1.*(n+1))
+        vw % mean(c) = (vw % mean(c)*(1.*n) + v % n(c) * w % n(c)) / (1.*(n+1))
+      end if
+
+      if(turbulence_model .eq. K_EPS) then           
+        kin % mean(c) = (kin % mean(c) * (1.*n) + kin % n(c)) / (1.*(n+1))
+        eps % mean(c) = (eps % mean(c) * (1.*n) + eps % n(c)) / (1.*(n+1))
+      end if
+
+      if(turbulence_model .eq. K_EPS_ZETA_F) then           
+        kin  % mean(c) = (kin  % mean(c) * (1.*n) + kin  % n(c)) / (1.*(n+1))
+        eps  % mean(c) = (eps  % mean(c) * (1.*n) + eps  % n(c)) / (1.*(n+1))
+        zeta % mean(c) = (zeta % mean(c) * (1.*n) + zeta % n(c)) / (1.*(n+1))
+        f22  % mean(c) = (f22  % mean(c) * (1.*n) + f22  % n(c)) / (1.*(n+1))
+      end if
+
+      if(turbulence_model .eq. HANJALIC_JAKIRLIC .or.  &
+         turbulence_model .eq. REYNOLDS_STRESS) then           
+        uu  % mean(c) = (uu  % mean(c) * (1.*n) + uu  % n(c)) / (1.*(n+1))
+        vv  % mean(c) = (vv  % mean(c) * (1.*n) + vv  % n(c)) / (1.*(n+1))
+        ww  % mean(c) = (ww  % mean(c) * (1.*n) + ww  % n(c)) / (1.*(n+1))
+        uv  % mean(c) = (uv  % mean(c) * (1.*n) + uv  % n(c)) / (1.*(n+1))
+        uw  % mean(c) = (uw  % mean(c) * (1.*n) + uw  % n(c)) / (1.*(n+1))
+        vw  % mean(c) = (vw  % mean(c) * (1.*n) + vw  % n(c)) / (1.*(n+1))
+        kin % mean(c) = (kin % mean(c) * (1.*n) + kin % n(c)) / (1.*(n+1))
+        eps % mean(c) = (eps % mean(c) * (1.*n) + eps % n(c)) / (1.*(n+1))
+        if(turbulence_model .eq. REYNOLDS_STRESS) then
+          f22  % mean(c) = (f22  % mean(c) * (1.*n) + f22  % n(c)) / (1.*(n+1))
+        end if
+      end if
 
       if(heat_transfer == YES) then
-        T % mean(c) = ( T % mean(c) * (1.*n) + T % n(c) ) / (1.*(n+1))
-        TT % mean(c) = ( TT % mean(c)*(1.*n) + T % n(c) * T % n(c) ) & 
-                     / (1.*(n+1))
-        uT % mean(c) = ( uT % mean(c)*(1.*n) + u % n(c) * T % n(c) ) & 
-                     / (1.*(n+1))
-        vT % mean(c) = ( vT % mean(c)*(1.*n) + v % n(c) * T % n(c) ) & 
-                     / (1.*(n+1))
-        wT % mean(c) = ( wT % mean(c)*(1.*n) + w % n(c) * T % n(c) ) & 
-                     / (1.*(n+1))
+        t  % mean(c) = (t % mean(c) * (1.*n) + t % n(c) ) / (1.*(n+1))
+        tt % mean(c) = (tt % mean(c)*(1.*n) + t % n(c) * t % n(c) ) / (1.*(n+1))
+        ut % mean(c) = (ut % mean(c)*(1.*n) + u % n(c) * t % n(c) ) / (1.*(n+1))
+        vt % mean(c) = (vt % mean(c)*(1.*n) + v % n(c) * t % n(c) ) / (1.*(n+1))
+        wt % mean(c) = (wt % mean(c)*(1.*n) + w % n(c) * t % n(c) ) / (1.*(n+1))
       end if
+
     end do 
   end if
 
