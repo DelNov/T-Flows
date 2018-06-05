0cf9f76a2679f8bb11387f7c9f6a966e7141dc1f
Niceno <bojan@niceno.ch>
2018-06-05 14:09:17 +0200

    Removed unused variables.

--- Sources/Process/Turbulence/Les_Mod.f90
+++ Sources/Process/Turbulence/Les_Mod.f90
@@ -12,14 +12,14 @@
 
   ! Variables relevant for 'LES' computations
   real              :: c_smag
-  real, allocatable :: c_dyn(:), c_dyn_mean(:)
+  real, allocatable :: c_dyn(:)
 
   ! For LES you need to know nearest wall cell
   integer, allocatable :: nearest_wall_cell(:)
 
-  real, allocatable :: shear_mean(:), kin_sgs(:)
-  real, allocatable :: vis_t_sgs(:), vis_t_mean(:)
+! real, allocatable :: kin_sgs(:)
+! real, allocatable :: vis_t_sgs(:)
 
-  real, allocatable :: shear_r(:), shear_mean_r(:), wale_v(:)
+  real, allocatable :: wale_v(:)
 
 end module  
