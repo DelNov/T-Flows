49d264d7796db637089462c109e7bd9919d4b3c4
Niceno <bojan@niceno.ch>
2018-06-06 13:13:32 +0200

    Issue #22 (Channel flow RANS computation pressure)
    Performs normalization of residuals ONLY if residuals
    and difference in values are bigger than NANO (1.0e-9)

--- Sources/Process/Solvers_Mod/Normalized_Residual.f90
+++ Sources/Process/Solvers_Mod/Normalized_Residual.f90
@@ -26,14 +26,18 @@
   call Comm_Mod_Global_Sum_Real(error)
   error = sqrt(error)
 
-  ! Would it make sense to normalize it with the number of unknowns???
-  
   ! Normalize it with absolute values of the unknown
   x_min = minval(x(1:n))
   x_max = maxval(x(1:n))
   call Comm_Mod_Global_Min_Real(x_min)
   call Comm_Mod_Global_Max_Real(x_max)
-  error = error / (x_max - x_min + TINY)
+
+  ! Create a plateau for very small sources and values
+  if( (x_max-x_min) < NANO .and. error < NANO ) then
+    error = PICO
+  else
+    error = error / (x_max - x_min + TINY)
+  end if
 
   Normalized_Residual = error
 
