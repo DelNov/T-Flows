7594885eccd6f471bf9410e4bc4a82fa53f00551
Niceno <bojan@niceno.ch>
2018-06-08 13:53:54 +0200

    fixed Save_Results.f90 in User_Mod

--- Sources/Process/User_Mod/Save_Results.f90
+++ Sources/Process/User_Mod/Save_Results.f90
@@ -1,1 +1,28 @@
-../../../Tests/Rans/Channel_Re_Tau_590/User_Mod/Save_Results.f90
+!==============================================================================!
+  subroutine User_Mod_Save_Results(grid, name_save)
+!------------------------------------------------------------------------------!
+!   This is a prototype of customized saving functions.  All cases which       !
+!   require special procedures when saving results (1d or 2d profiles, non-    !
+!   standard save formats, and alike) should be done here, case-based.         !
+!------------------------------------------------------------------------------!
+!----------------------------------[Modules]-----------------------------------!
+  use Grid_Mod
+  use Var_Mod
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Grid_Type)  :: grid
+  character(len=*) :: name_save
+!==============================================================================!
+
+  !--------------------------! 
+  !   First level comments   !
+  !--------------------------! 
+
+  !---------------------------! 
+  !   Second level comments   !
+  !---------------------------! 
+
+  ! Third level comments
+
+  end subroutine  ! fourth level comments
