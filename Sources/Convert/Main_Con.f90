46084d184078cbaee3c7816491c57efb26d03d9a
Niceno <bojan@niceno.ch>
2018-08-09 12:33:43 +0200

    No more "shadow" faces - they were used for Fluent (TM) output anyhow.

--- Sources/Convert/Main_Con.f90
+++ Sources/Convert/Main_Con.f90
@@ -99,9 +99,6 @@
                       grid % n_bnd_cells,  &
                       0)
 
-  ! I believe these are needed for Fluent only
-  call Save_Shadows(grid, 0)
-
   ! Create 1D file (used for channel or pipe flow)
   call Probe_1D_Nodes(grid)
 
