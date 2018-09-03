7df6df1d66297fec726da02794581043405c8514
Niceno <bojan@niceno.ch>
2018-09-03 10:14:28 +0200

    Improved version which distinguishes between boundaries with triangular and quadrilaterall cells.

--- Sources/Shared/Cgns_Mod/Initialize_Counters.f90
+++ Sources/Shared/Cgns_Mod/Initialize_Counters.f90
@@ -7,21 +7,15 @@
 !==============================================================================!
 
   cnt_nodes     = 0 ! number of nodes
-  cnt_cells     = 0 ! number of cells (except boundary and interface)
+  cnt_cells     = 0 ! number of cells (except boundary)
   cnt_blocks    = 0 ! number of block
-  cnt_bnd_conds = 0 ! number of boundary
+  cnt_bnd_conds = 0 ! number of boundary cells
 
   cnt_hex = 0 ! number of hexahedral  cells
   cnt_pyr = 0 ! number of pyramid     cells
   cnt_wed = 0 ! number of wedge/prism cells
   cnt_tet = 0 ! number of tetrahedral cells
-
-  cnt_bnd_tri = 0 ! number of triangles defining boundary conditions
-  cnt_bnd_qua = 0 ! number of quads     defining boundary conditions
-
-  cnt_int     = 0   ! number of unique interfaces
-  cnt_int_tri = 0   ! number of triangles defining block interface
-  cnt_int_qua = 0   ! number of quads     defining block interface
-  cnt_int_cells = 0 ! number of cells of interfaces
+  cnt_tri = 0 ! number of triangles
+  cnt_qua = 0 ! number of quads
 
   end subroutine
