d11823892caf43fb0f860c174d146a85dd54ca98
Niceno <bojan@niceno.ch>
2018-09-03 11:27:22 +0200

    Uses METIS now and has therefore simplified a lot.

--- Sources/Divide/Main_Div.f90
+++ Sources/Divide/Main_Div.f90
@@ -5,20 +5,16 @@
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Name_Mod, only: problem_name
-  use gen_mod 
+  use Gen_Mod 
   use Div_Mod
   use Tokenizer_Mod
   use Grid_Mod
 !------------------------------------------------------------------------------!
   implicit none
 !-----------------------------------[Locals]-----------------------------------!
-  type(Grid_Type)  :: grid         ! grid to be divided
-  integer          :: n_sub_tot    ! total number of subdomains
-  integer          :: n_div        ! total number of divisions
-  integer          :: chunks(128)
-  integer          :: i, j, c
-  character(len=8) :: answer
-  real             :: start, finish
+  type(Grid_Type) :: grid           ! grid to be divided
+  integer         :: c, n_sub
+  real            :: start, finish  ! variables to time the program
 !==============================================================================!
 
   call cpu_time(start)
@@ -34,84 +30,21 @@
   call Allocate_Additional(grid)
   call Load_Geo           (grid, 0)
 
-  ! Initialize processor numbers
+  ! Initialize processor numbers (poor idea to put it here)
   do c = 1, grid % n_cells
-    proces(c)=1
+    grid % comm % proces(c) = 1
   end do
-
-  ! Sort the cells
-  print *, '# Sorting the cells'
-  do i = 1, grid % n_cells
-    ix(i) = i
-    criter(i) = grid % xc(i) + 0.01 * grid % yc(i) + 0.0001 * grid % zc(i)
-  end do
-  call Sort_Real_Carry_Int(criter(1),ix(1),grid % n_cells,2)
-  do i = 1, grid % n_cells
-    iy(i) = i
-    criter(i) = grid % yc(i) + 0.01 * grid % zc(i) + 0.0001 * grid % xc(i)
-  end do
-  call Sort_Real_Carry_Int(criter(1),iy(1),grid % n_cells,2)
-  do i = 1, grid % n_cells
-    iz(i) = i
-    criter(i) = grid % zc(i) + 0.01 * grid % xc(i) + 0.0001 * grid % yc(i)
-  end do
-  call Sort_Real_Carry_Int(criter(1),iz(1),grid % n_cells,2)
-  print *, '# Finished sorting'
 
   call Load_Geo(grid, 0)
 
   print *, '# Number of subdomains:'
-  read(*,*)  n_sub_tot
-  n_sub = n_sub_tot      ! Needed for EpsPar
+  read(*,*)  n_sub
 
-  allocate (sub_n_cells(n_sub))
-
-  print *, '#==============================='
-  print *, '# Algorythm for decomposition:'
-  print *, '# COO -> Coordinate multisection' 
-  print *, '# INE -> Inertial multisection' 
-  print *, '#-------------------------------'
-  read(*,*) answer
-  call To_Upper_Case(answer)
-  if(answer .eq. 'COO') then
-    division_algorithm = COORDINATE
-  else if(answer .eq. 'INE') then
-    division_algorithm = INERTIAL
-  else
-    print *, '# Error in input. Exiting!' 
-    stop
-  end if
-
-  n_sub=1
-  sub_n_cells(n_sub) = grid % n_cells
-
-  !----------------------------------!
-  !   Find the number of divisions   !
-  !----------------------------------!
-  call Factorize_Number(n_div, n_sub_tot, chunks)
-  print *, '# Number of divisions:', n_div
-
-  !-------------------------------! 
-  !   Through all the divisions   !
-  !-------------------------------!
-  do i = 1, n_div
-
-    do j = 1, n_sub
-      print *, '# Dividing', j, ' into', chunks(i), ' chunks'
-      call Split_Subdomain(grid, j, chunks(i))
-    end do
-
-  end do     
-
-  do j = 1, n_sub
-    print *, '# Processor:', j, ' cells:', sub_n_cells(j)
-  end do
+  call Grid_Mod_Decompose(grid, n_sub)
 
   call Create_Maps(grid)
   call Create_Buffers_And_Save(grid)
 
-  call Save_Com
-  
   call cpu_time(finish)
   print '(a10,f14.3,a9)', ' # Time = ', finish-start, ' seconds.'
 
