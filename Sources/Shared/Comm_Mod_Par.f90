902e69f38602550b6110845f4a4d8772bfee7a8d
Niceno <bojan@niceno.ch>
2018-09-03 10:48:47 +0200

    A lot of data has been moved from here to module "Grid_Mod" where it naturally belongs.  Communication patterns are grid specific.

--- Sources/Shared/Comm_Mod_Par.f90
+++ Sources/Shared/Comm_Mod_Par.f90
@@ -15,32 +15,14 @@
   integer :: this_proc  ! processor i.d.
   integer :: n_proc     ! number of processors
 
-  ! These names are ugly but mean number of buffer boundaries start and end
-  integer, allocatable :: nbb_s(:), nbb_e(:)
-  integer, allocatable :: buffer_index(:)
-
   ! Variables which follow are for backup saving to single file
-  integer                      :: nc_s   ! number of cells in subdomain
-  integer                      :: nb_s   ! number of bundary cells in subdoima
-  integer                      :: nf_s   ! number of faces in subdomain
-  integer                      :: nbf_s  ! number of buffer faces in subdomain
-  integer                      :: nc_t   ! total number of cells 
-  integer                      :: nb_t   ! total number of bundary cells
-  integer                      :: nf_t   ! total number of faces
-  integer(kind=4), allocatable :: cell_map(:)
-  integer(kind=4), allocatable :: bnd_cell_map(:)
-  integer(kind=4), allocatable :: face_map(:)
-  integer(kind=4), allocatable :: face_ord(:)
-  real, allocatable            :: face_val(:)      ! face values
-  integer(kind=4), allocatable :: buf_face_map(:)  ! buffer face map
-  integer(kind=4), allocatable :: buf_cell_map(:)  ! buffer cell map
-  integer(kind=4), allocatable :: buf_face_ord(:)  ! buffer face order
-  real, allocatable            :: buf_face_val(:)  ! buffer face values
-  real, allocatable            :: buf_face_sgn(:)  ! buffer face sign  
-  integer                      :: cell_map_type
-  integer                      :: bnd_cell_map_type
-  integer                      :: face_map_type
-  integer                      :: buf_face_map_type
+  integer :: nc_s   ! number of cells in subdomain
+  integer :: nb_s   ! number of bundary cells in subdoima
+  integer :: nc_t   ! total number of cells 
+  integer :: nb_t   ! total number of bundary cells
+
+  integer :: cell_map_type
+  integer :: bnd_cell_map_type
 
   contains
 
@@ -48,9 +30,12 @@
   include 'Comm_Mod/Parallel/Close_File.f90'
   include 'Comm_Mod/Parallel/Create_New_Types.f90'
   include 'Comm_Mod/Parallel/End.f90'
-  include 'Comm_Mod/Parallel/Exchange.f90'
+  include 'Comm_Mod/Parallel/Exchange_Int.f90'
+  include 'Comm_Mod/Parallel/Exchange_Real.f90'
   include 'Comm_Mod/Parallel/Global_Max_Real.f90'
   include 'Comm_Mod/Parallel/Global_Min_Real.f90'
+  include 'Comm_Mod/Parallel/Global_Max_Int.f90'
+  include 'Comm_Mod/Parallel/Global_Min_Int.f90'
   include 'Comm_Mod/Parallel/Global_Sum_Int_Array.f90'
   include 'Comm_Mod/Parallel/Global_Sum_Int.f90'
   include 'Comm_Mod/Parallel/Global_Sum_Real.f90'
@@ -61,15 +46,14 @@
   include 'Comm_Mod/Parallel/Read_Int.f90'
   include 'Comm_Mod/Parallel/Read_Bnd_Real.f90'
   include 'Comm_Mod/Parallel/Read_Cell_Real.f90'
-  include 'Comm_Mod/Parallel/Read_Face_Real.f90'
   include 'Comm_Mod/Parallel/Read_Real.f90'
   include 'Comm_Mod/Parallel/Read_Text.f90'
+  include 'Comm_Mod/Parallel/Set_Buffer_Color.f90'
   include 'Comm_Mod/Parallel/Start.f90'
   include 'Comm_Mod/Parallel/Wait.f90'
   include 'Comm_Mod/Parallel/Write_Int.f90'
   include 'Comm_Mod/Parallel/Write_Bnd_Real.f90'
   include 'Comm_Mod/Parallel/Write_Cell_Real.f90'
-  include 'Comm_Mod/Parallel/Write_Face_Real.f90'
   include 'Comm_Mod/Parallel/Write_Real.f90'
   include 'Comm_Mod/Parallel/Write_Text.f90'
 
--- Sources/Shared/Comm_Mod_Seq.f90
+++ Sources/Shared/Comm_Mod_Seq.f90
@@ -15,20 +15,11 @@
   integer :: this_proc  ! processor i.d.
   integer :: n_proc     ! number of processors
 
-  ! These names are ugly but mean number of buffer boundaries start and end
-  integer, allocatable :: nbb_s(:), nbb_e(:)
-  integer, allocatable :: buffer_index(:)
-
   ! Variables which follow are for backup saving to single file
   integer :: nc_s   ! number of cells in subdomain
   integer :: nb_s   ! number of bundary cells in subdoima
-  integer :: nf_s   ! number of faces in subdomain
-  integer :: nbf_s  ! number of buffer faces in subdomain
   integer :: nc_t   ! total number of cells 
   integer :: nb_t   ! total number of bundary cells
-  integer :: nf_t   ! total number of faces
-
-  real, allocatable :: buf_face_sgn(:)  ! buffer face sign  
 
   contains
 
@@ -36,9 +27,12 @@
   include 'Comm_Mod/Sequential/Close_File.f90'
   include 'Comm_Mod/Sequential/Create_New_Types.f90'
   include 'Comm_Mod/Sequential/End.f90'
-  include 'Comm_Mod/Sequential/Exchange.f90'
+  include 'Comm_Mod/Sequential/Exchange_Int.f90'
+  include 'Comm_Mod/Sequential/Exchange_Real.f90'
   include 'Comm_Mod/Sequential/Global_Max_Real.f90'
   include 'Comm_Mod/Sequential/Global_Min_Real.f90'
+  include 'Comm_Mod/Sequential/Global_Max_Int.f90'
+  include 'Comm_Mod/Sequential/Global_Min_Int.f90'
   include 'Comm_Mod/Sequential/Global_Sum_Int_Array.f90'
   include 'Comm_Mod/Sequential/Global_Sum_Int.f90'
   include 'Comm_Mod/Sequential/Global_Sum_Real.f90'
@@ -49,15 +43,14 @@
   include 'Comm_Mod/Sequential/Read_Int.f90'
   include 'Comm_Mod/Sequential/Read_Bnd_Real.f90'
   include 'Comm_Mod/Sequential/Read_Cell_Real.f90'
-  include 'Comm_Mod/Sequential/Read_Face_Real.f90'
   include 'Comm_Mod/Sequential/Read_Real.f90'
   include 'Comm_Mod/Sequential/Read_Text.f90'
+  include 'Comm_Mod/Sequential/Set_Buffer_Color.f90'
   include 'Comm_Mod/Sequential/Start.f90'
   include 'Comm_Mod/Sequential/Wait.f90'
   include 'Comm_Mod/Sequential/Write_Int.f90'
   include 'Comm_Mod/Sequential/Write_Bnd_Real.f90'
   include 'Comm_Mod/Sequential/Write_Cell_Real.f90'
-  include 'Comm_Mod/Sequential/Write_Face_Real.f90'
   include 'Comm_Mod/Sequential/Write_Real.f90'
   include 'Comm_Mod/Sequential/Write_Text.f90'
 
