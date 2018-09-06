ffb1451a524f5a89d6d0f77564896fbb97db5867
Niceno <bojan@niceno.ch>
2018-09-06 22:58:04 +0200

    BUFFERS ARE SAVED AS ADDITIONAL INSIDE CELLS, RATHER THAN ADDITIONAL BOUNDARY CELLS.  THAT SIMPLIFIES THE CODE SIGNIFICANTLY AND ELIMINATES THE NEED FOR ADDITIONAL BOUNDARY CONDITION TYPE "BUFFER".

--- Sources/Process/Calculate_Mass_Flow_Rate.f90
+++ Sources/Process/Calculate_Mass_Flow_Rate.f90
@@ -23,15 +23,14 @@
     fs = grid % f(s)
 
     ! Face is inside the domain
-    if( c2 > 0 .or.  &
-        c2 < 0 .and. Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. BUFFER) then 
- 
+    if( c2 > 0 ) then
+
       ! Extract the "centred" pressure terms from cell velocities
       flux(s) = density *                                                 &
               (   (fs * u % n(c1) + (1.0-fs) * u % n(c2)) * grid % sx(s)  &
                 + (fs * v % n(c1) + (1.0-fs) * v % n(c2)) * grid % sy(s)  &
                 + (fs * w % n(c1) + (1.0-fs) * w % n(c2)) * grid % sz(s) )
- 
+
     ! Face is on the boundary
     else
       flux(s) = density *                   &
