3d0d1d9128867a01fbecf56914385453193135af
Niceno <bojan@niceno.ch>
2018-09-06 22:52:49 +0200

    BUFFERS ARE SAVED AS ADDITIONAL INSIDE CELLS, RATHER THAN ADDITIONAL BOUNDARY CELLS.  THAT SIMPLIFIES THE CODE SIGNIFICANTLY AND ELIMINATES THE NEED FOR ADDITIONAL BOUNDARY CONDITION TYPE "BUFFER".

--- Sources/Process/Bulk_Mod/Compute_Fluxes.f90
+++ Sources/Process/Bulk_Mod/Compute_Fluxes.f90
@@ -16,7 +16,7 @@
   real, dimension(:) :: flux
 !-----------------------------------[Locals]-----------------------------------!
   integer :: c1, c2, s
-  real    :: xc1, yc1, zc1, xc2, yc2, zc2
+  real    :: xc1, yc1, zc1, xc2, yc2, zc2, wgt
 !==============================================================================!
 
   bulk % flux_x = 0.0
@@ -26,59 +26,43 @@
   do s = 1, grid % n_faces
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
+
     if(c2 > 0) then
-      xc1 = grid % xc(c1) 
-      yc1 = grid % yc(c1) 
-      zc1 = grid % zc(c1) 
-      xc2 = grid % xc(c1) + grid % dx(s) 
-      yc2 = grid % yc(c1) + grid % dy(s) 
+
+      xc1 = grid % xc(c1)
+      yc1 = grid % yc(c1)
+      zc1 = grid % zc(c1)
+      xc2 = grid % xc(c1) + grid % dx(s)
+      yc2 = grid % yc(c1) + grid % dy(s)
       zc2 = grid % zc(c1) + grid % dz(s)
 
-      if((xc1 <= bulk % xp).and.(xc2 > bulk % xp))  &
-        bulk % flux_x = bulk % flux_x + flux(s)
+      ! If the flux is across a buffer face, it is summed up twice.  
+      ! The variable "wgt" is here to take care of that.
+      wgt = 1.0
+      if(c2 > grid % n_cells - grid % comm % n_buff_cells) wgt = 0.5
 
-      if((yc1 <= bulk % yp).and.(yc2 > bulk % yp))  &
-        bulk % flux_y = bulk % flux_y + flux(s)
-
-      if((zc1 <= bulk % zp).and.(zc2 > bulk % zp))  &
-        bulk % flux_z = bulk % flux_z + flux(s)
-
-      if((xc2 < bulk % xp).and.(xc1 >= bulk % xp))  &
-        bulk % flux_x = bulk % flux_x - flux(s)
-
-      if((yc2 < bulk % yp).and.(yc1 >= bulk % yp))  &
-        bulk % flux_y = bulk % flux_y - flux(s)
-
-      if((zc2 < bulk % zp).and.(zc1 >= bulk % zp))  &
-        bulk % flux_z = bulk % flux_z - flux(s)
-
-    else if(c2 < 0 .and. Grid_Mod_Bnd_Cond_Type(grid, c2) .eq. BUFFER) then
-      xc1 = grid % xc(c1) 
-      yc1 = grid % yc(c1) 
-      zc1 = grid % zc(c1) 
-      xc2 = grid % xc(c1) + grid % dx(s) 
-      yc2 = grid % yc(c1) + grid % dy(s) 
-      zc2 = grid % zc(c1) + grid % dz(s)
-
-      if((xc1 <= bulk % xp).and.(xc2 > bulk % xp))  &
-        bulk % flux_x = bulk % flux_x + .5*flux(s)
-
-      if((yc1 <= bulk % yp).and.(yc2 > bulk % yp))  &
-        bulk % flux_y = bulk % flux_y + .5*flux(s)
-
-      if((zc1 <= bulk % zp).and.(zc2 > bulk % zp))  &
-        bulk % flux_z = bulk % flux_z + .5*flux(s)
-
-      if((xc2 < bulk % xp).and.(xc1 >= bulk % xp))  &
-        bulk % flux_x = bulk % flux_x - .5*flux(s)
-
-      if((yc2 < bulk % yp).and.(yc1 >= bulk % yp))  &
-        bulk % flux_y = bulk % flux_y - .5*flux(s)
-
-      if((zc2 < bulk % zp).and.(zc1 >= bulk % zp))  &
-        bulk % flux_z = bulk % flux_z - .5*flux(s)
-
-    end if   ! c2 > 0
+      !-------!
+      !   X   !
+      !-------!
+      if((xc1 <= bulk % xp).and.(xc2 > bulk % xp)) bulk % flux_x =             &
+                                                   bulk % flux_x + wgt * flux(s)
+      if((xc2 < bulk % xp).and.(xc1 >= bulk % xp)) bulk % flux_x =             &
+                                                   bulk % flux_x - wgt * flux(s)
+      !-------!
+      !   Y   !
+      !-------!
+      if((yc1 <= bulk % yp).and.(yc2 > bulk % yp)) bulk % flux_y =             &
+                                                   bulk % flux_y + wgt * flux(s)
+      if((yc2 < bulk % yp).and.(yc1 >= bulk % yp)) bulk % flux_y =             &
+                                                   bulk % flux_y - wgt * flux(s)
+      !-------!
+      !   Z   !
+      !-------!
+      if((zc1 <= bulk % zp).and.(zc2 > bulk % zp)) bulk % flux_z =             &
+                                                   bulk % flux_z + wgt * flux(s)
+      if((zc2 < bulk % zp).and.(zc1 >= bulk % zp)) bulk % flux_z =             &
+                                                   bulk % flux_z - wgt * flux(s)
+    end if
   end do
 
   call Comm_Mod_Global_Sum_Real(bulk % flux_x)
--- Sources/Process/Bulk_Mod/Monitoring_Planes_Areas.f90
+++ Sources/Process/Bulk_Mod/Monitoring_Planes_Areas.f90
@@ -13,7 +13,7 @@
   type(Bulk_Type) :: bulk
 !-----------------------------------[Locals]-----------------------------------!
   integer :: c1, c2, s, m
-  real    :: xc1, yc1, zc1, xc2, yc2, zc2, ax_t, ay_t, az_t
+  real    :: xc1, yc1, zc1, xc2, yc2, zc2, ax_t, ay_t, az_t, wgt
 !==============================================================================!
 
   bulk % area_x = 0.0
@@ -23,69 +23,52 @@
   do s = 1, grid % n_faces
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
-    if(c2  < 0) then
-      if (Grid_Mod_Bnd_Cond_Type(grid, c2) .ne. BUFFER) then
-        cycle ! skip current surface
+
+    if(c2 > 0) then
+
+      xc1 = grid % xc(c1)
+      yc1 = grid % yc(c1)
+      zc1 = grid % zc(c1)
+      xc2 = grid % xc(c1) + grid % dx(s)
+      yc2 = grid % yc(c1) + grid % dy(s)
+      zc2 = grid % zc(c1) + grid % dz(s)
+
+      ax_t = abs(grid % sx(s))
+      ay_t = abs(grid % sy(s))
+      az_t = abs(grid % sz(s))
+
+      ! If the flux is across a buffer face, it is summed up twice.  
+      ! The variable "wgt" is here to take care of that.
+      wgt = 1.0
+      if(c2 > grid % n_cells - grid % comm % n_buff_cells) wgt = 0.5
+
+      !-------!
+      !   X   !
+      !-------!
+      if( (xc1 <= bulk % xp).and.(xc2 > bulk % xp) .or.    &
+          (xc2 <= bulk % xp).and.(xc1 > bulk % xp) ) then
+
+        bulk % area_x = bulk % area_x + wgt * ax_t
       end if
-    end if
 
-    xc1 = grid % xc(c1)
-    yc1 = grid % yc(c1)
-    zc1 = grid % zc(c1)
-    xc2 = grid % xc(c1) + grid % dx(s)
-    yc2 = grid % yc(c1) + grid % dy(s)
-    zc2 = grid % zc(c1) + grid % dz(s)
+      !-------!
+      !   Y   !
+      !-------!
+      if( (yc1 <= bulk % yp).and.(yc2 > bulk % yp) .or. & 
+          (yc2 <= bulk % yp).and.(yc1 > bulk % yp) ) then
 
-    ax_t = abs(grid % sx(s))
-    ay_t = abs(grid % sy(s))
-    az_t = abs(grid % sz(s))
-
-    !-------!
-    !   X   !
-    !-------!
-    if( (xc1 <= bulk % xp).and.(xc2 > bulk % xp) .or.    &
-        (xc2 <= bulk % xp).and.(xc1 > bulk % xp) ) then
-
-      ! Watch out: buffer cell faces will be counted twice
-      if(c2  < 0) then
-        if (Grid_Mod_Bnd_Cond_Type(grid, c2) .eq. BUFFER) then
-            bulk % area_x = bulk % area_x + 0.5 * ax_t
-        end if
-      else
-        bulk % area_x = bulk % area_x + ax_t    
+        bulk % area_y = bulk % area_y + wgt * ay_t
       end if
-    end if
 
-    !-------!
-    !   Y   !
-    !-------!
-    if( (yc1 <= bulk % yp).and.(yc2 > bulk % yp) .or. & 
-        (yc2 <= bulk % yp).and.(yc1 > bulk % yp) ) then
+      !-------!
+      !   Z   !
+      !-------!
+      if( (zc1 <= bulk % zp).and.(zc2 > bulk % zp) .or. & 
+          (zc2 <= bulk % zp).and.(zc1 > bulk % zp) ) then
 
-      ! Watch out: buffer cell faces will be counted twice
-      if(c2  < 0) then
-        if (Grid_Mod_Bnd_Cond_Type(grid, c2) .eq. BUFFER) then
-          bulk % area_y = bulk % area_y + 0.5 * ay_t
-        end if
-      else
-        bulk % area_y = bulk % area_y + ay_t    
+        bulk % area_z = bulk % area_z + wgt * az_t
       end if
-    end if
 
-    !-------!
-    !   Z   !
-    !-------!
-    if( (zc1 <= bulk % zp).and.(zc2 > bulk % zp) .or. & 
-        (zc2 <= bulk % zp).and.(zc1 > bulk % zp) ) then
-
-      ! Watch out: buffer cell faces will be counted twice
-      if(c2  < 0) then
-        if (Grid_Mod_Bnd_Cond_Type(grid, c2) .eq. BUFFER) then
-          bulk % area_z = bulk % area_z + 0.5 * az_t
-        end if
-      else
-        bulk % area_z = bulk % area_z + az_t    
-      end if
     end if
 
   end do
