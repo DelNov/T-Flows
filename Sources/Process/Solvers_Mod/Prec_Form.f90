f95c12568ed90026a711346e388e17fbef160f79
Niceno <bojan@niceno.ch>
2018-09-07 11:57:47 +0200

    Formatting changes; made it more readable.

--- Sources/Process/Solvers_Mod/Prec_Form.f90
+++ Sources/Process/Solvers_Mod/Prec_Form.f90
@@ -15,25 +15,25 @@
   real     :: sum1
   integer  :: i, j, k, n
 !==============================================================================!
-                 
+
   n = a % pnt_grid % n_cells - a % pnt_grid % comm % n_buff_cells
 
   !---------------------------------! 
   !   1) diagonal preconditioning   !
   !---------------------------------!
-  if(prec .eq. 'DIAGONAL') then        
-    do i=1,n                     
-      d % val(d % dia(i)) = a % val(a % dia(i))           
-    end do                      
+  if(prec .eq. 'DIAGONAL') then
+    do i = 1, n
+      d % val(d % dia(i)) = a % val(a % dia(i))
+    end do
 
   !--------------------------------------------! 
   !   2) incomplete cholesky preconditioning   !
   !--------------------------------------------!
-  else if(prec .eq. 'INCOMPLETE_CHOLESKY') then   
+  else if(prec .eq. 'INCOMPLETE_CHOLESKY') then
     do i = 1,n
       sum1 = a % val(a % dia(i))       ! take diaginal entry   
       do j = a % row(i), a % dia(i)-1  ! only lower traingular
-        k = a % col(j)                    
+        k = a % col(j)
         sum1 = sum1 - d % val(d % dia(k)) * a % val(j) * a % val(j)  
       end do
       d % val(d % dia(i)) = 1.0 / sum1
@@ -42,10 +42,10 @@
   !---------------------------!
   !   .) no preconditioning   !
   !---------------------------!
-  else                          
-    do i=1,n
+  else
+    do i = 1, n
       d % val(d % dia(i)) = 1.0
     end do
-  end if 
+  end if
 
   end subroutine
