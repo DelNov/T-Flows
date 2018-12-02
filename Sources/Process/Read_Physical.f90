c11b1162321b084eb36dbb89a717d0cf656e980f
Niceno <bojan.niceno.scientist@gmail.com>
2018-12-02 17:23:39 +0100

    "Field_Type" is now an entity defined in "Main_Pro" This is an important step towards the implementation of two materials.

--- Sources/Process/Read_Physical.f90
+++ Sources/Process/Read_Physical.f90
@@ -1,17 +1,19 @@
 !==============================================================================!
-  subroutine Read_Physical(grid, restar)
+  subroutine Read_Physical(flow, bulk, restar)
 !------------------------------------------------------------------------------!
 !   Reads details about physial models.                                        !
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
-  use Field_Mod
+  use Field_Mod,   only: Field_Type
+  use Bulk_Mod,    only: Bulk_Type
   use Rans_Mod
   use Control_Mod
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type) :: grid
-  logical         :: restar
+  type(Field_Type) :: flow
+  type(Bulk_Type)  :: bulk
+  logical          :: restar
 !==============================================================================!
 
   !-------------------------!
@@ -19,7 +21,7 @@
   !-------------------------!
   call Control_Mod_Gravitational_Vector(.true.)
   call Control_Mod_Buoyancy(.true.)
-  call Control_Mod_Reference_Temperature(.true.)
+  call Control_Mod_Reference_Temperature(flow, .true.)
 
   !---------------------------!
   !   Related to turbulence   !
@@ -58,10 +60,8 @@
   !   Pressure drops and mass fluxes   !
   !------------------------------------!
   if(.not. restar) then
-    call Control_Mod_Pressure_Drops()
-  end if
-  if(.not. restar) then
-    call Control_Mod_Mass_Flow_Rates()
+    call Control_Mod_Pressure_Drops(bulk)
+    call Control_Mod_Mass_Flow_Rates(bulk)
   end if
 
   end subroutine
