74dbfe2748fec5e07e986f57ab68de42b6e9129f
Niceno <bojan.niceno.scientist@gmail.com>
2018-12-02 17:37:18 +0100

    "Field_Type" is now an entity defined in "Main_Pro" This is an important step towards the implementation of two materials.

--- Sources/Process/Turbulence/Calculate_Heat_Flux.f90
+++ Sources/Process/Turbulence/Calculate_Heat_Flux.f90
@@ -1,32 +1,34 @@
 !==============================================================================!
-  subroutine Calculate_Heat_Flux(grid)
+  subroutine Calculate_Heat_Flux(flow)
 !------------------------------------------------------------------------------!
 !   Computes turbulent heat fluxes                                             !
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Const_Mod
   use Control_Mod
-  use Grid_Mod
+  use Grid_Mod,  only: Grid_Type
   use Grad_Mod
-  use Field_Mod
+  use Field_Mod, only: Field_Type
   use Rans_Mod
-  use Work_Mod, only: t_x => r_cell_01,  &
-                      t_y => r_cell_02,  &
-                      t_z => r_cell_03
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type) :: grid
+  type(Field_Type), target :: flow
 !---------------------------------[Calling]------------------------------------!
   real :: Turbulent_Prandtl_Number
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: c 
-  real    :: beta
+  type(Grid_Type), pointer :: grid
+  type(Var_Type),  pointer :: t
+  integer                  :: c
 !==============================================================================!
 
-  call Grad_Mod_For_Phi(grid, t % n, 1, t_x, .true.)
-  call Grad_Mod_For_Phi(grid, t % n, 2, t_y, .true.)
-  call Grad_Mod_For_Phi(grid, t % n, 3, t_z, .true.)
+  ! Take aliases
+  grid => flow % pnt_grid
+  t    => flow % t
+
+  call Grad_Mod_For_Phi(grid, t % n, 1, t % x, .true.)
+  call Grad_Mod_For_Phi(grid, t % n, 2, t % y, .true.)
+  call Grad_Mod_For_Phi(grid, t % n, 3, t % z, .true.)
 
   !-----------------------------------------!
   !   Compute the sources in the interior   !
@@ -37,24 +39,24 @@
 
     do c = 1, grid % n_cells
       pr_t = max(Turbulent_Prandtl_Number(grid, c), TINY)
-      ut % n(c) = -vis_t(c) / pr_t * t_x(c)
-      vt % n(c) = -vis_t(c) / pr_t * t_y(c)
-      wt % n(c) = -vis_t(c) / pr_t * t_z(c)
+      ut % n(c) = -vis_t(c) / pr_t * t % x(c)
+      vt % n(c) = -vis_t(c) / pr_t * t % y(c)
+      wt % n(c) = -vis_t(c) / pr_t * t % z(c)
     end do
 
   else if(turbulent_heat_flux_model .eq. GGDH) then
    
     do c = 1, grid % n_cells
 
-      ut % n(c) =  -0.22*t_scale(c) * (uu % n(c) * t_x(c) +  &
-                                       uv % n(c) * t_y(c) +  &
-                                       uw % n(c) * t_z(c))
-      vt % n(c) =  -0.22*t_scale(c) * (uv % n(c) * t_x(c) +  &
-                                       vv % n(c) * t_y(c) +  &
-                                       vw % n(c) * t_z(c))
-      wt % n(c) =  -0.22*t_scale(c) * (uw % n(c) * t_x(c) +  &
-                                       vw % n(c) * t_y(c) +  &
-                                       ww % n(c) * t_z(c))
+      ut % n(c) =  -0.22*t_scale(c) * (uu % n(c) * t % x(c) +  &
+                                       uv % n(c) * t % y(c) +  &
+                                       uw % n(c) * t % z(c))
+      vt % n(c) =  -0.22*t_scale(c) * (uv % n(c) * t % x(c) +  &
+                                       vv % n(c) * t % y(c) +  &
+                                       vw % n(c) * t % z(c))
+      wt % n(c) =  -0.22*t_scale(c) * (uw % n(c) * t % x(c) +  &
+                                       vw % n(c) * t % y(c) +  &
+                                       ww % n(c) * t % z(c))
 
     end do
 
