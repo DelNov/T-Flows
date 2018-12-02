d0dae742ace6bd2d97ce106aeac7b000dfebcf3e
Niceno <bojan.niceno.scientist@gmail.com>
2018-12-02 16:18:26 +0100

    "Field_Type" is now an entity defined in "Main_Pro" This is an important step towards the implementation of two materials.

--- Sources/Process/Initialize_Variables.f90
+++ Sources/Process/Initialize_Variables.f90
@@ -1,11 +1,11 @@
 !==============================================================================!
-  subroutine Initialize_Variables(grid)
+  subroutine Initialize_Variables(flow, bulk)
 !------------------------------------------------------------------------------!
-!   Initialize dependent variables.                                            !
+!   Initialize dependent variables.  (It is a bit of a mess still)             !
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Const_Mod
-  use Field_Mod
+  use Field_Mod, only: Field_Type, heat_transfer, density
   use Les_Mod
   use Comm_Mod
   use Rans_Mod
@@ -16,30 +16,42 @@
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type) :: grid
+  type(Field_Type), target :: flow
+  type(Bulk_Type)          :: bulk
 !----------------------------------[Calling]-----------------------------------!
   integer :: Key_Ind
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: i, c, c1, c2, m, s, nks, nvs
-  integer           :: n_wall, n_inflow, n_outflow, n_symmetry, n_heated_wall, &
-                       n_convect
-  character(len=80) :: keys(128)
-  character(len=80) :: keys_file(128)
-  real              :: vals(0:128)   ! note that they start from zero!
-  real              :: area
+  type(Grid_Type), pointer :: grid
+  type(Var_Type),  pointer :: u, v, w, t
+  real,            pointer :: flux(:)
+  integer                  :: i, c, c1, c2, m, s, nks, nvs
+  integer                  :: n_wall, n_inflow, n_outflow, n_symmetry,  &
+                              n_heated_wall, n_convect
+  character(len=80)        :: keys(128)
+  character(len=80)        :: keys_file(128)
+  real                     :: vals(0:128)   ! note that they start from zero!
+  real                     :: area
 
-  integer           :: n_points, k
-  real, allocatable :: prof(:,:), x(:), y(:), z(:), dist(:)
-  logical           :: found
+  integer                  :: n_points, k
+  real, allocatable        :: prof(:,:), x(:), y(:), z(:), dist(:)
+  logical                  :: found
 
   ! Default values for initial conditions
-  real, parameter   :: u_def   = 0.0,  v_def   = 0.0,  w_def    = 0.0
-  real, parameter   :: t_def   = 0.0
-  real, parameter   :: kin_def = 0.0,  eps_def = 0.0,  f22_def  = 0.0
-  real, parameter   :: vis_def = 0.0,  zeta_def = 0.0
-  real, parameter   :: uu_def  = 0.0,  vv_def  = 0.0,  ww_def   = 0.0
-  real, parameter   :: uv_def  = 0.0,  uw_def  = 0.0,  vw_def   = 0.0
+  real, parameter          :: u_def   = 0.0,  v_def    = 0.0,  w_def    = 0.0
+  real, parameter          :: t_def   = 0.0
+  real, parameter          :: kin_def = 0.0,  eps_def  = 0.0,  f22_def  = 0.0
+  real, parameter          :: vis_def = 0.0,  zeta_def = 0.0
+  real, parameter          :: uu_def  = 0.0,  vv_def   = 0.0,  ww_def   = 0.0
+  real, parameter          :: uv_def  = 0.0,  uw_def   = 0.0,  vw_def   = 0.0
 !==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
+  flux => flow % flux
+  u    => flow % u
+  v    => flow % v
+  w    => flow % w
+  t    => flow % t
 
   area  = 0.0
   if (this_proc < 2) print *, '# Grid material: ', grid % material % name
