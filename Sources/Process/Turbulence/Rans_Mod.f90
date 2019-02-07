d922a12b67f1a86d472ba3ceb3c802c1f78800ec
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-07 14:56:43 +0100

    Variables got "target" attribute (so that they can be accessed by aliases)

--- Sources/Process/Turbulence/Rans_Mod.f90
+++ Sources/Process/Turbulence/Rans_Mod.f90
@@ -11,19 +11,19 @@
 !==============================================================================!
 
   ! Turbulence models variables
-  type(Var_Type) :: kin
-  type(Var_Type) :: eps
-  type(Var_Type) :: zeta
-  type(Var_Type) :: f22
-  type(Var_Type) :: vis
-  type(Var_Type) :: t2
+  type(Var_Type), target :: kin
+  type(Var_Type), target :: eps
+  type(Var_Type), target :: zeta
+  type(Var_Type), target :: f22
+  type(Var_Type), target :: vis
+  type(Var_Type), target :: t2
 
   ! Constants for the k-eps model:
   real :: c_1e, c_2e, c_3e, c_mu, c_mu25, c_mu75, kappa, e_log, z_o
- 
+
   ! Constants for the k-eps-v2f model:
   real :: c_mu_d, c_l, c_t, alpha, c_nu, c_f1, c_f2
-  real :: g1, g1_star, g2, g3, g3_star, g4, g5, c_theta 
+  real :: g1, g1_star, g2, g3, g3_star, g4, g5, c_theta
 
   ! Constants for the Spalart-Allmaras model:
   real :: c_b1, c_b2, c_w1, c_w2, c_w3, c_v1
@@ -31,10 +31,10 @@
   ! Effective turbulent viscosity
   real, allocatable :: vis_t_eff(:)
   real, allocatable :: vis_t_sgs(:)
- 
+
   ! Lenght and Time Scales
   real,allocatable :: l_scale(:)
-  real,allocatable :: t_scale(:)   
+  real,allocatable :: t_scale(:)
 
   ! Production of turbulent kinetic energy
   real,allocatable :: p_kin(:), p_t2(:)
