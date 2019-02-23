4c53ff6a8edc814c9981baf7fb85526bbfbdc625
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-23 14:18:45 +0100

    Added to repository (parameters for particle tracking)

---
+++ Sources/Process/Control_Mod/Physics/Number_Of_Particles.f90
@@ -0,0 +1,13 @@
+!==============================================================================!
+  subroutine Control_Mod_Number_Of_Particles(val, verbose)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  integer           :: val
+  logical, optional :: verbose
+!==============================================================================!
+
+  call Control_Mod_Read_Int_Item('NUMBER_OF_PARTICLES', 16, &
+                                  val, verbose)
+
+  end subroutine
