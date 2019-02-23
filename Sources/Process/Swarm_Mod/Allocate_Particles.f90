eb0269a81aa665fe98685332c52bfa86e91cc3c4
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-23 14:19:32 +0100

    Updated! (Number of particles is read from control file)

--- Sources/Process/Swarm_Mod/Allocate_Particles.f90
+++ Sources/Process/Swarm_Mod/Allocate_Particles.f90
@@ -1,24 +1,20 @@
 !==============================================================================!
-  subroutine Swarm_Mod_Allocate(flow, swarm, n_part)
+  subroutine Swarm_Mod_Allocate(flow, swarm)
 !------------------------------------------------------------------------------!
 !   Allocates memory to store the charge of each particle                      !
+!   It assumes that the number of particles was read from the control file     !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   type(Field_Type), target :: flow
   type(Swarm_Type), target :: swarm
-  integer                  :: n_part
-!-----------------------------------[Locals]-----------------------------------!
 !==============================================================================!
 
   ! Take aliases to object particle flow around
   swarm % pnt_flow => flow
   swarm % pnt_grid => flow % pnt_grid
 
-  ! Store number of particles (to be assigned by user later!)
-  swarm % n_particles = n_part
-
   ! Allocate memory for all of them
-  allocate(swarm % particles(n_part))
+  allocate(swarm % particles(swarm % n_particles))
 
   end subroutine
