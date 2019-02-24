04f9a473f09457d97bcf6a4f7f228e04b1c3bb41
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-24 11:24:06 +0100

    Refactoring! (Removed all non-essential variables from Particle_Type)

--- Sources/Process/Swarm_Mod.f90
+++ Sources/Process/Swarm_Mod.f90
@@ -30,34 +30,16 @@
     ! Particle's density
     real :: density
 
-    ! Particle surface area
-    real :: area
-
-    ! Particle Volume
-    real :: vol
-
     ! Particle's diameter
     real :: d
 
-    ! Particle time step
-    real :: dt
-
-    ! Particle relaxation time
-    real :: tau
-
-    ! The closest cell and node
+    ! The closest cell, node and boundary cell
     integer :: cell
     integer :: node
-
-    ! Particle relative (magnitude) velocity
-    real :: rel_vn  ! +ve value of relative velocity
+    integer :: bnd_cell
 
     ! Particle relative velocity in y-dir (buoyancy force)
     real :: rel_vv
-    real :: rel_vvn  ! +ve value of y-dir relative velocity
-
-    ! Particle velocity magnitude
-    real :: vel_p
 
     ! Particle Reynolds number (computed from relative velocity)
     real :: re
@@ -65,32 +47,17 @@
     ! Particle drag factor (from Re_p)
     real :: f    ! this is not to be confused with the drag coefficient
 
-    ! Forces exerted on particle
-    real :: fd                           ! drag force
-    real :: fb                           ! buoyant force
-    real :: ft                           ! total force
-
-    ! Particle terminal velocity
-    real :: vt
+    ! Forces exerted on the particle
+    real :: fd  ! drag force
+    real :: fb  ! buoyant force
+    real :: ft  ! total force
 
     ! Particle deposition and departure from domain 
     logical  :: deposited
     logical  :: escaped
 
-    ! trapped BC type
-    logical :: trapped
-
     ! Reflection BC type
     logical :: reflected
-
-    ! Counter for testing
-    integer :: counter
-
-    ! Particle cfl number
-    real :: cfl
-
-    type(Grid_Type),  pointer :: pnt_grid  ! grid for which it is defined
-    type(Field_Type), pointer :: pnt_flow  ! flow field for which it is defined
 
   end type
 
@@ -111,6 +78,9 @@
     ! (Mean) diameter for this swarm
     real :: diameter
 
+    ! Time step for the swarm
+    real :: dt
+
     ! Counter for depositing (d), escaped (e) and reflected (r) particles
     integer :: cnt_d
     integer :: cnt_e
@@ -120,10 +90,10 @@
 
   contains
 
-  include 'Swarm_Mod/Allocate_Particles.f90'
+  include 'Swarm_Mod/Advance_Particles.f90'
+  include 'Swarm_Mod/Create.f90'
   include 'Swarm_Mod/Find_Nearest_Cell.f90'
   include 'Swarm_Mod/Find_Nearest_Node.f90'
-  include 'Swarm_Mod/Find_Neighboring_Cells.f90'
   include 'Swarm_Mod/Interpolate_Velocity.f90'
   include 'Swarm_Mod/Particle_Forces.f90'
 
