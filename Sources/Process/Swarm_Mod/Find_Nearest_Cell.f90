e987c9f85aad718fd47aad52005d1a47c5fa75da
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-24 13:52:05 +0100

    Refactoring! (Removed redundancy and code duplication.)

--- Sources/Process/Swarm_Mod/Find_Nearest_Cell.f90
+++ Sources/Process/Swarm_Mod/Find_Nearest_Cell.f90
@@ -15,92 +15,38 @@
   real                         :: xc, yc, zc  ! cell center coordinates
   real                         :: d_sq        ! distance squared
   real                         :: min_d       ! minimum distance computed
-  logical,             pointer :: reflected   ! reflection BC type
 !==============================================================================!
 
   ! Take aliases
-  grid      => swarm % pnt_grid
-  part      => swarm % particle(k)
-  reflected => part  % reflected
+  grid => swarm % pnt_grid
+  part => swarm % particle(k)
 
-  !-------------------------------------------------!
-  !                                                 !
-  !   Browse through all cells as another example   !
-  !                                                 !
-  !-------------------------------------------------!
+  ! Initialize minimum distance
   min_d = HUGE
 
-  !-------------!
-  !   Trap BC   !
-  !-------------!
-  if (.not. reflected) then
-    if(.not. part % escaped .and. .not. part % deposited) then 
+  !-------------------------------------------------!
+  !   Browse through all cells as another example   !
+  !-------------------------------------------------!
+  do c = 1, grid % n_cells
 
-      do c = 1, grid % n_cells
+    ! Take cell centre
+    xc = grid % xc(c)
+    yc = grid % yc(c)
+    zc = grid % zc(c)
 
-        ! Take cell centre
-        xc = grid % xc(c)
-        yc = grid % yc(c)
-        zc = grid % zc(c)
+    ! Distance squared from the particle to cell centre
+    d_sq = (xc - part % x)**2  &
+         + (yc - part % y)**2  &
+         + (zc - part % z)**2
 
-      ! Distance squared from the particle to cell centre
-        d_sq = (xc - part % x)**2  &
-             + (yc - part % y)**2  &
-             + (zc - part % z)**2
-
-        if(d_sq < min_d) then 
-          min_d = d_sq  ! new minimum distance
-          cc = c        ! cc is the closest cell index
-        end if
-
-      end do
-
-      ! save the last value of cc (the index of closest cell) 
-      part % cell = cc
-
-      ! Calling the nearest node subroutine to find the ...
-      ! ... nearest node for each particle and stores it
-      call Swarm_Mod_Find_Nearest_Node(swarm, k)
-
-    else
-      reflected = .true.  ! end because the particle either escaped or deposited
+    if(d_sq < min_d) then
+      min_d = d_sq  ! new minimum distance
+      cc = c        ! cc is the closest cell index
     end if
-  end if
 
-  !------------------!
-  !   Reflected BC   !
-  !------------------!
-  if (reflected) then
-    if(.not. part % escaped) then
+  end do
 
-      do c = 1, grid % n_cells
-
-        ! Take cell centre
-        xc = grid % xc(c)
-        yc = grid % yc(c)
-        zc = grid % zc(c)
-
-        ! Distance squared from the particle to cell centre
-        d_sq = (xc - part % x)**2  &
-             + (yc - part % y)**2  &
-             + (zc - part % z)**2
-
-        if(d_sq < min_d) then
-          min_d = d_sq  ! new minimum distance
-          cc = c        ! cc is the closest cell index
-        end if
-      end do
-
-      ! saves the last value of cc (the index of closest cell) 
-      part % cell = cc
-
-      ! Calling the nearest node subroutine to find the ...
-      ! ... nearest node for each particle and stores it
-      call Swarm_Mod_Find_Nearest_Node(swarm, k)
-
-    else
-      reflected = .false.
-    end if
-  end if
+  ! Save the last value of cc (the index of closest cell) 
+  part % cell = cc
 
   end subroutine
