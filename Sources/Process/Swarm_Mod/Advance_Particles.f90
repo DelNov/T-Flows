8f0ea41b6bb5f53941226b527fe8568fe2c4081f
Niceno <bojan.niceno.scientist@gmail.com>
2019-02-24 13:55:19 +0100

    Refactoring! (Removed redundancy and code duplication.)

--- Sources/Process/Swarm_Mod/Advance_Particles.f90
+++ Sources/Process/Swarm_Mod/Advance_Particles.f90
@@ -21,7 +21,6 @@
   real                         :: min_dc       ! minimum distance computed
   logical,             pointer :: escaped
   logical,             pointer :: deposited
-  logical,             pointer :: reflected    ! reflection BC type
 !==============================================================================!
 
   ! Take aliases for the swarm
@@ -38,117 +37,48 @@
     part      => swarm % particle(k)
     escaped   => part  % escaped
     deposited => part  % deposited
-    reflected => part  % reflected
 
-    !-------------------------------------------------!
-    !   Browse through all cells as another example   !
-    !-------------------------------------------------!
+    ! Set minimum distance from cells
     min_dc = HUGE
 
-    !----------------!
-    !   Trapped BC   !
-    !----------------!
-    if (.not. reflected) then
-      if(.not. escaped .and. .not. deposited) then 
+    !------------------------------------------------!
+    !   Now we're going to use that node index ...   !
+    !   ... to search for the 8 neighboring cells    !
+    !------------------------------------------------!
+    do lc = 1, grid % nodes_n_cells(part % node)   ! local cell number
+      c = grid % nodes_c(lc, part % node)          ! global cell number
 
-        !------------------------------------------------!
-        !   Now we're going to use that node index ...   !
-        !   ... to search for the 8 neighboring cells    !
-        !------------------------------------------------!
-        do n = 1, grid % n_nodes
-          do lc = 1, grid % nodes_n_cells(part % node)   ! local cell number
-            c = grid % nodes_c(lc, part % node)          ! global cell number
+      xc = grid % xc(c)
+      yc = grid % yc(c)
+      zc = grid % zc(c)
 
-            xc = grid % xc(c)
-            yc = grid % yc(c)
-            zc = grid % zc(c)
+      ! Distance squared from the particle to cell centre
+      dc_sq = (xc - part % x)**2  &
+            + (yc - part % y)**2  &
+            + (zc - part % z)**2
 
-            ! Distance squared from the particle to cell centre
-            dc_sq = (xc - part % x)**2  &
-                  + (yc - part % y)**2  &
-                  + (zc - part % z)**2
-
-            ! Finding the closest cell of those 8 neighbors
-            if(dc_sq < min_dc) then
-              min_dc = dc_sq  ! new minimum distance
-              cc = c        ! cc is the closest cell index
-            end if
-
-          end do
-        end do
-
-        ! Closest cell of the 8 neighboring to the node
-        part % cell = cc
-
-        ! Calling the nearest node subroutine to find the ...
-        ! ... nearest node for each particle and stores it
-        call Swarm_Mod_Find_Nearest_Node(swarm, k)
-
-        ! Calling interpolate velocity subroutine ...
-        ! ... to update velocity of each particle
-        call Swarm_Mod_Interpolate_Velocity(swarm, k)
-
-        ! Calling particle forces subroutine to ...
-        ! ... compute the forces on each particle and store it
-        call Swarm_Mod_Particle_Forces(swarm, k)
-
-      else
-        reflected = .true. ! End because the particle either escaped or deposited
-      end if
-    end if
-
-    !-------------------!
-    !   Reflection BC   !
-    !-------------------!
-    if (reflected) then
-      if(.not. escaped) then 
-
-        !------------------------------------------------!
-        !   Now we're going to use that node index ...   !
-        !   ... to search for the 8 neighboring cells    !
-        !------------------------------------------------!
-        do n = 1, grid % n_nodes
-          do lc = 1, grid % nodes_n_cells(part % node)    ! local cell number
-             c = grid % nodes_c(lc, part % node)          ! global cell number
-
-             xc = grid % xc(c)
-             yc = grid % yc(c)
-             zc = grid % zc(c)
-
-            ! Distance squared from the particle to cell centre
-            dc_sq = (xc - part % x)**2  &
-                  + (yc - part % y)**2  &
-                  + (zc - part % z)**2
-
-            ! Finding the closest cell of those 8 neighbors
-             if(dc_sq < min_dc) then
-               min_dc = dc_sq  ! new minimum distance
-               cc = c        ! cc is the closest cell index
-             end if
-
-          end do
-        end do
-
-        ! Closest cell of the 8 neighboring to the node
-        part % cell = cc
-
-        ! Calling the nearest node subroutine to find the ...
-        ! ... nearest node for each particle and stores it
-        call Swarm_Mod_Find_Nearest_Node(swarm, k)
-
-        ! Calling interpolate velocity subroutine ...
-        ! ... to update velocity of each particle
-        call Swarm_Mod_Interpolate_Velocity(swarm, k)
-
-        ! Calling particle forces subroutine to ...
-        ! ... compute the forces on each particle and store it
-        call Swarm_Mod_Particle_Forces(swarm, k)
-
-      else
-        reflected = .false.
+      ! Finding the closest cell of those 8 neighbors
+      if(dc_sq < min_dc) then
+        min_dc = dc_sq  ! new minimum distance
+        cc = c          ! cc is the closest cell index
       end if
 
-    end if
+    end do
+
+    ! Closest cell of the 8 neighboring to the node
+    part % cell = cc
+
+    ! Calling the nearest node subroutine to find the ...
+    ! ... nearest node for each particle and stores it
+    call Swarm_Mod_Find_Nearest_Node(swarm, k)
+
+    ! Calling interpolate velocity subroutine ...
+    ! ... to update velocity of each particle
+    call Swarm_Mod_Interpolate_Velocity(swarm, k)
+
+    ! Calling particle forces subroutine to ...
+    ! ... compute the forces on each particle and store it
+    call Swarm_Mod_Particle_Forces(swarm, k)
 
   end do
 
