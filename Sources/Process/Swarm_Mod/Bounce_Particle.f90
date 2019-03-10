cfc3db4c1ea06598aa5b81823c088ea0cabc3628
Niceno <bojan.niceno.scientist@gmail.com>
2019-03-10 06:15:54 +0100

    Calculates impact coordinates.

--- Sources/Process/Swarm_Mod/Bounce_Particle.f90
+++ Sources/Process/Swarm_Mod/Bounce_Particle.f90
@@ -15,7 +15,7 @@
   integer                      :: c, c2, s             ! nearest cells, face
   real                         :: rx_nx_o, ry_ny_o, rz_nz_o,  &
                                   rx_nx_n, ry_ny_n, rz_nz_n,  &
-                                  nx,     ny,     nz
+                                  xi, yi, zi, dx, dy, dz, nx, ny, nz, f
 !==============================================================================!
 
   ! Take aliases
@@ -63,6 +63,55 @@
           + ry_ny_o * ry_ny_n  &
           + rz_nz_o * rz_nz_n ) <= 0.0 ) then
 
+      !--------------------------------------!
+      !   Find where particle hit the wall   !
+      !   (call this point i, like impact)   !
+      !--------------------------------------!
+
+      !---------------------------------------------!
+      !   Equation for line which connects          !
+      !   old and new particle position reads:      !
+      !      ->        ->   ->                      !
+      !   L: po + f * (pn - po)                     !
+      !                                             !
+      !   or:                                       !
+      !   x(f) = xo + f * dx;  where dx = xn - xo   !
+      !   y(f) = yo + f * dy;  where dy = yn - yo   !
+      !   z(f) = zo + f * dz;  where dz = zn - zo   !
+      !---------------------------------------------!
+      dx = part % x_n - part % x_o
+      dy = part % y_n - part % y_o
+      dz = part % z_n - part % z_o
+
+      !----------------------------------------------------!
+      !   Vector connecting crossing and bondary           !
+      !   center are orthogonal to face normal:            !
+      !    ->   ->    ->                                   !
+      !   (xi - xb) * n  = 0                               !
+      !                                                    !
+      !   Furhtermore, xi is on the line                   !
+      !   connecting po and pn, hence:                     !
+      !   ->    ->        ->                               !
+      !   xi = po_x + f * dx                               !
+      !                                                    !
+      !   So the condition above reads                     !
+      !     (po_x +  f * dx - xb) * nx                     !
+      !   + (po_y +  f * dy - yb) * ny                     !
+      !   + (po_z +  f * dz - zb) * nz = 0                 !
+      !                                                    !
+      !   And f is, eventually:                            !
+      !                                                    !
+      !       (xb-po_x)*nx + (yb-po_y)*ny + (zb-po_z)*nz   !
+      !   f = ------------------------------------------   !
+      !               dx*nx + dy*ny + dz*nz                !
+      !                                                    !
+      !----------------------------------------------------!
+      f = (rx_nx_o + ry_ny_o + rz_nz_o) / (dx*nx + dy*ny + dz*nz)
+
+      xi = part % x_o + f*dx
+      yi = part % y_o + f*dy
+      zi = part % z_o + f*dz
+
       !---------------------------------!
       !   The boundary cell is a wall   !
       !---------------------------------!
@@ -72,7 +121,7 @@
         if(swarm % rst <= TINY) then
           deposited = .true.
           swarm % cnt_d = swarm % cnt_d + 1
-          print *, k, 'Particle is deposited!'
+          print *, k, 'Particle is deposited at: ', xi, yi, zi
         end if  ! trap condition
 
         ! Reflection condition   !
@@ -93,7 +142,7 @@
           swarm % cnt_r = swarm % cnt_r + 1   ! to be engineered because ...
                                               ! ... a single particle can ...
                                               ! ... bounce several times.
-          print *, k, 'Particle is reflected!'
+          print *, k, 'Particle is reflected from: ', xi, yi, zi
         end if  ! reflection condition
 
       end if  ! is it a wall
@@ -104,7 +153,7 @@
       if(Grid_Mod_Bnd_Cond_Type(grid, c2) == OUTFLOW) then
         escaped =  .true.
         swarm % cnt_e = swarm % cnt_e + 1
-        print *,k,'Particle escaped from outlet!'
+        print *, k, 'Particle escaped from outlet at: ', xi, yi, zi
       end if  ! it is an outflow
 
     end if  ! really crossed a boundary cell
