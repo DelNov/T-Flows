8fbf812931c411fb90c03a644d783829e8668511
Niceno <bojan.niceno.scientist@gmail.com>
2019-03-19 23:52:16 +0100

    Corrected an error here.

--- Sources/Process/User_Mod/End_Of_Time_Step.f90
+++ Sources/Process/User_Mod/End_Of_Time_Step.f90
@@ -1,1 +1,14 @@
-../../../Tests/Laminar/T_Junction/User_Mod/End_Of_Time_Step.f90
+!==============================================================================!
+  subroutine User_Mod_End_Of_Time_Step(flow, swarm, n, time)
+!------------------------------------------------------------------------------!
+!   This function is called at the end of time step.                           !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type), target :: flow
+  type(Swarm_Type), target :: swarm
+  integer                  :: n     ! time step
+  real                     :: time  ! physical time
+!==============================================================================!
+
+  end subroutine
