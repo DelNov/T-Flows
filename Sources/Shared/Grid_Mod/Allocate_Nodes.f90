287d9b57be632ef5bd36748920d82838c72e7015
Niceno <bojan.niceno.scientist@gmail.com>
2019-03-30 20:30:41 +0100

    Removed trailing spaces.

--- Sources/Shared/Grid_Mod/Allocate_Nodes.f90
+++ Sources/Shared/Grid_Mod/Allocate_Nodes.f90
@@ -11,8 +11,8 @@
   grid % n_nodes = n
 
   ! Allocate memory for node coordinates
-  allocate(grid % xn(1:n));  grid % xn = 0.0 
-  allocate(grid % yn(1:n));  grid % yn = 0.0 
-  allocate(grid % zn(1:n));  grid % zn = 0.0 
+  allocate(grid % xn(1:n));  grid % xn = 0.0
+  allocate(grid % yn(1:n));  grid % yn = 0.0
+  allocate(grid % zn(1:n));  grid % zn = 0.0
 
   end subroutine
