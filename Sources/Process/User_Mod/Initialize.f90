f1d75b7e0c2d85e6c0453ca9a060bee653df4c18
MassimoBorrelliPhysicist <massimo.borrelli.physicist@gmail.com>
2019-04-26 10:06:41 +0200

    Updated!

--- Sources/Process/User_Mod/Initialize.f90
+++ Sources/Process/User_Mod/Initialize.f90
@@ -1,12 +1,14 @@
 !==============================================================================!
-  subroutine User_Mod_Initialize(grid)
+  subroutine User_Mod_Initialize(flow, turb, swarm)
 !------------------------------------------------------------------------------!
 !   This is embrio of a function which will be used to initialize variables    !
 !   by the user.                                                               !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type)    :: grid
+  type(Field_Type) :: flow
+  type(Turb_Type)  :: turb
+  type(Swarm_Type) :: swarm
 !------------------------------------------------------------------------------!
 
   end subroutine
