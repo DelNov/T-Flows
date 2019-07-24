a7c61c869653fc43fc6ff9ca99e036cadbfd2c11
Niceno <bojan.niceno.scientist@gmail.com>
2019-07-24 16:10:19 +0200

    Added to repository.

---
+++ Sources/Process/Control_Mod/Physics/Number_Of_Swarm_Sub_Steps.f90
@@ -0,0 +1,13 @@
+!==============================================================================!
+  subroutine Control_Mod_Number_Of_Swarm_Sub_Steps(val, verbose)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  integer           :: val
+  logical, optional :: verbose
+!==============================================================================!
+
+  call Control_Mod_Read_Int_Item('NUMBER_OF_SWARM_SUB_STEPS', 60, &
+                                  val, verbose)
+
+  end subroutine
