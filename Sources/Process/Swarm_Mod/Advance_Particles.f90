f1a0c3231ed85b50f409437660400f8cf6a49e68
Niceno <bojan.niceno.scientist@gmail.com>
2019-10-08 13:06:58 +0200

    IMPORTANT: fixed the parallel version.

--- Sources/Process/Swarm_Mod/Advance_Particles.f90
+++ Sources/Process/Swarm_Mod/Advance_Particles.f90
@@ -15,7 +15,7 @@
   logical,             pointer :: escaped
   logical,             pointer :: deposited
   integer                      :: ss         ! sub-step counter
-  integer                      :: part_node  ! storage for particle node
+  integer                      :: n_parts_in_buffers
 !==============================================================================!
 
   ! Take aliases for the swarm
@@ -31,6 +31,9 @@
   !                                  !
   !----------------------------------!
   do ss = 1, swarm % n_sub_steps
+
+    n_parts_in_buffers = 0
+
     do k = 1, swarm % n_particles
 
       ! Take aliases for the particle
@@ -46,14 +49,6 @@
         ! If particle is in this processor, carry on with it
         if(part % proc .eq. this_proc) then
 
-          ! Calling the nearest cell subroutine to find the ...
-          ! ... nearest cell for each particle and stores it
-          call Swarm_Mod_Find_Nearest_Cell(swarm, k)
-
-          ! Calling the nearest node subroutine to find the ...
-          ! ... nearest node for each particle and stores it
-          call Swarm_Mod_Find_Nearest_Node(swarm, k)
-
           ! Compute velocity at the particle, and move it
           ! (also calls Bounce_Particle)
           call Swarm_Mod_Move_Particle(swarm, turb, k)
@@ -62,51 +57,47 @@
           ! ... compute the forces on each particle and store it
           call Swarm_Mod_Particle_Forces(swarm, k)
 
-        end if  ! in this processor
+          ! Calling the nearest cell subroutine to find the ...
+          ! ... nearest cell for each particle and stores it
+          call Swarm_Mod_Find_Nearest_Cell(swarm, k, n_parts_in_buffers)
 
-      end if    ! not deposited or escaped
+          ! Calling the nearest node subroutine to find the ...
+          ! ... nearest node for each particle and stores it
+          call Swarm_Mod_Find_Nearest_Node(swarm, k)
+
+          ! First check if it didn't escape through periodicity
+          call Swarm_Mod_Check_Periodicity(swarm, k, n_parts_in_buffers)
+
+        end if  ! in this processor
+      end if    ! deposited or escaped
     end do      ! through particles
 
-    ! Make sure particles didn't run out of periodicity
-    ! (If they did, their node will be set to zero meaning it is lost)
-    call Swarm_Mod_Check_Periodicity(swarm)
-
-    ! If any of the particles escaped through periodicity, locate it again
-    do k = 1, swarm % n_particles
-      part_node = part % node
-      call Comm_Mod_Global_Min_Int(part_node)
-      if(part_node .eq. 0) then
-        call Swarm_Mod_Find_Nearest_Cell(swarm, k)
-        call Swarm_Mod_Find_Nearest_Node(swarm, k)
-      end if
-    end do
+    ! Exchange particles for parallel version; if needed
+    call Comm_Mod_Global_Sum_Int(n_parts_in_buffers)
+    if(n_parts_in_buffers > 0) then
+if(this_proc < 2) PRINT *, 'IS EXCHANGING PARTICLES'
+      call Swarm_Mod_Exchange_Particles(swarm)
+    end if
 
   end do        ! through sub-steps
-
-  !---------------------------------------------!
-  !                                             !
-  !   Exchange particles for parallel version   !
-  !                                             !
-  !---------------------------------------------!
-  call Swarm_Mod_Exchange_Particles(swarm)
 
   !-----------------------------------!
   !   Print some data on the screen   !
   !-----------------------------------!
-  if(this_proc < 2) then
-    do k = 1, swarm % n_particles
+  do k = 1, swarm % n_particles
 
-      ! Refresh the alias
-      part => swarm % particle(k)
+    ! Refresh the alias
+    part => swarm % particle(k)
 
+    if(this_proc .eq. part % proc) then
       ! Printing particle position
       write(*,'(a,i3,a,i7,a,i2,a,3es15.6,a,es12.4)')                 &
-              '#  particle: ',  k,                                   &
+              ' #  particle:',  k,                                   &
               ',  cell: ',      part % cell,                         &
               ',  processor: ', part % proc,                         &
               ',  x,y,z: ',     part % x_n, part % y_n, part % z_n,  &
               ',  cfl: ',       part % cfl
-    end do
-  end if
+    end if
+  end do
 
   end subroutine
