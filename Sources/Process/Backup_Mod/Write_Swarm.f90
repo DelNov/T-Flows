3df0162b9d256b8fb63002c115e7e12b8079b50f
Niceno <bojan.niceno.scientist@gmail.com>
2019-10-08 15:12:56 +0200

    Fixed an error.

--- Sources/Process/Backup_Mod/Write_Swarm.f90
+++ Sources/Process/Backup_Mod/Write_Swarm.f90
@@ -25,31 +25,43 @@
   ! Number of particles
   call Backup_Mod_Write_Int(fh, disp, vc, 'n_particles', swr % n_particles)
 
+  i_work(:) = 0
+  l_work(:) = .false.
+  r_work(:) = 0.0
+
   ! Pack particle data in arrays
   do k = 1, swr % n_particles
 
     ! Take aliases for the particle
     part => swr % particle(k)
 
-    i = (k-1) * n_i_vars
-    i_work(i + 1) = part % proc  ! where it resides
-    i_work(i + 2) = part % buff  ! where it wants to go
-    i_work(i + 3) = grid % comm % cell_glo(part % cell)
+    if(part % proc .eq. this_proc) then
+      i = (k-1) * n_i_vars
+      i_work(i + 1) = part % proc  ! where it resides
+      i_work(i + 2) = part % buff  ! where it wants to go
+      i_work(i + 3) = grid % comm % cell_glo(part % cell)
 
-    i = (k-1) * n_l_vars
-    l_work(i + 1) = part % deposited
-    l_work(i + 2) = part % escaped
+      i = (k-1) * n_l_vars
+      l_work(i + 1) = part % deposited
+      l_work(i + 2) = part % escaped
 
-    i = (k-1) * n_r_vars
-    r_work(i + 1) = part % x_n
-    r_work(i + 2) = part % y_n
-    r_work(i + 3) = part % z_n
-    r_work(i + 4) = part % u
-    r_work(i + 5) = part % v
-    r_work(i + 6) = part % w
-    r_work(i + 7) = part % d
-    r_work(i + 8) = part % cfl
+      i = (k-1) * n_r_vars
+      r_work(i + 1) = part % x_n
+      r_work(i + 2) = part % y_n
+      r_work(i + 3) = part % z_n
+      r_work(i + 4) = part % u
+      r_work(i + 5) = part % v
+      r_work(i + 6) = part % w
+      r_work(i + 7) = part % d
+      r_work(i + 8) = part % cfl
+    end if  ! particle on this processor
   end do
+
+  !-----------------------!
+  !   Exchange the data   !
+  !-----------------------!
+  call Comm_Mod_Global_Sum_Int_Array (swr % n_particles * n_i_vars, i_work)
+  call Comm_Mod_Global_Sum_Real_Array(swr % n_particles * n_r_vars, r_work)
 
   if(swr % n_particles > 0) then
     call Backup_Mod_Write_Int_Array(fh, disp, vc,         &
