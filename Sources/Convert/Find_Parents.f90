84732dd9c34fec1428dadaaec3e744761441b5d7
Niceno <bojan.niceno.scientist@gmail.com>
2019-10-10 18:00:36 +0200

    Made a supercharged version of Find_Parents :-)

--- Sources/Convert/Find_Parents.f90
+++ Sources/Convert/Find_Parents.f90
@@ -5,6 +5,7 @@
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Grid_Mod, only: Grid_Type
+  use Sort_Mod
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
@@ -22,17 +23,36 @@
   integer, allocatable :: near_bnd(:)  ! near boundary cells
   logical, allocatable :: is_node_bnd(:)
   logical, allocatable :: is_cell_marked(:)
+  integer, allocatable :: a1(:), a2(:), a3(:), b(:), c(:), d(:)
+  integer              :: ai(4)
 !==============================================================================!
 
   print *, '#================================================='
-  print *, '# Parent information not given in CGNS file!'
+  print *, '# Parent information not given in the input file!'
   print *, '# Looking for parents. This may take a few minutes'
   print *, '#-------------------------------------------------'
 
+  !----------------------------------------!
+  !   Allocate memory for helping arrays   !
+  !----------------------------------------!
+
+  ! Needed for sorting
+  allocate(a1(grid % n_bnd_cells))
+  allocate(a2(grid % n_bnd_cells))
+  allocate(a3(grid % n_bnd_cells))
+  allocate(b (grid % n_bnd_cells))
+  allocate(c (grid % n_bnd_cells))
+  allocate(d (grid % n_bnd_cells))
+
+  ! Needed for algorithm logic
   allocate(is_node_bnd   (grid % n_nodes))
   allocate(is_cell_marked(grid % n_cells))
   is_node_bnd   (:) = .false.
   is_cell_marked(:) = .false.
+
+  !----------------------------------------------------------!
+  !   Prepare near boundary cells to work on a smaller set   !
+  !----------------------------------------------------------!
 
   ! Mark all boundary nodes
   do c2 = -grid % n_bnd_cells, -1
@@ -71,17 +91,12 @@
   print *, '# Total number of cells:         ', grid % n_cells
   print *, '# Number of near boundary cells: ', n_near_bnd
 
+  !---------------------------------------------!
+  !   Collect and sort data from inside cells   !
+  !---------------------------------------------!
   n_bnd_proj = 0
   n_fr_cells = n_near_bnd / 20
   do cb = 1, n_near_bnd
-
-    ! Print some statistics on the screen
-    if(mod( cb, n_fr_cells ) .eq. 0) then
-      print '(a2, f5.0, a14)',              &
-        ' #',                               &
-        (100. * cb / (1.0*(n_near_bnd))),  &
-        ' % complete...'
-    end if ! each 5%
 
     ! Get near boundary cell value
     c1 = near_bnd(cb)
@@ -92,39 +107,56 @@
     if(grid % cells_n_nodes(c1) .eq. 6) fn = neu_wed
     if(grid % cells_n_nodes(c1) .eq. 8) fn = neu_hex
 
-    do c2 = -grid % n_bnd_cells, -1
+    do j = 1, 6  ! from 1 to 6th face (6 = hexahedron)
+      n_match = 0
 
-      !------------------------------!
-      !   Number of matching nodes   !
-      !------------------------------!
-      do j = 1, 6  ! from 1 to 6th face (6 = hexahedron)
-        n_match = 0
+      n_face_nodes = 4         ! assume it is a quad
+      if( fn(j, 4) < 0 ) then  ! nope, it was a triangle
+        n_face_nodes = 3
+      end if
 
-        n_face_nodes = 4         ! assume it is a quad
-        if( fn(j, 4) < 0 ) then  ! nope, it was a triangle
-          n_face_nodes = 3
+      do n1 = 1, n_face_nodes
+        if(fn(j, n1) > 0) then  ! if this node exists
+          if( is_node_bnd( grid % cells_n(fn(j, n1), c1) ) ) then
+            n_match = n_match + 1
+          end if
         end if
+      end do ! n1
+      if(n_match .eq. n_face_nodes) then
+        n_bnd_proj = n_bnd_proj + 1
+        ai(1:4) = grid % cells_n( fn(j,1:4), c1)
+        call Sort_Mod_Int(ai)
+        a1(n_bnd_proj) = ai(2)
+        a2(n_bnd_proj) = ai(3)
+        a3(n_bnd_proj) = ai(4)
+        c (n_bnd_proj) = c1
+        d (n_bnd_proj) = j
+      end if ! n_match .eq. n_face_nodes
+    end do ! j
 
-        do n1 = 1, n_face_nodes
-          do n2 = 1, grid % cells_n_nodes(c2)  ! from 1 to 3/4th node in face
-            if(fn(j, n1) > 0) then  ! if this node exists
-              if(grid % cells_n(fn(j, n1), c1) .eq.  &
-                 grid % cells_n(n2,        c2)) then
-                n_match = n_match + 1
-              end if
-            end if
-          end do ! n2
-        end do ! n1
-        if(n_match .eq. n_face_nodes) then
-          grid % cells_bnd_color(j, c1) = grid % bnd_cond % color(c2)
-          n_bnd_proj = n_bnd_proj + 1
-        end if ! n_match .eq. n_face_nodes
-      end do ! j
-
-    end do  ! c2
   end do    ! c1(cb)
+  call Sort_Mod_3_Int_Carry_2_Int(a1, a2, a3, c, d)
 
-  print *, '# Number of boundary cells:           ', grid % n_bnd_cells
-  print *, '# Number of projected boundary cells: ', n_bnd_proj
+  !-----------------------------------------------!
+  !   Collect and sort data from boundary cells   !
+  !-----------------------------------------------!
+  do c2 = -grid % n_bnd_cells, -1
+    ai(1:4) = -1
+    n_face_nodes = grid % cells_n_nodes(c2)
+    ai(1:n_face_nodes) = grid % cells_n(1:n_face_nodes, c2)
+    call Sort_Mod_Int(ai)
+    a1(-c2) = ai(2)
+    a2(-c2) = ai(3)
+    a3(-c2) = ai(4)
+    b (-c2) = c2
+  end do
+  call Sort_Mod_3_Int_Carry_Int(a1, a2, a3, b)
+
+  !---------------------------------!
+  !   Finally, just match them up   !
+  !---------------------------------!
+  do cb = 1, n_bnd_proj
+    grid % cells_bnd_color(d(cb), c(cb)) = grid % bnd_cond % color(b(cb))
+  end do
 
   end subroutine
