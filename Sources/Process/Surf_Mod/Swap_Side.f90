5597f4bcb982d494695980a212f2fea3cc5bb51a
Niceno <bojan.niceno.scientist@gmail.com>
2019-10-17 14:17:40 +0200

    Cleaned it up a lot!

--- Sources/Process/Surf_Mod/Swap_Side.f90
+++ Sources/Process/Surf_Mod/Swap_Side.f90
@@ -58,6 +58,9 @@
     sbc = elem(eb) % si;  sbd = elem(eb) % sj
   end if
 
+  !--------------------------------------------!
+  !   Change the orientation of the elements   !
+  !--------------------------------------------!
   elem(ea) % i  = a;    elem(ea) % j  = b;    elem(ea) % k  = d
   elem(ea) % ei = ebd;  elem(ea) % ej = ead;  elem(ea) % ek = eb
   elem(ea) % si = sbd;  elem(ea) % sj = sad;  elem(ea) % sk = s
@@ -66,6 +69,22 @@
   elem(eb) % ei = ebc;  elem(eb) % ej = ea;   elem(eb) % ek = eac
   elem(eb) % si = sbc;  elem(eb) % sj = s;    elem(eb) % sk = sac
 
+  !-------------------------------------------------!
+  !   Change the orientation of the side properly   !
+  !-------------------------------------------------!
+  if(a < b) then
+    side(s) % c  = a;   side(s) % d  = b
+    side(s) % a  = d;   side(s) % b  = c
+    side(s) % ea = ea;  side(s) % eb = eb
+  else
+    side(s) % c  = b;   side(s) % d  = a
+    side(s) % a  = c;   side(s) % b  = d
+    side(s) % ea = eb;  side(s) % eb = ea
+  end if
+
+  !-----------------------------------------------------!
+  !   Update information on the neighbouring elements   !
+  !-----------------------------------------------------!
   if(eac .ne. 0) then
     if(elem(eac) % ei .eq. ea) elem(eac) % ei = eb
     if(elem(eac) % ej .eq. ea) elem(eac) % ej = eb
@@ -78,6 +97,9 @@
     if(elem(ebd) % ek .eq. eb) elem(ebd) % ek = ea
   end if
 
+  !--------------------------------------------------!
+  !   Update information on the neighbouring sides   !
+  !--------------------------------------------------!
   if(side(sad) % ea .eq. ea) side(sad) % a = b
   if(side(sad) % eb .eq. ea) side(sad) % b = b
 
@@ -93,16 +115,6 @@
     side(sbd) % b  = a
   end if
 
-  if(a < b) then
-    side(s) % c  = a;   side(s) % d  = b
-    side(s) % a  = d;   side(s) % b  = c
-    side(s) % ea = ea;  side(s) % eb = eb
-  else
-    side(s) % c  = b;   side(s) % d  = a
-    side(s) % a  = c;   side(s) % b  = d
-    side(s) % ea = eb;  side(s) % eb = ea
-  end if
-
   if(side(sac) % ea .eq. ea) then
     side(sac) % ea = eb
     side(sac) % a  = b
@@ -110,21 +122,6 @@
   if(side(sac) % eb .eq. ea) then
     side(sac) % eb = eb
     side(sac) % b  = b
-  end if
-
-  if(side(sad) % ea .eq. ea)  side(sad) % a = b
-  if(side(sad) % eb .eq. ea)  side(sad) % b = b
-
-  if(side(sbc) % ea .eq. eb)  side(sbc) % a = a
-  if(side(sbc) % eb .eq. eb)  side(sbc) % b = a
-
-  if(side(sbd) % ea .eq. eb) then
-    side(sbd) % ea = ea
-    side(sbd) % a  = a
-  end if
-  if(side(sbd) % eb .eq. eb) then
-    side(sbd) % eb = ea
-    side(sbd) % b  = a
   end if
 
   end subroutine
