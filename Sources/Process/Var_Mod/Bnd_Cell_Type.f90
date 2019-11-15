c116e24262c90e4e5da3f79f68ada0632388a3c6
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-15 05:45:50 +0100

    Removed a trailing space.

--- Sources/Process/Var_Mod/Bnd_Cell_Type.f90
+++ Sources/Process/Var_Mod/Bnd_Cell_Type.f90
@@ -6,7 +6,7 @@
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   type(Var_Type) :: phi
-  integer        :: bnd_cell  
+  integer        :: bnd_cell
 !------------------------------------------------------------------------------!
 
   Var_Mod_Bnd_Cell_Type = phi % bnd_cell_type(bnd_cell)
