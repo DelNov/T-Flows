b9ba4f9800200adc4fec6792e890786df9914674
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-15 16:31:36 +0100

    Added two new memeber functions

---
+++ Sources/Process/Control_Mod/Basic_Functions/Read_Int_Item_On.f90
@@ -0,0 +1,52 @@
+!==============================================================================!
+  subroutine Control_Mod_Read_Int_Item_On(keyword, def, val, verbose)
+!------------------------------------------------------------------------------!
+!   Working horse function to read integer value (argument "val") behind a     !
+!   keyword (argument "keyword") in control file.  If not found, a default     !
+!   vaue specified in argument "def" is used.
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  character(len=*)  :: keyword
+  integer           :: def      ! default value
+  integer           :: val      ! spefified value, if found
+  logical, optional :: verbose
+!-----------------------------------[Locals]-----------------------------------!
+  logical :: reached_end
+!==============================================================================!
+
+  ! Set default value
+  val = def
+
+  !-----------------------------------------------------!
+  !   Browse through command file to find the keyword   !
+  !-----------------------------------------------------!
+  call Tokenizer_Mod_Read_Line(CONTROL_FILE_UNIT, reached_end)
+  if(reached_end) goto 1
+
+  ! Found the correct keyword
+  if(line % tokens(1) .eq. trim(keyword)) then
+    read(line % tokens(2), *) val
+    return
+
+  ! Keyword not found, try to see if there is similar, maybe it was a typo
+  else
+    call Control_Mod_Similar_Warning( keyword, trim(line % tokens(1)) )
+  end if
+
+  !--------------------------------------------!
+  !   Keyword was not found; issue a warning   !
+  !--------------------------------------------!
+1 if(present(verbose)) then
+    if(verbose .and. this_proc < 2) then
+      if(def .eq. HUGE_INT) then
+        print '(3a,i9)', ' # NOTE! Could not find the keyword: ',  &
+                          trim(keyword), '. Using the default HUGE_INT value'
+      else
+        print '(3a,i9)', ' # NOTE! Could not find the keyword: ',  &
+                          trim(keyword), '. Using the default: ', def
+      end if
+    end if
+  end if
+
+  end subroutine
