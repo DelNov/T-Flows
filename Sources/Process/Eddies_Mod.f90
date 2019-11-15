8925abc981c9d83343038cb38995abc97d05347d
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-15 12:45:28 +0100

    Works in three directions now.

--- Sources/Process/Eddies_Mod.f90
+++ Sources/Process/Eddies_Mod.f90
@@ -18,7 +18,8 @@
   !               !
   !---------------!
   type Eddy_Type
-    real :: x, y ,z  ! eddy's position
+    real :: x, y, z  ! eddy's position
+    real :: u, v, w  ! eddy's velocity
     real :: length   ! eddy's length
     real :: radius   ! eddy's radius
     real :: sense    ! eddy's sense of rotation
@@ -39,20 +40,29 @@
     type(Eddy_Type), allocatable :: eddy(:)   ! storage for eddies
 
     real :: max_radius  ! maximum radius for an eddy
+    real :: x_plane
+    real :: y_plane
+    real :: z_plane
 
     ! Data specific to boundary condition plane eddies are defined
     integer :: n_bnd_cells      ! number of boundary cells in this processor
     integer :: n_bnd_cells_glo  ! number of boundary cells over all processors
 
-    ! Coordinates of boundary cells over all processors
-    ! (these are also potential eddy centers)
+    ! Coordinates, wall distance and velocity components
+    ! of boundary cells over all processors
+    ! (coordinates are also potential eddy centers)
     real, allocatable :: bnd_xc(:)
     real, allocatable :: bnd_yc(:)
     real, allocatable :: bnd_zc(:)
+    real, allocatable :: bnd_wd(:)
+    real, allocatable :: bnd_u (:)
+    real, allocatable :: bnd_v (:)
+    real, allocatable :: bnd_w (:)
   end type
 
   contains
 
+  include 'Eddies_Mod/Advance.f90'
   include 'Eddies_Mod/Allocate.f90'
   include 'Eddies_Mod/Gather_Bnd_Cells.f90'
   include 'Eddies_Mod/Place_Eddy.f90'
