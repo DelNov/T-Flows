861a4a76c033cb5a4f60e58b5c5e908f98e834b6
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-16 08:03:01 +0100

    Added to repository.

---
+++ Sources/Process/Turb_Mod/Calculate_Face_Stress.f90
@@ -0,0 +1,56 @@
+!==============================================================================!
+  subroutine Turb_Mod_Calculate_Face_Stress(turb, ui, f_stress, s)
+!------------------------------------------------------------------------------!
+!   Computes turbulent stress on a cell face for all turbulence models.        !
+!   It is called from Compute_Momentum, while discretizing diffusion terms.    !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Turb_Type), target :: turb
+  real                    :: f_stress
+  integer                 :: s
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type),  pointer :: grid
+  type(Var_Type),   pointer :: ui
+  type(Var_Type),   pointer :: uu, vv, ww, uv, uw, vw
+  integer                   :: c1, c2
+  real                      :: uu_f, vv_f, ww_f, uv_f, uw_f, vw_f
+!==============================================================================!
+
+  ! Take alias
+  grid => turb % pnt_grid
+  call Turb_Mod_Alias_Stresses(turb, uu, vv, ww, uv, uw, vw)
+
+  c1 = grid % faces_c(1,s)
+  c2 = grid % faces_c(2,s)
+
+  ! Add influence of Re stresses
+  if(turbulence_model .eq. RSM_MANCEAU_HANJALIC .or.  &
+     turbulence_model .eq. RSM_HANJALIC_JAKIRLIC) then
+    if(turbulence_model_variant .ne. STABILIZED) then
+      if(ui % name .eq. 'U') then
+        uu_f = grid % fw(s) * uu % n(c1) + (1.0-grid % fw(s)) * uu % n(c2)
+        uv_f = grid % fw(s) * uv % n(c1) + (1.0-grid % fw(s)) * uv % n(c2)
+        uw_f = grid % fw(s) * uw % n(c1) + (1.0-grid % fw(s)) * uw % n(c2)
+        f_stress = - (  uu_f * grid % sx(s)  &
+                      + uv_f * grid % sy(s)  &
+                      + uw_f * grid % sz(s) )
+      else if(ui % name .eq. 'V') then
+        uv_f = grid % fw(s) * uv % n(c1) + (1.0-grid % fw(s)) * uv % n(c2)
+        vv_f = grid % fw(s) * vv % n(c1) + (1.0-grid % fw(s)) * vv % n(c2)
+        vw_f = grid % fw(s) * vw % n(c1) + (1.0-grid % fw(s)) * vw % n(c2)
+        f_stress =  - (  uv_f * grid % sx(s)  &
+                       + vv_f * grid % sy(s)  &
+                       + vw_f * grid % sz(s) )
+      else if(ui % name .eq. 'W') then
+        uw_f = grid % fw(s) * uw % n(c1) + (1.0-grid % fw(s)) * uw % n(c2)
+        vw_f = grid % fw(s) * vw % n(c1) + (1.0-grid % fw(s)) * vw % n(c2)
+        ww_f = grid % fw(s) * ww % n(c1) + (1.0-grid % fw(s)) * ww % n(c2)
+        f_stress =  - (  uw_f * grid % sx(s)  &
+                       + vw_f * grid % sy(s)  &
+                       + ww_f * grid % sz(s) )
+      end if
+    end if
+  end if
+
+  end subroutine
---
+++ Sources/Process/Turb_Mod/Calculate_Face_Vis.f90
@@ -0,0 +1,65 @@
+!==============================================================================!
+  subroutine Turb_Mod_Calculate_Face_Vis(turb, vis_eff, s)
+!------------------------------------------------------------------------------!
+!   Computes turbulent viscosity on a cell face for all turbulence models.     !
+!   It is called from Compute_Momentum, while discretizing diffusion terms.    !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Turb_Type), target :: turb
+  real                    :: vis_eff
+  integer                 :: s
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type),  pointer :: grid
+  integer                   :: c1, c2
+  real                      :: fs
+!==============================================================================!
+
+  ! Take alias
+  grid => turb % pnt_grid
+
+  c1 = grid % faces_c(1,s)
+  c2 = grid % faces_c(2,s)
+  fs = grid % f(s)
+
+  if (c2 > 0) then
+    vis_eff = fs * viscosity(c1) + (1.0 - fs) * viscosity(c2)
+  else
+    vis_eff = viscosity(c1)
+  end if
+
+  if(turbulence_model .ne. NONE .and.  &
+     turbulence_model .ne. DNS) then
+    vis_eff = vis_eff + grid % fw(s)  * turb % vis_t(c1)  &
+                  +(1.0-grid % fw(s)) * turb % vis_t(c2)
+  end if
+
+  if(turbulence_model .eq. HYBRID_LES_RANS) then
+    vis_eff =      grid % fw(s)  * turb % vis_t_eff(c1)   &
+            + (1.0-grid % fw(s)) * turb % vis_t_eff(c2) + vis_eff
+    end if
+
+  if(c2 < 0) then
+    if( turbulence_model .eq. LES_SMAGORINSKY    .or.  &
+        turbulence_model .eq. LES_DYNAMIC        .or.  &
+        turbulence_model .eq. HYBRID_LES_PRANDTL .or.  &
+        turbulence_model .eq. LES_WALE) then
+      if(Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. WALL .or.  &
+         Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. WALLFL) then
+        vis_eff = turb % vis_w(c1)
+      end if
+    end if
+  end if
+
+  if( turbulence_model .eq. K_EPS_ZETA_F     .or.  &
+      turbulence_model .eq. HYBRID_LES_RANS  .or.  &
+      turbulence_model .eq. K_EPS) then 
+    if(c2 < 0) then
+      if(Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. WALL .or.  &
+         Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. WALLFL) then
+        vis_eff = turb % vis_w(c1)
+      end if
+    end if
+  end if
+
+  end subroutine
