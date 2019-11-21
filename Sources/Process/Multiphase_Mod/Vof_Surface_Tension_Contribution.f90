4176041d779550de13b8a3d9f70e9fc1efb70bb5
mijaplex <mijail.febres@gmail.com>
2019-11-21 11:56:45 +0100

    Simplified a bit.

--- Sources/Process/Multiphase_Mod/Vof_Surface_Tension_Contribution.f90
+++ Sources/Process/Multiphase_Mod/Vof_Surface_Tension_Contribution.f90
@@ -1,27 +1,40 @@
 !==============================================================================!
-  subroutine Multiphase_Mod_Vof_Surface_Tension_Contribution(mult)
+  subroutine Multiphase_Mod_Vof_Surface_Tension_Contribution(mult, n)
 !------------------------------------------------------------------------------!
-!   Computes the value at the cell face using different convective  schemes.   !
+!        Computes the Curvature based on old-fashion  Brackbill's CSF          !
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use Work_Mod, only: sum_v1 => r_cell_01,  &
                       sum_v2 => r_cell_02,  &
-                      sum_v3 => r_cell_03
+                      sum_v3 => r_cell_03,  &
+                      sum_v4 => r_cell_04,  &
+                      sum_v5 => r_cell_05,  &
+                      sum_v6 => r_cell_06
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   type(Multiphase_Type), target :: mult
 !-----------------------------------[Locals]-----------------------------------!
+  type(Field_Type),pointer :: flow
   type(Grid_Type), pointer :: grid
-  integer                  :: s, c, c1, c2, c_iter,n_conv
+  type(Var_Type),  pointer :: vof
+  type(Face_type),   pointer :: v_flux
+  character(len=1)         :: charI
+  integer                  :: s, c, c1, c2, c_iter,n_conv,n
   real                     :: vol_face, grad_face(3)
-  real                     :: dotprod, sxyz_mod, fs
+  real                     :: dotprod, sxyz_mod, fs, epsloc, err_2
 !==============================================================================!
 
-  ! First take aliases
-  grid => mult % pnt_grid
+  err_2 = 1.0e-14
+  epsloc = epsilon(epsloc)
 
-  mult % vof % oo = mult % vof % n
+  ! First take aliases
+  flow   => mult % pnt_flow
+  grid => mult % pnt_grid
+  vof  => mult % vof
+  v_flux => flow % v_flux
+
+  vof % oo = vof % n
 
   ! This parameter maybe also implemented in "control" file
   n_conv = 2
@@ -39,7 +52,7 @@
       c1 = grid % faces_c(1,s)
       c2 = grid % faces_c(2,s)
       if (c2 < 0) then
-        mult % vof % oo(c2) = mult % vof % oo(c1) 
+        vof % oo(c2) = vof % oo(c1) 
       end if
     end do
 
@@ -50,23 +63,41 @@
       fs = grid % f(s)
       sxyz_mod = sqrt(grid % sx(s) ** 2 + grid % sy(s) ** 2 + grid % sz(s) ** 2)
       if (c2 > 0) then
-        vol_face = fs * mult % vof % oo(c1) + (1.0 - fs) * mult % vof % oo(c2)
+        vol_face = fs * vof % oo(c1) + (1.0 - fs) * vof % oo(c2)
         sum_v1(c1) = sum_v1(c1) + vol_face * sxyz_mod
         sum_v2(c1) = sum_v2(c1) + sxyz_mod 
         sum_v1(c2) = sum_v1(c2) + vol_face * sxyz_mod
         sum_v2(c2) = sum_v2(c2) + sxyz_mod 
       else
-        vol_face = mult % vof % oo(c1)
+        vol_face = vof % oo(c1)
         sum_v1(c1) = sum_v1(c1) + vol_face * sxyz_mod
         sum_v2(c1) = sum_v2(c1) + sxyz_mod 
       end if
     end do
 
+    call Comm_Mod_Exchange_Real(grid, sum_v1)
+    call Comm_Mod_Exchange_Real(grid, sum_v2)
+
     do c = 1, grid % n_cells
-      mult % vof % oo(c) = sum_v1(c) / sum_v2(c)
+      vof % oo(c) = sum_v1(c) / sum_v2(c)
     end do
 
+    call Comm_Mod_Exchange_Real(grid, vof % oo)
+
+!    if (c_iter == 6) then
+!      vof % min = vof % oo
+!    end if
   end do
+
+  !clean noise
+  do c = 1, grid % n_cells
+    if (vof % oo(c) < epsloc) then
+      vof % oo(c) = 0.0
+    end if
+    if ((1.0 - vof % oo(c)) < epsloc) then
+      vof % oo(c) = 1.0
+    end if
+  end do 
 
   !-------------------!
   !   Find Gradient   !
@@ -75,13 +106,17 @@
   sum_v1 = 0.0
   sum_v2 = 0.0
   sum_v3 = 0.0
+  sum_v4 = 0.0
+  sum_v5 = 0.0
+  sum_v6 = 0.0
 
   do s = 1, grid % n_faces
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
     fs = grid % f(s)
     if (c2 > 0) then
-      vol_face = fs * mult % vof % oo(c1) + (1.0 - fs) * mult % vof % oo(c2)
+      vol_face = fs * vof % oo(c1) + (1.0 - fs) * vof % oo(c2)
+
       sum_v1(c1) = sum_v1(c1) + vol_face * grid % sx(s)
       sum_v2(c1) = sum_v2(c1) + vol_face * grid % sy(s)
       sum_v3(c1) = sum_v3(c1) + vol_face * grid % sz(s)
@@ -89,25 +124,66 @@
       sum_v1(c2) = sum_v1(c2) - vol_face * grid % sx(s)
       sum_v2(c2) = sum_v2(c2) - vol_face * grid % sy(s)
       sum_v3(c2) = sum_v3(c2) - vol_face * grid % sz(s)
+
+!      vol_face = fs * vof % min(c1) + (1.0 - fs) * vof % min(c2)
+!      sum_v4(c1) = sum_v4(c1) + vol_face * grid % sx(s)
+!      sum_v5(c1) = sum_v5(c1) + vol_face * grid % sy(s)
+!      sum_v6(c1) = sum_v6(c1) + vol_face * grid % sz(s)
+!
+!      sum_v4(c2) = sum_v4(c2) - vol_face * grid % sx(s)
+!      sum_v5(c2) = sum_v5(c2) - vol_face * grid % sy(s)
+!      sum_v6(c2) = sum_v6(c2) - vol_face * grid % sz(s)
     else
-      vol_face = mult % vof % oo(c1)
+      vol_face = vof % oo(c1)
+
       sum_v1(c1) = sum_v1(c1) + vol_face * grid % sx(s)
       sum_v2(c1) = sum_v2(c1) + vol_face * grid % sy(s)
       sum_v3(c1) = sum_v3(c1) + vol_face * grid % sz(s)
+
+!      vol_face = vof % min(c1)
+!      sum_v4(c1) = sum_v4(c1) + vol_face * grid % sx(s)
+!      sum_v5(c1) = sum_v5(c1) + vol_face * grid % sy(s)
+!      sum_v6(c1) = sum_v6(c1) + vol_face * grid % sz(s)
     end if
   end do
 
   do c = 1, grid % n_cells
+    !clean noise
+    if (abs(sum_v1(c)) < epsloc) then
+      sum_v1(c) = 0.0
+    end if
+    if (abs(sum_v2(c)) < epsloc) then
+      sum_v2(c) = 0.0
+    end if
+    if (abs(sum_v3(c)) < epsloc) then
+      sum_v3(c) = 0.0
+    end if
     sum_v1(c) = sum_v1(c) / grid % vol(c)
     sum_v2(c) = sum_v2(c) / grid % vol(c)
     sum_v3(c) = sum_v3(c) / grid % vol(c)
+
+!    sum_v4(c) = sum_v4(c) / grid % vol(c)
+!    sum_v5(c) = sum_v5(c) / grid % vol(c)
+!    sum_v6(c) = sum_v6(c) / grid % vol(c)
   end do
+
+  call Comm_Mod_Exchange_Real(grid, sum_v1)
+  call Comm_Mod_Exchange_Real(grid, sum_v2)
+  call Comm_Mod_Exchange_Real(grid, sum_v3)
+
+!  call Comm_Mod_Exchange_Real(grid, sum_v4)
+!  call Comm_Mod_Exchange_Real(grid, sum_v5)
+!  call Comm_Mod_Exchange_Real(grid, sum_v6)
+
+  !vof % x = sum_v4
+  !vof % y = sum_v5
+  !vof % z = sum_v6
 
   !--------------------!
   !   Find Curvature   !
   !--------------------!
 
-  mult % vof % oo =0.0
+  vof % oo = 0.0
 
   do s = 1, grid % n_faces
     c1 = grid % faces_c(1,s)
@@ -122,16 +198,13 @@
                     + grad_face(2) ** 2  &
                     + grad_face(3) ** 2)
 
-      if (sxyz_mod > TINY) then
+      if (sxyz_mod > epsloc) then
         dotprod = (grad_face(1) * grid % sx(s)  &
                  + grad_face(2) * grid % sy(s)  &
                  + grad_face(3) * grid % sz(s)) / sxyz_mod
 
-        mult % vof % oo(c1) = mult % vof % oo(c1) + dotprod
-        mult % vof % oo(c2) = mult % vof % oo(c2) - dotprod
-      else
-        mult % vof % oo(c1) = 0.0
-        mult % vof % oo(c2) = 0.0
+        vof % oo(c1) = vof % oo(c1) + dotprod
+        vof % oo(c2) = vof % oo(c2) - dotprod
       end if
 
     else
@@ -140,24 +213,32 @@
       grad_face(3) = sum_v3(c1)
 
       sxyz_mod = sqrt(grad_face(1) ** 2  &
-                    + grad_face(2) ** 2                   &
+                    + grad_face(2) ** 2  &
                     + grad_face(3) ** 2)
 
-      if (sxyz_mod > TINY) then
+      if (sxyz_mod > epsloc) then
         dotprod = (grad_face(1) * grid % sx(s)  &
                  + grad_face(2) * grid % sy(s)  &
                  + grad_face(3) * grid % sz(s)) / sxyz_mod
 
-        mult % vof % oo(c1) = mult % vof % oo(c1) + dotprod
-      else
-        mult % vof % oo(c1) = 0.0
+        vof % oo(c1) = vof % oo(c1) + dotprod
       end if
 
     end if
   end do
 
+  call Comm_Mod_Exchange_Real(grid, vof % oo)
+
+
   do c = 1, grid % n_cells
-    mult % vof % oo(c) = - mult % vof % oo(c) / grid % vol(c)
+    vof % oo(c) = - vof % oo(c) / grid % vol(c)
   end do
+
+  !clean noise
+  do c = 1, grid % n_cells
+    if (abs(vof % oo(c)) < epsloc) then
+      vof % oo(c) = 0.0
+    end if
+  end do 
 
   end subroutine
