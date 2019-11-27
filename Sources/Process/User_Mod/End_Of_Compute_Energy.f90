7d3324038598415468c0bb04976a7c82a22004bb
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-27 13:25:47 +0100

    Updated!

--- Sources/Process/User_Mod/End_Of_Compute_Energy.f90
+++ Sources/Process/User_Mod/End_Of_Compute_Energy.f90
@@ -1,13 +1,15 @@
 !==============================================================================!
-  subroutine User_Mod_End_Of_Compute_Energy(flow, dt, ini)
+  subroutine User_Mod_End_Of_Compute_Energy(flow, turb, mult, dt, ini)
 !------------------------------------------------------------------------------!
-!   This function is called at the end of time step.                           !
+!   This function is called at the end of Compute_Energy function.             !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Field_Type) :: flow
-  real             :: dt    ! time step
-  integer          :: ini   ! inner iteration
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  real                          :: dt    ! time step
+  integer                       :: ini   ! inner iteration
 !==============================================================================!
 
   end subroutine
