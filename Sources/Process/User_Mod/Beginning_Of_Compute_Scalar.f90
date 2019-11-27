4e042c77cd9ee93bb721f2a45a286ca028173466
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-27 13:26:12 +0100

    Updated!

---
+++ Sources/Process/User_Mod/Beginning_Of_Compute_Scalar.f90
@@ -0,0 +1,15 @@
+!==============================================================================!
+  subroutine User_Mod_Beginning_Of_Compute_Scalar(flow, turb, mult, dt, ini)
+!------------------------------------------------------------------------------!
+!   This function is called at the end of Compute_Scalar function.             !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  real                          :: dt    ! time step
+  integer                       :: ini   ! inner iteration
+!==============================================================================!
+
+  end subroutine
