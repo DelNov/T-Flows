6ffefe94d52c0f6ac5515c58b5843f026a0fceef
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-28 03:15:55 +0100

    Corrected indentation at one place.

--- Sources/Process/Update_Boundary_Values.f90
+++ Sources/Process/Update_Boundary_Values.f90
@@ -273,11 +273,11 @@
   end do
 
   ! Integrate (summ) heated area, and heat up
- if(heat_transfer) then
-   call Comm_Mod_Global_Sum_Real(heat)
-   call Comm_Mod_Global_Sum_Real(heated_area)
-   heat_flux = heat / max(heated_area, TINY)
- end if
+  if(heat_transfer) then
+    call Comm_Mod_Global_Sum_Real(heat)
+    call Comm_Mod_Global_Sum_Real(heated_area)
+    heat_flux = heat / max(heated_area, TINY)
+  end if
 
   !-------------!
   !   Scalars   !
