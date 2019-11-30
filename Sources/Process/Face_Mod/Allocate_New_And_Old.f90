f35987453db2fb219ae2ded47dc96dcca35fdd60
Niceno <bojan.niceno.scientist@gmail.com>
2019-11-30 07:39:25 +0100

    Corrected the order of arguments.

--- Sources/Process/Face_Mod/Allocate_New_And_Old.f90
+++ Sources/Process/Face_Mod/Allocate_New_And_Old.f90
@@ -1,20 +1,20 @@
 !==============================================================================!
-  subroutine Face_Mod_Allocate_New_Only(name_phi, phi, grid)
+  subroutine Face_Mod_Allocate_New_Only(phi, grid, name_phi)
 !------------------------------------------------------------------------------!
 !   This is to allocate a face-based uknown, holding only current value.       !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  character(len=*)        :: name_phi
   type(Face_Type)         :: phi
   type(Grid_Type), target :: grid
+  character(len=*)        :: name_phi
 !==============================================================================!
-
-  ! Store variable name
-  phi % name = name_phi
 
   ! Store grid for which the variable is defined
   phi % pnt_grid => grid
+
+  ! Store variable name
+  phi % name = name_phi
 
   ! Values in the new (n) time step
   allocate (phi % n(grid % n_faces));  phi % n = 0.0
