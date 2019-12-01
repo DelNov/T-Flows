8883676e2118f871b3d3763e806ab581fdb2fda1
Niceno <bojan.niceno.scientist@gmail.com>
2019-12-01 07:48:05 +0100

    Uses grid % fw(s) wherever more appropriate.

--- Sources/Process/Turb_Mod/Compute_Stress.f90
+++ Sources/Process/Turb_Mod/Compute_Stress.f90
@@ -39,7 +39,7 @@
   real                       :: phix_f, phiy_f, phiz_f
   real                       :: vis_t_f
   real                       :: dt
-  real                       :: visc_const, dens_const
+  real                       :: visc_f
 !==============================================================================!
 !                                                                              !
 !   The form of equations which are being solved:                              !
@@ -112,17 +112,17 @@
 
     ! vis_tur is used to make diaginal element more dominant.
     ! This contribution is later substracted.
-    vis_t_f = grid % fw(s)       * turb % vis_t(c1)  &
+    vis_t_f =      grid % fw(s)  * turb % vis_t(c1)  &
             + (1.0-grid % fw(s)) * turb % vis_t(c2)
 
-    visc_const = grid % f(s)         * flow % viscosity(c1)  &
-               + (1.0 - grid % f(s)) * flow % viscosity(c2)
+    visc_f =        grid % fw(s)  * flow % viscosity(c1)  &
+           + (1.0 - grid % fw(s)) * flow % viscosity(c2)
 
-    vis_eff = visc_const + vis_t_f
+    vis_eff = visc_f + vis_t_f
 
     if(turbulence_model .eq. RSM_HANJALIC_JAKIRLIC) then
       if(turbulence_model_variant .ne. STABILIZED) then
-        vis_eff = 1.5*visc_const + vis_t_f
+        vis_eff = 1.5 * visc_f + vis_t_f
       end if
     end if
 
