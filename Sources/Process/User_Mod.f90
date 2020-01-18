0cb2e42d1af4bd8135a4404cf8351160da0ff0bb
Niceno <bojan.niceno.scientist@gmail.com>
2020-01-18 16:41:42 +0100

    Pulled some files from development_branch.

--- Sources/Process/User_Mod.f90
+++ Sources/Process/User_Mod.f90
@@ -7,12 +7,12 @@
   use Const_Mod
   use File_Mod
   use Comm_Mod
-  use Grid_Mod,    only: Grid_Type
-  use Matrix_Mod,  only: Matrix_Type
-  use Var_Mod,     only: Var_Type
+  use Grid_Mod
+  use Matrix_Mod
+  use Var_Mod
   use Field_Mod
-  use Bulk_Mod,    only: Bulk_Type
-  use Turb_Mod                       ! needed for saving results
+  use Bulk_Mod
+  use Turb_Mod
   use Swarm_Mod
   use Multiphase_Mod
   use Control_Mod
@@ -27,21 +27,25 @@
 
   include 'User_Mod/Allocate.f90'
   include 'User_Mod/Before_Exit.f90'
-  include 'User_Mod/Beginning_Of_Time_Step.f90'
   include 'User_Mod/Beginning_Of_Compute_Energy.f90'
   include 'User_Mod/Beginning_Of_Compute_Momentum.f90'
   include 'User_Mod/Beginning_Of_Compute_Pressure.f90'
+  include 'User_Mod/Beginning_Of_Compute_Scalar.f90'
   include 'User_Mod/Beginning_Of_Correct_Velocity.f90'
+  include 'User_Mod/Beginning_Of_Iteration.f90'
+  include 'User_Mod/Beginning_Of_Time_Step.f90'
   include 'User_Mod/Calculate_Mean.f90'
   include 'User_Mod/End_Of_Compute_Energy.f90'
   include 'User_Mod/End_Of_Compute_Momentum.f90'
   include 'User_Mod/End_Of_Compute_Pressure.f90'
+  include 'User_Mod/End_Of_Compute_Scalar.f90'
   include 'User_Mod/End_Of_Correct_Velocity.f90'
+  include 'User_Mod/End_Of_Iteration.f90'
+  include 'User_Mod/End_Of_Simulation.f90'
   include 'User_Mod/End_Of_Time_Step.f90'
-  include 'User_Mod/Initialize.f90'
+  include 'User_Mod/Initialize_Variables.f90'
   include 'User_Mod/Force.f90'
   include 'User_Mod/Save_Results.f90'
-  include 'User_Mod/Save_Swarm.f90'
   include 'User_Mod/Source.f90'
 
   end module 
---
+++ Sources/Process/User_Mod/End_Of_Simulation.f90
@@ -0,0 +1,16 @@
+!==============================================================================!
+  subroutine User_Mod_End_Of_Simulation(flow, turb, mult, swarm, n, time)
+!------------------------------------------------------------------------------!
+!   This function is called at the end of simulation.                          !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  integer                       :: n     ! time step
+  real                          :: time  ! physical time
+!==============================================================================!
+
+  end subroutine
