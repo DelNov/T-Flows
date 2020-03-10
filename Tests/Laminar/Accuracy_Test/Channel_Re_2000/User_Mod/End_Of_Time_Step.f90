174bed443db87b410801fb1c085662efa229aaff
Egor Palkin <palkinev89@gmail.com>
2020-03-10 12:24:59 +0700

    Updated Tests/Laminar/Accuracy_Test/Channel_Re_2000/User_Mod/End_Of_Time_Step.f90 Now solution does not diverge

--- Tests/Laminar/Accuracy_Test/Channel_Re_2000/User_Mod/End_Of_Time_Step.f90
+++ Tests/Laminar/Accuracy_Test/Channel_Re_2000/User_Mod/End_Of_Time_Step.f90
@@ -13,18 +13,26 @@
   real                          :: time  ! physical time
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type), pointer :: grid
+  type(Bulk_Type), pointer :: bulk
+  type(Var_Type),  pointer :: u, v, w ! [m/s]
+  real, pointer            :: visc(:) ! [kg/(m s)]
   integer                  :: s, c1, c2
-  real                     :: nx, ny, nz, ut, vt, wt, ut_mag, stress
-  real                     :: tau_wall_x, tau_wall_y, tau_wall_z
+  real                     :: nx, ny, nz
+  real                     :: ut, vt, wt, ut_mag ! [m/s]
+  real                     :: tau_wall_x, tau_wall_y, tau_wall_z,  &
+                              tau_wall ! [(kg m)/s^2]
 !==============================================================================!
 
   ! Take the alias to the grid
   grid => flow % pnt_grid
+  bulk => flow % bulk
+  visc => flow % viscosity
+  call Field_Mod_Alias_Momentum(flow, u, v, w)
 
   ! Initialize stresses at the walls
-  tau_wall_x = 0.0  ! [(kg m)/s^2]
-  tau_wall_y = 0.0  ! [(kg m)/s^2]
-  tau_wall_z = 0.0  ! [(kg m)/s^2]
+  tau_wall_x = 0.0
+  tau_wall_y = 0.0
+  tau_wall_z = 0.0
 
   !---------------------------------------!
   !   Browse through all boundary faces   !
@@ -45,32 +53,46 @@
         nz = grid % sz(s) / grid % s(s)
 
         ! Velocity tangential to the wall (u_t = u - u_n = u (1.0-n))
-        ut = flow % u % n(c1) * (1.0 - nx)
-        vt = flow % v % n(c1) * (1.0 - ny)
-        wt = flow % w % n(c1) * (1.0 - nz)
+        ut = u % n(c1) * (1.0 - nx)
+        vt = v % n(c1) * (1.0 - ny)
+        wt = w % n(c1) * (1.0 - nz)
 
-        ut_mag = sqrt(ut**2 + vt**2 + wt ** 2)
+        ut_mag = sqrt(ut**2 + vt**2 + wt**2)
 
-        ! Stress at the boundary cell [(kg m)/s^2 = N]
-        stress = flow % viscosity(c1) * ut_mag  &
-               / grid % wall_dist(c1) * grid % s(s)
+        ! Shear stress magnitude at the boundary cell
+        !tau_wall = visc(c1) * ut_mag  &
+        !       / grid % wall_dist(c1) * grid % s(s)
 
-        tau_wall_x = tau_wall_x + stress * ut / ut_mag  ! [(kg m)/s^2 = N]
-        tau_wall_y = tau_wall_y + stress * vt / ut_mag  ! [(kg m)/s^2 = N]
-        tau_wall_z = tau_wall_z + stress * wt / ut_mag  ! [(kg m)/s^2 = N]
+        !tau_wall_x = tau_wall_x + tau_wall * ut / ut_mag
+        !tau_wall_y = tau_wall_y + tau_wall * vt / ut_mag
+        !tau_wall_z = tau_wall_z + tau_wall * wt / ut_mag
+
+        ! Summarize shear stress components at the boundary cell
+        tau_wall_x = tau_wall_x + visc(c1) * ut &
+                   / grid % wall_dist(c1) * grid % s(s)
+        tau_wall_y = tau_wall_y + visc(c1) * vt &
+                   / grid % wall_dist(c1) * grid % s(s)
+        tau_wall_z = tau_wall_z + visc(c1) * wt &
+                   / grid % wall_dist(c1) * grid % s(s)
       end if  ! if wall
     end if
   end do
 
-  call Comm_Mod_Global_Sum_Real(tau_wall_x)
-  call Comm_Mod_Global_Sum_Real(tau_wall_y)
-  call Comm_Mod_Global_Sum_Real(tau_wall_z)
+  if( abs(bulk % flux_x_o) >= TINY ) then
+    call Comm_Mod_Global_Sum_Real(tau_wall_x)
+    bulk % p_drop_x = bulk % p_drop_x  &
+                    + tau_wall_x / grid % tot_vol  ! [kg/m^2/s^2]
+  end if
+  if( abs(bulk % flux_y_o) >= TINY ) then
+    call Comm_Mod_Global_Sum_Real(tau_wall_y)
+    bulk % p_drop_y = bulk % p_drop_y  &
+                    + tau_wall_y / grid % tot_vol  ! [kg/m^2/s^2]
+  end if
+  if( abs(bulk % flux_z_o) >= TINY ) then
+    call Comm_Mod_Global_Sum_Real(tau_wall_z)
+    bulk % p_drop_z = bulk % p_drop_z  &
+                    + tau_wall_z / grid % tot_vol  ! [kg/m^2/s^2]
+  end if
 
-  flow % bulk % p_drop_x = flow % bulk % p_drop_x  &
-                         + tau_wall_x / grid % tot_vol  ! [kg/m^2/s^2]
-  flow % bulk % p_drop_y = flow % bulk % p_drop_y  &
-                         + tau_wall_y / grid % tot_vol  ! [kg/m^2/s^2]
-  flow % bulk % p_drop_z = flow % bulk % p_drop_z  &
-                         + tau_wall_z / grid % tot_vol  ! [kg/m^2/s^2]
 
   end subroutine
