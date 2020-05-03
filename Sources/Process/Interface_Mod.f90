8934830dbf2e4ad7dc3b379380ec8456c3b243cd
Niceno <bojan.niceno.scientist@gmail.com>
2020-05-03 22:05:32 +0200

    Added to repository!

---
+++ Sources/Process/Interface_Mod.f90
@@ -0,0 +1,31 @@
+!==============================================================================!
+  module Interface_Mod
+!------------------------------------------------------------------------------!
+!   Interface between two grids.                                               !
+!------------------------------------------------------------------------------!
+!----------------------------------[Modules]-----------------------------------!
+  use Field_Mod
+  use Control_Mod
+!------------------------------------------------------------------------------!
+  implicit none
+!==============================================================================!
+
+  !--------------------!
+  !   Interface type   !
+  !--------------------!
+  type Interface_Type
+
+    integer              :: n_faces        ! number of faces at that interface
+    integer, allocatable :: faces_1   (:)  ! list of faces in domain 1
+    integer, allocatable :: close_in_2(:)  ! list of closest faces in domain 2
+    integer, allocatable :: faces_2   (:)  ! list of faces in domain 2
+    integer, allocatable :: close_in_1(:)  ! list of closest faces in domain 1
+
+  end type
+
+  contains
+
+  include 'Interface_Mod/Create.f90'
+  include 'Interface_Mod/Exchange.f90'
+
+  end module
