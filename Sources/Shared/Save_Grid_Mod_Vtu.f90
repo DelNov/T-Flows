3817cdb63717c2130a8e4d6f970d55ac19dffe3e
Niceno <bojan.niceno.scientist@gmail.com>
2020-05-07 19:57:08 +0200

    Added a few comments, nothing crucial.

--- Sources/Shared/Save_Grid_Mod_Vtu.f90
+++ Sources/Shared/Save_Grid_Mod_Vtu.f90
@@ -30,10 +30,10 @@
 
   contains
 
-  include 'Save_Grid_Mod/Vtu/Save_Vtu_Cells.f90'
-  include 'Save_Grid_Mod/Vtu/Save_Vtu_Faces.f90'
-  include 'Save_Grid_Mod/Vtu/Save_Vtu_Grid_Levels.f90'
-  include 'Save_Grid_Mod/Vtu/Save_Vtu_Links.f90'
+  include 'Save_Grid_Mod/Vtu/Save_Vtu_Cells.f90'        ! binary
+  include 'Save_Grid_Mod/Vtu/Save_Vtu_Faces.f90'        ! binary
+  include 'Save_Grid_Mod/Vtu/Save_Vtu_Grid_Levels.f90'  ! ascii, but unused
+  include 'Save_Grid_Mod/Vtu/Save_Vtu_Links.f90'        ! binary
   include 'Save_Grid_Mod/Vtu/Save_Cgns_Cells_Void.f90'
 
   end module 
