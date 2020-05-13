f15f773d27460f51d5b17c51cdcaba529a9deef3
Niceno <bojan.niceno.scientist@gmail.com>
2020-05-13 14:55:31 +0200

    Finally fixed all the glitches with sorting reals! Not only that one needs to change .eq. with Math_Mod_Approx_Real, but to be compatible with the above, one also needs approximation of .lt. operator.  I had to add a new function to Math_Mod, called Math_Mod_Smaller_Real and change all routines in Sort_Mod which sort real numbers.

---
+++ Sources/Shared/Math_Mod/Smaller_Real.f90
@@ -0,0 +1,29 @@
+!==============================================================================!
+  logical function Math_Mod_Smaller_Real(a, b, tol)
+!------------------------------------------------------------------------------!
+!   Returns .true. if a is approximatelly equal to "b", .false. otherwise.     !
+!                                                                              !
+!   The approximation is controlled by optional parameter "tol".  If it is     !
+!   not provided, the function will use value 1.0e-9 as a default.             !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  real           :: a, b
+  real, optional :: tol
+!-----------------------------------[Locals]-----------------------------------!
+  real :: tolerance
+!==============================================================================!
+
+  if( .not. present(tol) ) then
+    tolerance = 1.e-9
+  else
+    tolerance = tol
+  end if
+
+  if( a < b - tolerance ) then
+    Math_Mod_Smaller_Real = .true.
+  else
+    Math_Mod_Smaller_Real = .false.
+  end if
+
+  end function
