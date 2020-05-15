aee85b215b2bd8583c6c981ff71dd62a93a66d48
Niceno <bojan.niceno.scientist@gmail.com>
2020-05-15 09:25:24 +0200

    Updated.

--- Sources/Process/User_Mod/Interface_Exchange.f90
+++ Sources/Process/User_Mod/Interface_Exchange.f90
@@ -21,23 +21,15 @@
   !-------------------------------------------!
   do d1 = 1, n_dom
     do d2 = 1, n_dom
-      call Interface_Mod_To_Buffer(inter,                &
-                                   flow(d1) % pnt_grid,  &
-                                   flow(d2) % pnt_grid,  &
+      call Interface_Mod_To_Buffer(inter(d1, d2),        &
                                    flow(d1) % t % n,     &
                                    flow(d2) % t % n,     &
-                                   d1, d2,               &
                                    T)
-      call Interface_Mod_To_Buffer(inter,                    &
-                                   flow(d1) % pnt_grid,      &
-                                   flow(d2) % pnt_grid,      &
+      call Interface_Mod_To_Buffer(inter(d1, d2),            &
                                    flow(d1) % conductivity,  &
                                    flow(d2) % conductivity,  &
-                                   d1, d2,                   &
                                    K)
-      call Interface_Mod_To_Buffer(inter,                            &
-                                   flow(d1) % pnt_grid,              &
-                                   flow(d2) % pnt_grid,              &
+      call Interface_Mod_To_Buffer(inter(d1, d2),                    &
                                    flow(d1) % pnt_grid % wall_dist,  &
                                    flow(d2) % pnt_grid % wall_dist,  &
                                    d1, d2,                           &
@@ -53,35 +45,33 @@
     do d2 = 1, n_dom
 
       ! On the side of domain 1
-      ! (c1 and c2 here mean cell inside and on the boundary of domain 2)
+      ! (ic1 and bc1 here mean cell inside and on the boundary of domain 2)
       do n1 = 1, inter(d1, d2) % n1_sub
         n   = inter(d1, d2) % face_1(n1)   ! interface index
         ic1 = inter(d1, d2) % cell_1(n1)   ! domain 1, cell inside the domain
         bc1 = inter(d1, d2) % bcel_1(n1)   ! domain 1, cell on the boundary
         t1  = flow(d1) % t % n(ic1)                 ! temperature in dom 1
         k1  = flow(d1) % conductivity(ic1)          ! conductivity in dom 1
-        wd1 = flow(d1) % pnt_grid % wall_dist(ic1)  ! wall distance in dom 1 1
+        wd1 = flow(d1) % pnt_grid % wall_dist(ic1)  ! wall distance in dom 1
         t2  = inter(d1, d2) % phi_2(n, T)           ! temperature in dom 2
         k2  = inter(d1, d2) % phi_2(n, K)           ! conductivity in dom 2
         wd2 = inter(d1, d2) % phi_2(n, WD)          ! wall distance in dom 2
-! WRITE(100 + d1, *) t1, t2, k1, k2, wd1, wd2
         flow(d1) % t % n(bc1) = (t1 * k1 / wd1 + t2 * k2 / wd2)  &
                               / (     k1 / wd1 +      k2 / wd2)
       end do
 
       ! On the side of domain 2
-      ! (c1 and c2 here mean cell inside and on the boundary of domain 2)
+      ! (ic2 and bc2 here mean cell inside and on the boundary of domain 2)
       do n2 = 1, inter(d1, d2) % n2_sub
         n   = inter(d1, d2) % face_2(n2)   ! interface index
         ic2 = inter(d1, d2) % cell_2(n2)   ! domain 1, cell inside the domain
         bc2 = inter(d1, d2) % bcel_2(n2)   ! domain 1, cell on the boundary
-        t2  = flow(d2) % t % n(ic2)                 ! temperature in dom 1
-        k2  = flow(d2) % conductivity(ic2)          ! conductivity in dom 1
-        wd2 = flow(d2) % pnt_grid % wall_dist(ic2)  ! wall distance in dom 1 1
-        t1  = inter(d1, d2) % phi_1(n, T)           ! temperature in dom 2
-        k1  = inter(d1, d2) % phi_1(n, K)           ! conductivity in dom 2
-        wd1 = inter(d1, d2) % phi_1(n, WD)          ! wall distance in dom 2
-! WRITE(100 + d2, *) t1, t2, k1, k2, wd1, wd2
+        t2  = flow(d2) % t % n(ic2)                 ! temperature in dom 2
+        k2  = flow(d2) % conductivity(ic2)          ! conductivity in dom 2
+        wd2 = flow(d2) % pnt_grid % wall_dist(ic2)  ! wall distance in dom 2
+        t1  = inter(d1, d2) % phi_1(n, T)           ! temperature in dom 1
+        k1  = inter(d1, d2) % phi_1(n, K)           ! conductivity in dom 1
+        wd1 = inter(d1, d2) % phi_1(n, WD)          ! wall distance in dom 1
         flow(d2) % t % n(bc2) = (t1 * k1 / wd1 + t2 * k2 / wd2)  &
                               / (     k1 / wd1 +      k2 / wd2)
       end do
