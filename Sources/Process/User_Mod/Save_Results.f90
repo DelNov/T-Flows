b0c1c9779ca1c185991d8c9bc78f36caf17f6b26
Niceno <bojan.niceno.scientist@gmail.com>
2020-05-18 16:08:19 +0200

    Recovered to the default state.

--- Sources/Process/User_Mod/Save_Results.f90
+++ Sources/Process/User_Mod/Save_Results.f90
@@ -1,1 +1,30 @@
-../../../Tests/Swarm/Channel_Re_Tau_180/User_Mod/Save_Results.f90
+!==============================================================================!
+  subroutine User_Mod_Save_Results(flow, turb, mult, swarm, ts)
+!------------------------------------------------------------------------------!
+!   This subroutine is called each                                             !
+!     RESULTS_SAVE_INTERVAL (set in conrtol file),                             !
+!     at the end of a simulation                                               !
+!     and after 'save_now' command                                             !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  integer                       :: ts
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type), pointer :: grid
+  type(Bulk_Type), pointer :: bulk
+  type(Var_Type),  pointer :: u, v, w, t
+!==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
+  bulk => flow % bulk
+  u    => flow % u
+  v    => flow % v
+  w    => flow % w
+  t    => flow % t
+
+  end subroutine
--- Sources/Process/User_Mod/Save_Swarm.f90
+++
@@ -1,1 +0,0 @@
-../../../Tests/Swarm/Channel_Re_Tau_180/User_Mod/Save_Swarm.f90
