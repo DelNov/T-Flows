922068cfc0594fc72b4c0eecf539d68dd5b765df
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-01 12:31:05 +0200

    Passes initial times for statistics.

--- Sources/Process/User_Mod/End_Of_Time_Step.f90
+++ Sources/Process/User_Mod/End_Of_Time_Step.f90
@@ -1,5 +1,6 @@
 !==============================================================================!
-  subroutine User_Mod_End_Of_Time_Step(flow, turb, mult, swarm, n, time)
+  subroutine User_Mod_End_Of_Time_Step(flow, turb, mult, swarm,  &
+                                       n, n_stat_t, n_stat_p, time)
 !------------------------------------------------------------------------------!
 !   This function is called at the end of time step.                           !
 !------------------------------------------------------------------------------!
@@ -9,8 +10,10 @@
   type(Turb_Type),       target :: turb
   type(Multiphase_Type), target :: mult
   type(Swarm_Type),      target :: swarm
-  integer                       :: n     ! time step
-  real                          :: time  ! physical time
+  integer                       :: n         ! time step
+  integer                       :: n_stat_t  ! start time step for turb. stat.
+  integer                       :: n_stat_p  ! start time step for swarm. stat.
+  real                          :: time      ! physical time
 !==============================================================================!
 
   end subroutine
