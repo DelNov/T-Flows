6507192c6ad9cb693c501bbe263077a357dbd1b4
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-06 12:21:38 +0200

    Shortened some variable names.

--- Sources/Process/Swarm_Mod/Print_Statistics.f90
+++ Sources/Process/Swarm_Mod/Print_Statistics.f90
@@ -9,67 +9,72 @@
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type),     pointer :: grid
   type(Particle_Type), pointer :: part
-  integer                      :: k
-  real                         :: avg_part_cfl, avg_part_re, avg_part_st
-  real                         :: max_part_cfl, max_part_re, max_part_st
+  integer                      :: k, n_dep, n_ref, n_esc
+  real                         :: avg_cfl, avg_re, avg_st
+  real                         :: max_cfl, max_re, max_st
 !==============================================================================!
 
   ! Take aliases for the swarm
   grid => swarm % pnt_grid
 
-  !-----------------------------------!
-  !   Print some data on the screen   !
-  !-----------------------------------!
-  avg_part_cfl = 0
-  avg_part_re  = 0
-  avg_part_st  = 0
-  max_part_cfl = -HUGE
-  max_part_re  = -HUGE
-  max_part_st  = -HUGE
+  !----------------------------------------------!
+  !   Sum and average data over all processors   !
+  !----------------------------------------------!
+  avg_cfl = 0
+  avg_re  = 0
+  avg_st  = 0
+  max_cfl = -HUGE
+  max_re  = -HUGE
+  max_st  = -HUGE
   do k = 1, swarm % n_particles
     part => swarm % particle(k)
     if(part % proc .eq. this_proc) then
-      avg_part_cfl = avg_part_cfl + part % cfl
-      avg_part_re  = avg_part_re  + part % re
-      avg_part_st  = avg_part_st  + part % st
-      max_part_cfl = max(max_part_cfl, part % cfl)
-      max_part_re  = max(max_part_re,  part % re)
-      max_part_st  = max(max_part_st,  part % st)
+      avg_cfl = avg_cfl + part % cfl
+      avg_re  = avg_re  + part % re
+      avg_st  = avg_st  + part % st
+      max_cfl = max(max_cfl, part % cfl)
+      max_re  = max(max_re,  part % re)
+      max_st  = max(max_st,  part % st)
     end if
   end do
-  call Comm_Mod_Global_Sum_Real(avg_part_cfl)
-  call Comm_Mod_Global_Sum_Real(avg_part_st)
-  call Comm_Mod_Global_Sum_Real(avg_part_re)
-  call Comm_Mod_Global_Max_Real(max_part_cfl)
-  call Comm_Mod_Global_Max_Real(max_part_re)
-  call Comm_Mod_Global_Max_Real(max_part_st)
-  avg_part_cfl = avg_part_cfl / swarm % n_particles
-  avg_part_re  = avg_part_re  / swarm % n_particles
-  avg_part_st  = avg_part_st  / swarm % n_particles
+  call Comm_Mod_Global_Sum_Real(avg_cfl)
+  call Comm_Mod_Global_Sum_Real(avg_st)
+  call Comm_Mod_Global_Sum_Real(avg_re)
+  call Comm_Mod_Global_Max_Real(max_cfl)
+  call Comm_Mod_Global_Max_Real(max_re)
+  call Comm_Mod_Global_Max_Real(max_st)
+  avg_cfl = avg_cfl / swarm % n_particles
+  avg_re  = avg_re  / swarm % n_particles
+  avg_st  = avg_st  / swarm % n_particles
+
+  n_dep = nint(sum(swarm % n_deposited(:)))
+  n_esc = nint(sum(swarm % n_escaped(:)))
+  n_ref = nint(sum(swarm % n_reflected(:)))
+  call Comm_Mod_Global_Sum_Int(n_dep)
+  call Comm_Mod_Global_Sum_Int(n_esc)
+  call Comm_Mod_Global_Sum_Int(n_ref)
+
+  !-----------------------------------!
+  !   Print some data on the screen   !
+  !-----------------------------------!
   if(this_proc < 2) then
     write(*,'(a)') ' #================================================#'
     write(*,'(a)') ' #                   Swarm statistics'
     write(*,'(a)') ' #------------------------------------------------#'
     write(*,'(a,i6)') ' #  Total number of particles     : ',  &
-                       swarm % n_particles
+                      swarm % n_particles
     write(*,'(a,i6)') ' #  Number of active particles    : ',  &
-                      swarm % n_particles - nint(sum(swarm % n_deposited(:)))
-    write(*,'(a,i6)') ' #  Number of deposited particles : ',  &
-                      nint(sum(swarm % n_deposited(:)))
-    write(*,'(a,i6)') ' #  Number of escaped particles   : ',  &
-                      nint(sum(swarm % n_escaped(:)))
-    write(*,'(a,1pe12.1)') ' #  Total number of reflections   : ',  &
-                      sum(swarm % n_reflected(:))
+                      swarm % n_particles - n_dep - n_esc
+    write(*,'(a,i6)')      ' #  Number of deposited particles : ',  n_dep
+    write(*,'(a,i6)')      ' #  Number of escaped particles   : ',  n_esc
+    write(*,'(a,1pe12.1)') ' #  Total number of reflections   : ',  real(n_ref)
     write(*,'(a)') ' #================================================#'
     write(*,'(a)') ' #     Characteristic non-dimensional numbers     #'
     write(*,'(a)') ' #          (average and maximum values)          #'
     write(*,'(a)') ' #------------------------------------------------#'
-    write(*,'(a,2(1pe11.3))') ' #    Courant number   :',  &
-                          avg_part_cfl, max_part_cfl
-    write(*,'(a,2(1pe11.3))') ' #    Reynolds number  :',  &
-                          avg_part_re,  max_part_re
-    write(*,'(a,2(1pe11.3))') ' #    Stokes number    :',  &
-                          avg_part_st,  max_part_st
+    write(*,'(a,2(1pe11.3))') ' #    Courant number   :',  avg_cfl, max_cfl
+    write(*,'(a,2(1pe11.3))') ' #    Reynolds number  :',  avg_re,  max_re
+    write(*,'(a,2(1pe11.3))') ' #    Stokes number    :',  avg_st,  max_st
     write(*,'(a)') ' #------------------------------------------------#'
   end if
 
