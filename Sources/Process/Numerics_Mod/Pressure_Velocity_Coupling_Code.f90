7d2feb4fe50457b2bcb0f37fa976ce3485bb1649
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-14 12:43:17 +0200

    Renamed the function.

---
+++ Sources/Process/Numerics_Mod/Pressure_Velocity_Coupling_Code.f90
@@ -0,0 +1,27 @@
+!==============================================================================!
+  integer function Numerics_Mod_Pressure_Velocity_Coupling_Code(name)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  character(len=80) :: name
+!==============================================================================!
+
+  select case(name)
+
+    case('SIMPLE')
+      Numerics_Mod_Pressure_Velocity_Coupling_Code = SIMPLE
+
+    case('PISO')
+      Numerics_Mod_Pressure_Velocity_Coupling_Code = PISO
+
+    case default
+      if(this_proc < 2) then
+        print *, '# ERROR!  Unknown pressure-velocity coupling algorithm: ',  &
+                 trim(name)
+        print *, '# Exiting!'
+      end if
+      call Comm_Mod_End
+
+  end select
+
+  end function
