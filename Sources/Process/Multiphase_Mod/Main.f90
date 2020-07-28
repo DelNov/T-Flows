51f057c835fdc0cb8b56be48050d9c84a7d4d167
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-28 15:44:50 +0200

    Small steps for front tracking.

--- Sources/Process/Multiphase_Mod/Main.f90
+++ Sources/Process/Multiphase_Mod/Main.f90
@@ -18,24 +18,20 @@
     flow % m_flux % o(1:) = flow % m_flux % n(1:)  &
                              / flow % density_f(1:)
 
-    ! Update the values at boundaries
-    call Update_Boundary_Values(flow, turb, mult)  ! nema veze
-    call Multiphase_Mod_Compute_Vof(mult, sol, flow % dt, n)
-    call Field_Mod_Body_Forces(flow)
-    call Multiphase_Averaging(flow, mult, mult % vof)
+    ! Front tracking is not engaged
+    if(.not. mult % track_front) then
+      call Update_Boundary_Values(flow, turb, mult)
+      call Multiphase_Mod_Compute_Vof(mult, sol, flow % dt, n)
+      call Field_Mod_Body_Forces(flow)
+      call Multiphase_Averaging(flow, mult, mult % vof)
 
-    if(mult % track_front) then
-    call Surf_Mod_Allocate(mult % surf, flow)
-    call Surf_Mod_Place_At_Var_Value(mult % surf,  &
-                                     mult % vof,   &
-                                     sol,          &
-                                     0.5,          &
-                                     .true.)  ! don't print messages
-    call Surf_Mod_Calculate_Curvatures_From_Elems(mult % surf)
-    call Surf_Mod_Compute_Distance_Function_And_Vof(mult % surf,       &
-                                                    mult % dist_func,  &
-                                                    mult % vof)
+    ! Front tracking is engaged
+    else
+      call Update_Boundary_Values(flow, turb, mult)
+    ! call Surf_Mod_Advance_Front(mult, flow % dt, n) not implemented yet
+      call Field_Mod_Body_Forces(flow)
     end if
+
   else
     flow % m_flux % o(1:) = flow % m_flux % n(1:)
 
