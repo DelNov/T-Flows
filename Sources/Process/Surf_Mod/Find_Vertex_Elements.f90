6f495bdde504b125d0fa93cbe6a3562bba5108ec
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-28 11:36:09 +0200

    Added procedure to find vertex's elements (Important for front tracking)

---
+++ Sources/Process/Surf_Mod/Find_Vertex_Elements.f90
@@ -0,0 +1,45 @@
+!==============================================================================!
+  subroutine Surf_Mod_Find_Vertex_Elements(surf)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Surf_Type), target :: surf
+!-----------------------------------[Locals]-----------------------------------!
+  type(Vert_Type), pointer :: vert(:)
+  type(Elem_Type), pointer :: elem(:)
+  integer,         pointer :: nv, ne
+  integer                  :: e, v, i, j, k
+!==============================================================================!
+
+  ! Take aliases
+  nv   => surf % n_verts
+  ne   => surf % n_elems
+  vert => surf % vert
+  elem => surf % elem
+
+  call Surf_Mod_Count_Vertex_Elements(surf)
+
+  ! (Re)allocate memory for elements around each vertex
+  do v = 1, nv
+    if(allocated(vert(v) % vert_e)) then
+      if(size(vert(v) % vert_e) .ne. vert(v) % nne) then
+        deallocate(vert(v) % vert_e)
+        allocate(vert(v) % vert_e(vert(v) % nne))
+      end if
+    else
+      allocate(vert(v) % vert_e(vert(v) % nne))
+    end if
+  end do
+
+  ! Store elements around each vertex
+  vert(1:nv) % nne = 0
+  do e = 1, ne
+    i = elem(e) % i
+    j = elem(e) % j
+    k = elem(e) % k
+    vert(i) % nne = vert(i) % nne + 1;  vert(i) % vert_e(vert(i) % nne) = e
+    vert(j) % nne = vert(j) % nne + 1;  vert(j) % vert_e(vert(j) % nne) = e
+    vert(k) % nne = vert(k) % nne + 1;  vert(k) % vert_e(vert(k) % nne) = e
+  end do
+
+  end subroutine
