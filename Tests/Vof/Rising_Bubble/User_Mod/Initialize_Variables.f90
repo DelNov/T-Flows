96ae8741666e1384246b996cfd5be4470647a5ad
Niceno <bojan.niceno.scientist@gmail.com>
2020-07-28 15:49:30 +0200

    Updated!

--- Tests/Vof/Rising_Bubble/User_Mod/Initialize_Variables.f90
+++ Tests/Vof/Rising_Bubble/User_Mod/Initialize_Variables.f90
@@ -10,7 +10,7 @@ include '../User_Mod/Convert_Problem_Name_To_Integer.f90'
 include '../User_Mod/Vof_Area_Square_Circle.f90'
 include '../User_Mod/Vof_Exact_Cylinder.f90'
 !==============================================================================!
-  subroutine User_Mod_Initialize_Variables(flow, turb, mult, swarm)
+  subroutine User_Mod_Initialize_Variables(flow, turb, mult, swarm, sol)
 !------------------------------------------------------------------------------!
 !   Case-dependent initialization of VOF variable.                             !
 !------------------------------------------------------------------------------!
@@ -20,6 +20,7 @@ include '../User_Mod/Vof_Exact_Cylinder.f90'
   type(Turb_Type),       target :: turb
   type(Multiphase_Type), target :: mult
   type(Swarm_Type),      target :: swarm
+  type(Solver_Type),     target :: sol
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type),  pointer :: grid
   type(Var_Type),   pointer :: vof
@@ -35,10 +36,12 @@ include '../User_Mod/Vof_Exact_Cylinder.f90'
   dt    => flow % dt
   vof_f => mult % vof_f
 
+  !---------------------------------!
+  !   Initialize the VOF function   !
+  !---------------------------------!
+
   ! Initialize the whole domain as 0.0
-  do c = 1, grid % n_cells
-    vof % n(c) = 0.0
-  end do
+  vof % n(:) = 0.0
 
   ! Under a Plane:
   ! call Vof_Initialization_Plane(mult)
@@ -47,12 +50,22 @@ include '../User_Mod/Vof_Exact_Cylinder.f90'
   ! Cylinder:
   !call Vof_Initialization_Cylinder(mult)
 
-  call Grid_Mod_Exchange_Cells_Real(grid, vof % n)
-  ! call Grid_Mod_Exchange_Real(grid, vof % n)
+  ! Naive way to update bounary values
+  do s = 1, grid % n_bnd_cells
+    c1 = grid % faces_c(1, s)
+    c2 = grid % faces_c(2, s)
+    if(c2 < 0) then
+      vof % n(c2) = vof % n(c1)
+    end if
+  end do
 
-  ! Old value
+  ! Update buffer values
+  call Grid_Mod_Exchange_Cells_Real(grid, vof % n)
+
+  ! Set old values to be the same as new ones
   vof % o(:) = vof % n(:)
 
+  ! Initialize vof functions at faces
   do s = 1, grid % n_bnd_cells
     c1 = grid % faces_c(1,s)
     c2 = grid % faces_c(2,s)
@@ -70,4 +83,21 @@ include '../User_Mod/Vof_Exact_Cylinder.f90'
     fs = grid % f(s)
     vof_f(s) = fs * vof % n(c1) + (1.0 - fs) * vof % n(c2)
   end do
+
+  !--------------------------------!
+  !   Initialize front if needed   !
+  !--------------------------------!
+  if(mult % track_front) then
+    call Surf_Mod_Allocate(mult % surf, flow)
+    call Surf_Mod_Place_At_Var_Value(mult % surf,  &
+                                     mult % vof,   &
+                                     sol,          &
+                                     0.5,          &
+                                     .true.)  ! don't print messages
+    call Surf_Mod_Calculate_Curvatures_From_Elems(mult % surf)
+    call Surf_Mod_Compute_Distance_Function_And_Vof(mult % surf,       &
+                                                    mult % dist_func,  &
+                                                    mult % vof)
+  end if
+
   end subroutine
