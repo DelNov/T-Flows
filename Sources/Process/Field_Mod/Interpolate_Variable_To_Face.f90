84e47524f066f9b7198f861b3ee248495855bc06
Niceno <bojan.niceno.scientist@gmail.com>
2020-08-27 07:08:37 +0200

    Moved face interpolation from Var_Mod to Field_Mod (None of them was tested yet)

--- Sources/Process/Field_Mod/Interpolate_Variable_To_Face.f90
+++ Sources/Process/Field_Mod/Interpolate_Variable_To_Face.f90
@@ -1,19 +1,20 @@
 !==============================================================================!
-  real function Var_Mod_Face_Interpolation(phi, s)
+  real function Field_Mod_Interpolate_Variable_To_Face(flow, phi, s)
 !------------------------------------------------------------------------------!
 !   Computes cell-face value by gradient interpolation.                        !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Var_Type) :: phi
-  integer        :: s
+  type(Field_Type) :: flow
+  type(Var_Type)   :: phi
+  integer          :: s
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type), pointer :: grid
   real                     :: w1, w2
   integer                  :: c1, c2
 !==============================================================================!
 
-  grid => phi % pnt_grid
+  grid => flow % pnt_grid
 
   c1 = grid % faces_c(1,s)
   c2 = grid % faces_c(2,s)
@@ -21,7 +22,7 @@
   w1 = grid % f(s)
   w2 = 1.0 - w1
 
-  Var_Mod_Face_Interpolation =                             &
+  Field_Mod_Interpolate_Variable_To_Face =                 &
       phi % n(c1) * w1 + phi % n(c2) * w2                  &
    + (phi % x(c1) * w1 + phi % x(c2) * w2) * grid % xr(s)  &
    + (phi % y(c1) * w1 + phi % y(c2) * w2) * grid % yr(s)  &
