606d2b7a76d27f0f04d81668fcf9d4389edf6763
Niceno <bojan.niceno.scientist@gmail.com>
2020-09-04 14:33:17 +0200

    Added a case for spurious velocities. (It works with PISO and SIMPLE algorithms.)

---
+++ Tests/Vof/Spurious/User_Mod/End_Of_Time_Step.f90
@@ -0,0 +1,174 @@
+!==============================================================================!
+  subroutine User_Mod_End_Of_Time_Step(flow, turb, mult, swarm, n,    &
+                                       n_stat_t, n_stat_p, time)
+!------------------------------------------------------------------------------!
+!     This function computes position of contact line and high of droplet      !
+!----------------------------------[Modules]-----------------------------------!
+  use Work_Mod, only: sum_v1  => r_cell_01,   &
+                      dist_ck => r_Cell_02,   &
+                      dist_cn => r_Cell_02,   &
+                      dist_n  => i_cell_01
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  integer                       :: n     ! time step
+  integer                       :: n_stat_t, n_stat_p
+  real                          :: time  ! physical time
+!--------------------------------[Locals]--------------------------------------!
+  type(Grid_Type), pointer :: grid
+  type(Var_Type),  pointer :: vof
+  integer                  :: s, c, c1, c2, last_cell, fu, n_tot_cells, c_dist
+  real                     :: pos_mcl, h_drop !position mcl, droplet height
+  real                     :: vol_wall_bot, vol_symm !volume of boundaries
+  ! RMS u, area of droplet, area outside droplet
+  ! pressure inside and outside, area of droplet
+  real                     :: u_rms, a_in, a_out, p_in, p_out, a_vof
+  real                     :: u_res, u_max, p_max, p_min
+  real                     :: min_vfrac, max_vfrac
+  real                     :: L_dom, H_dom ! width and jeight of domain
+  real                     :: epsloc, norm_res
+  real                     :: x0(3), x1(3), x2(3)
+!==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
+  vof  => mult % vof
+
+  epsloc = epsilon(epsloc)
+
+  u_rms = 0.0
+  a_in  = 0.0
+  a_out = 0.0
+  p_in  = 0.0
+  p_out = 0.0
+  a_vof = 0.0
+
+  ! Find max and min vfractions, to limit pressure calculation:
+
+  min_vfrac = minval(vof % n(1:grid % n_cells - grid % comm % n_buff_cells))
+  max_vfrac = maxval(vof % n(1:grid % n_cells - grid % comm % n_buff_cells))
+
+  call Comm_Mod_Global_Min_Real(min_vfrac)
+  call Comm_Mod_Global_Max_Real(max_vfrac)
+
+  n_tot_cells = grid % n_cells - grid % comm % n_buff_cells
+
+  do c = 1, grid % n_cells - grid % comm % n_buff_cells
+    u_res = sqrt( flow % u % n(c) ** 2       &
+                + flow % v % n(c) ** 2       &
+                + flow % w % n(c) ** 2)
+    sum_v1(c) = u_res
+    u_rms = u_rms + u_res ** 2.0
+
+    if (abs(max_vfrac - vof % n(c)) < epsloc) then
+      a_in = a_in + grid % vol(c)
+      p_in = p_in + flow % p % n(c) * grid % vol(c)
+    end if
+
+    if (abs(vof % n(c)) < min_vfrac + epsloc) then
+      a_out = a_out + grid % vol(c)
+      p_out = p_out + flow % p % n(c) * grid % vol(c)
+    end if
+
+    a_vof = a_vof + grid % vol(c) * vof % n(c)
+
+  end do
+
+  call Comm_Mod_Global_Sum_Real(a_vof)
+
+  call Comm_Mod_Global_Sum_Int(n_tot_cells)
+  call Comm_Mod_Global_Sum_Real(u_rms)
+  u_rms = sqrt(1.0 / real(n_tot_cells) * u_rms)
+
+  u_max = maxval(sum_v1(1:grid % n_cells - grid % comm % n_buff_cells))
+  call Comm_Mod_Global_Max_Real(u_max)
+
+  p_max = maxval(flow % p % n(1:grid % n_cells - grid % comm % n_buff_cells))
+  p_min = minval(flow % p % n(1:grid % n_cells - grid % comm % n_buff_cells))
+  call Comm_Mod_Global_Max_Real(p_max)
+  call Comm_Mod_Global_Min_Real(p_min)
+
+  call Comm_Mod_Global_Sum_Real(p_in)
+  call Comm_Mod_Global_Sum_Real(p_out)
+  call Comm_Mod_Global_Sum_Real(a_in)
+  call Comm_Mod_Global_Sum_Real(a_out)
+
+  p_in = p_in / a_in
+  p_out = p_out / a_out
+
+  ! Write Results
+  if (this_proc < 2) then
+    call File_Mod_Append_File_For_Writing('spurious.dat', fu)
+    write(fu,'(6(2X,E16.10E2))')  time, a_vof, u_rms, u_max, &
+                                  p_max-p_min, p_in-p_out
+    close(fu)
+  end if
+
+  !! Calculate distance to center of cylinder
+  !! For normals for curvature
+  !if(this_proc <= 2) then
+  !  dist_n = 0
+  !  do c = 1, grid % n_cells
+  !    norm_res = norm2((/vof % x(c), vof % y(c), vof % z(c)/))
+  !    if(norm_res > epsloc) then
+  !      x0 = (/1.0, grid % yc(c), 1.0/)
+  !      x1 = (/grid % xc(c), grid % yc(c), grid % zc(c)/)
+  !      x2 = x1 + 10.0 * (/mult % fc_x(c), mult % fc_y(c), mult % fc_z(c)/) &
+  !                       / norm2((/mult % fc_x(c),   &
+  !                                 mult % fc_y(c),   &
+  !                                 mult % fc_z(c)/))
+  !      dist_ck(c) = norm2( Math_Mod_Cross_Product(x0 - x1, x0 - x2)    &
+  !                        / norm2(x2-x1) )
+
+  !      dist_n(c) = 1
+  !    end if
+  !  end do
+
+  !  call File_Mod_Open_File_For_Writing('dist_centk.dat', fu)
+
+  !  do c = 1, grid % n_cells
+  !    if(dist_n(c) == 1) then
+  !      write(fu,'(4(2X,E16.10E2))') grid % xc(c), grid % yc(c), grid % zc(c), &
+  !                                   dist_ck(c)
+  !    end if
+  !  end do
+
+  !  close(fu)
+
+  !! Calculate distance to center of cylinder
+  !! For normals for normals
+  !  dist_n = 0
+  !  do c = 1, grid % n_cells
+  !    norm_res = norm2((/vof % x(c), vof % y(c), vof % z(c)/))
+  !    if(norm_res > epsloc) then
+  !      x0 = (/1.0, grid % yc(c), 1.0/)
+  !      x1 = (/grid % xc(c), grid % yc(c), grid % zc(c)/)
+
+  !      x2 = x1 + 10.0 * (/mult % fs_x(c), mult % fs_y(c), mult % fs_z(c)/) &
+  !                       / norm2((/mult % fs_x(c),   &
+  !                                 mult % fs_y(c),   &
+  !                                 mult % fs_z(c)/))
+
+  !      dist_cn(c) = norm2( Math_Mod_Cross_Product(x0 - x1, x0 - x2)    &
+  !                        / norm2(x2-x1) )
+  !      dist_n(c) = 1
+  !    end if
+  !  end do
+
+  !  call File_Mod_Open_File_For_Writing('dist_centn.dat', fu)
+
+  !  do c = 1, grid % n_cells
+  !    if(dist_n(c) == 1) then
+  !      write(fu,'(4(2X,E16.10E2))') grid % xc(c), grid % yc(c), grid % zc(c), &
+  !                                   dist_cn(c)
+  !    end if
+  !  end do
+
+  !  close(fu)
+  !end if
+
+  end subroutine
---
+++ Tests/Vof/Spurious/User_Mod/Initialize_Variables.f90
@@ -0,0 +1,67 @@
+include '../User_Mod/Vof_Initialization_Cylinder.f90'
+include '../User_Mod/Vof_Exact_Cylinder.f90'
+include '../User_Mod/Vof_Area_Square_Circle.f90'
+
+!==============================================================================!
+  subroutine User_Mod_Initialize_Variables(flow, turb, mult, swarm, sol)
+!------------------------------------------------------------------------------!
+!   Case-dependent initialization of VOF variable.                             !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  type(Solver_Type),     target :: sol
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type),  pointer :: grid
+  type(Var_Type),   pointer :: vof
+  real,             pointer :: dt
+  real,             pointer :: vof_f(:)
+  integer                   :: c, c1, c2, s, i_probe, c_inters, n, code
+  integer                   :: i, j
+  real                      :: fs, min_dist, dist, glo_dist, epsloc
+!==============================================================================!
+
+  ! Take aliases
+  grid  => flow % pnt_grid
+  vof   => mult % vof
+  dt    => flow % dt
+
+  epsloc = epsilon(epsloc)
+
+  ! Initialize the whole domain as 0.0
+  do c = 1, grid % n_cells
+    vof % n(c) = 0.0
+  end do
+
+  ! Under a Plane:
+  ! call Vof_Initialization_Plane(mult)
+  ! Ellipsoid:
+  !call Vof_Initialization_Ellipsoid(mult)
+  ! Cylinder:
+  call Vof_Initialization_Cylinder(mult)
+  ! Box:
+  !call Vof_Initialization_Box(mult)
+
+  call Grid_Mod_Exchange_Cells_Real(grid, vof % n)
+
+  ! Old value
+  vof % o(:) = vof % n(:)
+
+  ! At faces
+
+  ! At boundaries
+  do s = 1, grid % n_bnd_faces
+    c1 = grid % faces_c(1,s)
+    c2 = grid % faces_c(2,s)
+    if(Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. OUTFLOW) then
+      vof % n(c2) = vof % n(c1)
+    else if(Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. INFLOW) then
+    else
+      vof % n(c2) = vof % n(c1)
+    end if
+  end do
+
+  end subroutine
