1e2e37f1b06d9622912dc09f381f6f403af257e2
Niceno <bojan.niceno.scientist@gmail.com>
2020-11-24 17:41:21 +0100

    Added Fluent's format to recognize here.

--- Sources/Convert/Guess_Format.f90
+++ Sources/Convert/Guess_Format.f90
@@ -12,7 +12,7 @@
   character(SL), intent(in)  :: file_name
   character(SL), intent(out) :: file_format
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: c, n, s, l, p, fu
+  integer :: l, fu
 !==============================================================================!
 
   !----------------------------!
@@ -23,15 +23,35 @@
   ! Open the file as it is
   call File_Mod_Open_File_For_Reading(file_name, fu)
 
-  !--------------------------------------------!
-  !   Check if it is Gmsh's .msh file format   !
-  !--------------------------------------------!
+  !-----------------------------------------!
+  !   Check if it is Fluent's file format   !
+  !-----------------------------------------!
+  if(file_format .eq. 'UNKNOWN') then
+    rewind(fu)
+    do l = 1, 64
+      call File_Mod_Read_Line(fu)
+      if(line % tokens(1) .eq. '(10') then
+        file_format = 'FLUENT'
+        print *, '#================================='   // &
+                 '=================================='
+        print *, '# Based on the contents, you are'     // &
+                 ' reading Fluent''s .msh file format'
+        print *, '#---------------------------------'   // &
+                 '----------------------------------'
+        exit
+      end if
+    end do
+  end if
+
+  !---------------------------------------!
+  !   Check if it is Gmsh's file format   !
+  !---------------------------------------!
   if(file_format .eq. 'UNKNOWN') then
     rewind(fu)
     do l = 1, 64
       call File_Mod_Read_Line(fu)
       if(line % tokens(1) .eq. '$MeshFormat') then
-        file_format = 'GMSH_MSH'
+        file_format = 'GMSH'
         print *, '#================================='   // &
                  '=================================='
         print *, '# Based on the contents, you are'     // &
@@ -43,9 +63,9 @@
     end do
   end if
 
-  !----------------------------------------------!
-  !   Check if it is Gambit's .neu file format   !
-  !----------------------------------------------!
+  !-----------------------------------------!
+  !   Check if it is Gambit's file format   !
+  !-----------------------------------------!
   if(file_format .eq. 'UNKNOWN') then
     rewind(fu)
     do l = 1, 64
@@ -53,7 +73,7 @@
       if(line % tokens(1) .eq. 'NUMNP' .and.  &
          line % tokens(2) .eq. 'NELEM' .and.  &
          line % tokens(3) .eq. 'NGRPS') then
-        file_format = 'GAMBIT_NEU'
+        file_format = 'GAMBIT'
         print *, '#================================='   // &
                  '=================================='
         print *, '# Based on the contents, you are'     // &
