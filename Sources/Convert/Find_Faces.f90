26556f3b80f3cd0118cd0010ab5bf6a29eb820e9
Niceno <bojan.niceno.scientist@gmail.com>
2020-11-25 18:54:04 +0100

    IMPORTANT: Find_Faces doesn't compare real arrays!

--- Sources/Convert/Find_Faces.f90
+++ Sources/Convert/Find_Faces.f90
@@ -22,21 +22,24 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Grid_Type) :: grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer                 :: c, c1, c2, n1, n2, n3, f_nod(4), n_f_nod
-  integer                 :: n_match, j, match_nodes(-1:8) 
-  integer                 :: i1, i2, k, cnt
-  integer                 :: fn(6,4)
-  real,    allocatable    :: face_coor(:)
-  integer, allocatable    :: face_cell(:), starts(:), ends(:)
-  real                    :: very_big
+  integer              :: c, c1, c2, n1, n2, n3, f_nod(4)
+  integer              :: n_match, i_fac, match_nodes(-1:8)
+  integer              :: i1, i2, i_nod, cnt
+  integer              :: fn(6,4)
+  integer, allocatable :: face_n1(:)
+  integer, allocatable :: face_n2(:)
+  integer, allocatable :: face_n3(:)
+  integer, allocatable :: face_cell(:), starts(:), ends(:)
 !------------------------------------------------------------------------------!
   include 'Cell_Numbering_Neu.f90'
 !==============================================================================!
 
-  very_big = max(real(grid % n_nodes), real(grid % n_cells))
-
-  allocate(face_coor(grid % n_cells*6))
-  face_coor = real(grid % n_nodes) * HUGE
+  allocate(face_n1(grid % n_cells*6))
+  allocate(face_n2(grid % n_cells*6))
+  allocate(face_n3(grid % n_cells*6))
+  face_n1(:) = HUGE_INT
+  face_n2(:) = HUGE_INT
+  face_n3(:) = HUGE_INT
 
   allocate(face_cell(grid % n_cells*6));  face_cell(:) = 0
   allocate(starts   (grid % n_cells*6));  starts   (:) = 0
@@ -50,39 +53,38 @@
     if(grid % cells_n_nodes(c) .eq. 5) fn = neu_pyr
     if(grid % cells_n_nodes(c) .eq. 6) fn = neu_wed
     if(grid % cells_n_nodes(c) .eq. 8) fn = neu_hex
-    do j = 1, 6
-      if(grid % cells_bnd_color(j,c) .eq. 0) then
+    do i_fac = 1, 6
+      if(grid % cells_bnd_color(i_fac, c) .eq. 0) then
 
-        n_f_nod = 0
-        f_nod = -1
-        do k = 1, 4
-          if(fn(j,k) > 0) then
-            f_nod(k) = grid % cells_n(fn(j,k), c)
-            n_f_nod = n_f_nod + 1
+        ! Fetch face nodes (-1 becomes HUGE_INT and it will be the biggest)
+        do i_nod = 1, 4
+          f_nod(i_nod) = HUGE_INT
+          if(fn(i_fac, i_nod) > 0) then
+            f_nod(i_nod) = grid % cells_n(fn(i_fac, i_nod), c)
           end if
         end do
 
-        if( n_f_nod >  0 ) then
-          if(f_nod(4) > 0) then
-            face_coor((c-1)*6+j) =   &
-               very_big * real(max(f_nod(1), f_nod(2), f_nod(3), f_nod(4)))   &
-            +             real(min(f_nod(1), f_nod(2), f_nod(3), f_nod(4)))
-          else
-            face_coor((c-1)*6+j) =   &
-              very_big * real(max(f_nod(1), f_nod(2), f_nod(3)))   &
-           +             real(min(f_nod(1), f_nod(2), f_nod(3)))
-           end if
-          face_cell((c-1)*6+j) = c
-        end if 
+        ! Sort them ...
+        call Sort_Mod_Int(f_nod(1:4))
+
+        ! ... and store three of the smallest
+        face_n1((c-1)*6 + i_fac) = f_nod(1)
+        face_n2((c-1)*6 + i_fac) = f_nod(2)
+        face_n3((c-1)*6 + i_fac) = f_nod(3)
+
+        ! Store the cell too
+        face_cell((c-1)*6 + i_fac) = c
       end if
     end do
   end do
 
   !--------------------------------------------------!
-  !   Sort the cell faces according to coordinares   !
+  !   Sort the cell faces according to coordinates   !
   !--------------------------------------------------!
-  call Sort_Mod_Real_Carry_Int(face_coor(1:grid % n_cells*6),  & 
-                               face_cell(1:grid % n_cells*6))
+  call Sort_Mod_3_Int_Carry_Int(face_n1(1:grid % n_cells*6),    &
+                                face_n2(1:grid % n_cells*6),    &
+                                face_n3(1:grid % n_cells*6),    &
+                                face_cell(1:grid % n_cells*6))
 
   !------------------------------------------------!
   !   Anotate cell faces with same coordinates     !
@@ -92,18 +94,20 @@
   cnt = 1
   starts(1) = 1
   do c = 2, grid % n_cells * 6
-    if( face_coor(c) .ne. face_coor(c-1) ) then
+    if( face_n1(c) .ne. face_n1(c-1) .and.  &
+        face_n2(c) .ne. face_n2(c-1) .and.  &
+        face_n3(c) .ne. face_n3(c-1) ) then
       cnt = cnt + 1
       starts(cnt) = c
       ends(cnt-1) = c-1
     end if
   end do
 
-  !-------------------------------------------!
-  !                                           !
-  !   Main loop to fill the SideC structure   !
-  !                                           !
-  !-------------------------------------------!
+  !---------------------------------------------!
+  !                                             !
+  !   Main loop to fill the faces_c structure   !
+  !                                             !
+  !---------------------------------------------!
   do n3 = 1, cnt
     if(starts(n3) .ne. ends(n3)) then
       do i1=starts(n3),ends(n3)
@@ -135,23 +139,23 @@
               if(grid % cells_n_nodes(c1) .eq. 5) fn = neu_pyr
               if(grid % cells_n_nodes(c1) .eq. 6) fn = neu_wed
               if(grid % cells_n_nodes(c1) .eq. 8) fn = neu_hex
-              do j = 1, 6
-                if(grid % cells_c(j, c1) .eq. 0  .and.   & ! not set yet
-                    ( max( match_nodes(fn(j,1)),0 ) + &
-                      max( match_nodes(fn(j,2)),0 ) + &
-                      max( match_nodes(fn(j,3)),0 ) + &
-                      max( match_nodes(fn(j,4)),0 ) .eq. n_match ) ) then
+              do i_fac = 1, 6
+                if(grid % cells_c(i_fac, c1) .eq. 0  .and.   & ! not set yet
+                    ( max( match_nodes(fn(i_fac, 1)),0 ) + &
+                      max( match_nodes(fn(i_fac, 2)),0 ) + &
+                      max( match_nodes(fn(i_fac, 3)),0 ) + &
+                      max( match_nodes(fn(i_fac, 4)),0 ) .eq. n_match ) ) then
                   grid % n_faces = grid % n_faces + 1
                   grid % faces_c(1,grid % n_faces) = c1
                   grid % faces_c(2,grid % n_faces) = c2
-                  grid % faces_n_nodes(grid % n_faces) = n_match 
-                  do k = 1, 4
-                    if(fn(j,k) > 0) then
-                      grid % faces_n(k,grid % n_faces) =  &
-                      grid % cells_n(fn(j,k), c1)
+                  grid % faces_n_nodes(grid % n_faces) = n_match
+                  do i_nod = 1, 4
+                    if(fn(i_fac, i_nod) > 0) then
+                      grid % faces_n(i_nod,grid % n_faces) =  &
+                      grid % cells_n(fn(i_fac, i_nod), c1)
                     end if
                   end do
-                  grid % cells_c(j, c1) = 1  ! -> means: set
+                  grid % cells_c(i_fac, c1) = 1  ! -> means: set
                 end if
               end do
             end if   ! n_match .ne. 2
@@ -162,5 +166,13 @@
   end do    ! do n3
 
   print '(a38,i9)', '# Number of faces:                   ', grid % n_faces
+
+  ! De-allocate local memory
+  deallocate(face_n1)
+  deallocate(face_n2)
+  deallocate(face_n3)
+  deallocate(face_cell)
+  deallocate(starts)
+  deallocate(ends)
 
   end subroutine
