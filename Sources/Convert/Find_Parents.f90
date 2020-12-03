e654cb32fa3e14bff36376a0bc9bd6da8b63990c
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-03 05:12:21 +0100

    SUPER IMPORTANT: New algorithm for Find_Parents! The old one was actually not working at all for tetrahedral grids.

--- Sources/Convert/Find_Parents.f90
+++ Sources/Convert/Find_Parents.f90
@@ -10,12 +10,13 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Grid_Type) :: grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer              :: fn(6,4), i_fac, i_nod, c1, c2, bc
-  integer              :: n_match
-  integer              :: n_bnd_proj   ! bnd. cells projected from cells
+  integer              :: fn(6,4), i_fac, j_fac, i_nod, i_cel, c1, c2
+  integer              :: nodes(4), n_match, dir, bc, cnt_c, cnt_f
   integer              :: n_face_nodes ! number of nodes in a face
   integer              :: n_cell_faces ! number of faces in a cell
   logical, allocatable :: is_node_bnd(:)
+  integer, allocatable :: cell_near_bnd(:)
+  integer, allocatable :: cr1(:), cr2(:), cr3(:), w1(:), w2(:)  ! for sorting
 !------------------------------------------------------------------------------!
   include 'Cells_Faces_Nodes.f90'
 !==============================================================================!
@@ -36,24 +37,84 @@
     stop
   end if
 
-  allocate(is_node_bnd(grid % n_nodes))
+  allocate(is_node_bnd(grid % n_nodes));    is_node_bnd(:)   = .false.
+  allocate(cell_near_bnd(grid % n_cells));  cell_near_bnd(:) = 0
 
-  n_bnd_proj = 0
+  !--------------------------------------------------------------!
+  !   Find all cells near boundary and allocate working arrays   !
+  !--------------------------------------------------------------!
+
+  ! Mark all nodes on the boundary
+  is_node_bnd(:) = .false.
+  do c2 = -grid % n_bnd_cells, -1
+    if( grid % bnd_cond % color(c2) .gt. 0 ) then
+      do i_nod = 1, grid % cells_n_nodes(c2)  ! 3 or 4
+        is_node_bnd( grid % cells_n(i_nod, c2) ) = .true.
+      end do
+    end if
+  end do
+
+  ! Count cells near boundaries
+  cnt_c = 0
+  cnt_f = grid % n_bnd_cells
+  do c1 = 1, grid % n_cells
+    do i_nod = 1, grid % cells_n_nodes(c1)  ! 4 to 8
+      if( is_node_bnd(grid % cells_n(i_nod, c1)) ) then
+        cnt_c = cnt_c + 1
+        cell_near_bnd(cnt_c) = c1
+        if(grid % cells_n_nodes(c1) .eq. 4) cnt_f = cnt_f + 4  ! tet
+        if(grid % cells_n_nodes(c1) .eq. 5) cnt_f = cnt_f + 5  ! pyr
+        if(grid % cells_n_nodes(c1) .eq. 6) cnt_f = cnt_f + 5  ! wed
+        if(grid % cells_n_nodes(c1) .eq. 8) cnt_f = cnt_f + 6  ! hex
+        goto 1
+      end if
+    end do
+1   continue
+  end do
+  PRINT *, '# Number of all cells near boundary:          ', cnt_c
+  PRINT *, '# Number of all cells''s faces near boundary: ', cnt_f
+
+  allocate(cr1(cnt_f));  cr1(:) = 0
+  allocate(cr2(cnt_f));  cr2(:) = 0
+  allocate(cr3(cnt_f));  cr3(:) = 0
+  allocate(w1 (cnt_f));  w1 (:) = 0
+  allocate(w2 (cnt_f));  w2 (:) = 0
+
+  !----------------------!
+  !   Real work begins   !
+  !----------------------!
+
   do bc = 1, grid % n_bnd_cond
 
-    ! Re-initialize the logical array
-    is_node_bnd(:) = .false.
+    cr1(:) = HUGE_INT
+    cr2(:) = HUGE_INT
+    cr3(:) = HUGE_INT
+    w1 (:) = HUGE_INT
+    w2 (:) = HUGE_INT
 
-    ! Mark boundary nodes in this section
+    cnt_f = 0
     do c2 = -grid % n_bnd_cells, -1
       if( grid % bnd_cond % color(c2) .eq. bc ) then
+
+        ! Increase total face count
+        cnt_f = cnt_f + 1
+
+        ! Form and store local nodes list
+        nodes(:) = HUGE_INT
         do i_nod = 1, grid % cells_n_nodes(c2)  ! 3 or 4
-          is_node_bnd( grid % cells_n(i_nod, c2) ) = .true.
+          nodes(i_nod) = grid % cells_n(i_nod, c2)
         end do
+        call Sort_Mod_Int(nodes(1:4))
+        cr1(cnt_f) = nodes(1)
+        cr2(cnt_f) = nodes(2)
+        cr3(cnt_f) = nodes(3)
+        w1 (cnt_f) = c2        ! not sure this will be needed, but OK
+        w2 (cnt_f) = bc        ! not sure this will be needed, but OK
       end if
     end do
 
-    do c1 = 1, grid % n_cells
+    do i_cel = 1, cnt_c
+      c1 = cell_near_bnd(i_cel)
 
       ! Fetch the faces from this cell
       if(grid % cells_n_nodes(c1) .eq. 4) fn = tet
@@ -66,40 +127,70 @@
       if(grid % cells_n_nodes(c1) .eq. 5) n_cell_faces = 5
       if(grid % cells_n_nodes(c1) .eq. 6) n_cell_faces = 5
 
-      ! Browse through all possible faces
+      ! Browse through all possible faces and their nodes
       do i_fac = 1, n_cell_faces
-        n_match = 0
 
-        n_face_nodes = 4         ! assume it is a quad
+        ! Increase total face count
+        cnt_f = cnt_f + 1
+
+        ! Estimate number of nodes for this face
+        n_face_nodes = 4             ! assume it is a quad
         if( fn(i_fac, 4) < 0 ) then  ! nope, it was a triangle
           n_face_nodes = 3
         end if
 
+        ! Form and store local nodes list
+        nodes(:) = HUGE_INT
         do i_nod = 1, n_face_nodes
           if(fn(i_fac, i_nod) > 0) then  ! if this node exists
-            if( is_node_bnd( grid % cells_n(fn(i_fac, i_nod), c1) ) ) then
-              n_match = n_match + 1
-            end if
+            nodes(i_nod) = grid % cells_n(fn(i_fac, i_nod), c1)
           end if
-        end do ! i_nod
+        end do
+        call Sort_Mod_Int(nodes(1:4))
+        cr1(cnt_f) = nodes(1)
+        cr2(cnt_f) = nodes(2)
+        cr3(cnt_f) = nodes(3)
+        w1 (cnt_f) = c1
+        w2 (cnt_f) = i_fac
 
-        ! This face is matching
-        if(n_match .eq. n_face_nodes) then
-          grid % cells_bnd_color(i_fac, c1) = bc
-          n_bnd_proj = n_bnd_proj + 1
-        end if ! n_match .eq. n_face_nodes
+      end do  ! i_fac
 
-      end do ! i_fac
+    end do  ! c1
+
+    do i_fac = 1, cnt_f
+      write(100,'(99i9)') cr1(i_fac), cr2(i_fac), cr3(i_fac), w1(i_fac), w2(i_fac)
     end do
-  end do
+    call Sort_Mod_3_Int_Carry_2_Int(cr1(1:cnt_f), cr2(1:cnt_f), cr3(1:cnt_f),  &
+                                    w1(1:cnt_f), w2(1:cnt_f))
+    do i_fac = 1, cnt_f
+      write(200,'(99i9)') cr1(i_fac), cr2(i_fac), cr3(i_fac), w1(i_fac), w2(i_fac)
+    end do
 
-  !-------------------------------------------!
-  !   Update number of boundary cells         !
-  !   (This is probably not needed because    !
-  !   it is re-calculated in Grid_Topology)   !
-  !-------------------------------------------!
-  if(n_bnd_proj > grid % n_bnd_cells) then
-    grid % n_bnd_cells = n_bnd_proj
-  end if
+    n_match = 0
+    do i_fac = 1, cnt_f-1
+      j_fac = i_fac + 1
+      if( w1(i_fac) * w1(j_fac) < 0 ) then  ! they have different signs
+
+        ! Is it a match?
+        if( cr1(i_fac) .eq. cr1(j_fac) .and.  &
+            cr2(i_fac) .eq. cr2(j_fac) .and.  &
+            cr3(i_fac) .eq. cr3(j_fac) ) then
+
+          n_match = n_match + 1
+          if( w1(i_fac) > 0 ) then  ! i_fac stores internal cell
+            c1  = w1(i_fac)
+            dir = w2(i_fac)
+          else                      ! j_fac stores internal cell
+            c1  = w1(j_fac)
+            dir = w2(j_fac)
+          end if
+          grid % cells_bnd_color(dir, c1) = bc
+
+        end if
+      end if
+    end do
+    print *, '# Number of matches: ', n_match
+
+  end do  ! bc
 
   end subroutine
