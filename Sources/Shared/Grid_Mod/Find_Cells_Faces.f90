4284d04589265d3c6141b88e30135b5e268cbab3
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-07 12:07:39 +0100

    Added a non-critical warning.

--- Sources/Shared/Grid_Mod/Find_Cells_Faces.f90
+++ Sources/Shared/Grid_Mod/Find_Cells_Faces.f90
@@ -1,7 +1,7 @@
 !==============================================================================!
   subroutine Grid_Mod_Find_Cells_Faces(grid)
 !------------------------------------------------------------------------------!
-!   Find faces surrounding each cell; needed for VOF for now.                  !
+!   Find faces surrounding each cell; including boundary cells                 !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
@@ -10,22 +10,25 @@
   integer :: c1, c2, s   ! counters
 !==============================================================================!
 
-  grid % cells_n_faces = 0
+  if(any(grid % cells_n_faces(:) .ne. 0)) then
+    print *, '# WARNING: Seems you are looking for cells'' faces' //  &
+             ' although this information has already been found!'
+  end if
 
   !---------------------------------------!
   !   Browse through all faces to form:   !
   !        cells_n_faces, cells_f         !
   !---------------------------------------!
+  grid % cells_n_faces(:) = 0
   do s = 1, grid % n_faces
     c1 = grid % faces_c(1, s)
     c2 = grid % faces_c(2, s)
 
     grid % cells_n_faces(c1) = grid % cells_n_faces(c1) + 1
     grid % cells_f(grid % cells_n_faces(c1), c1) = s
-    if (c2 > 0) then
-      grid % cells_n_faces(c2) = grid % cells_n_faces(c2) + 1
-      grid % cells_f(grid % cells_n_faces(c2), c2) = s
-    end if
+
+    grid % cells_n_faces(c2) = grid % cells_n_faces(c2) + 1
+    grid % cells_f(grid % cells_n_faces(c2), c2) = s
   end do
 
   end subroutine
