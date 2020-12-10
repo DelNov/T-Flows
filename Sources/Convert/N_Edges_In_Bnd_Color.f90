e05926a7e299456bc4a49aad9eae937347e1b24c
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-10 15:37:51 +0100

    VERY GOOD: Found a way to handle concave faces! It works more or less like this: a node which makes a face concave (sometimes called concave nodes) is temporarily move into a convex position.  Nodes are ordered on such, convex, face and then the node is moved back into its concave position.  To help this being well represented in Paraview, the nodes are rotated (cshift) until the concave node is first. However, a modification in Calculate_Geometry was also needed to preserve such order when changing the sign of faces.
    Although this works fine when faces are plotted,
    cells are still sometimes messy.  I checked the way
    they are stored in cell-based and face-based .vtu
    files, and they are the same.  I reckon it is just
    a way Paraview deals with polyhedral cells.

--- Sources/Convert/N_Edges_In_Bnd_Color.f90
+++ Sources/Convert/N_Edges_In_Bnd_Color.f90
@@ -12,48 +12,108 @@
   integer         :: bc
   integer         :: edge_data(grid % n_edges)
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: e, cnt, s1, s2
-  real    :: n1(3), n2(3)
+  integer :: e, cnt, s1, s2, n1, n2
+  real    :: norm_1(3), norm_2(3)
+  real    :: xs, ys, zs, xe, ye, ze
+  real    :: vec_ef(3)  ! from edge to face mid-point
 !==============================================================================!
 
   ! Nullify on entry
   cnt = 0
   edge_data(:) = 0
 
+  !---------------------------------------------!
+  !                                             !
+  !   Fetch geometrically sharp edges (first)   !
+  !                                             !
+  !---------------------------------------------!
   do e = 1, grid % n_edges
 
-    ! Edge is on this BC
+    !-------------------------!
+    !   Edge is on this BC   !
+    !-------------------------
     if(grid % edges_bc(bc, e) .gt. 0) then
+    if(grid % edges_fb(1, e) .ne. 0 .and.  &
+       grid % edges_fb(2, e) .ne. 0) then
 
-      if(grid % edges_fb(1, e) .ne. 0 .and.  &
-         grid % edges_fb(2, e) .ne. 0) then
+      ! Take face indices
+      s1 = grid % edges_fb(1, e)
+      s2 = grid % edges_fb(2, e)
 
-        s1 = grid % edges_fb(1, e)
-        s2 = grid % edges_fb(2, e)
+      ! Compute normals of the two faces surrounding the edge
+      norm_1(1) = grid % sx(s1);  norm_2(1) = grid % sx(s2)
+      norm_1(2) = grid % sy(s1);  norm_2(2) = grid % sy(s2)
+      norm_1(3) = grid % sz(s1);  norm_2(3) = grid % sz(s2)
+      norm_1(1:3) = norm_1(1:3) / norm2(norm_1(1:3))
+      norm_2(1:3) = norm_2(1:3) / norm2(norm_2(1:3))
 
-        ! Compute normals of the two faces surrounding the edge
-        n1(1) = grid % sx(s1);  n1(2) = grid % sy(s1);  n1(3) = grid % sz(s1)
-        n2(1) = grid % sx(s2);  n2(2) = grid % sy(s2);  n2(3) = grid % sz(s2)
-        n1(1:3) = n1(1:3) / norm2(n1(1:3))
-        n2(1:3) = n2(1:3) / norm2(n2(1:3))
+      !----------------------------------------!
+      !   Edge is not sharp, mark it as zero   !
+      !----------------------------------------!
+      if(dot_product(norm_1, norm_2) >= 0.7071) then
+        edge_data(e) = 0
 
-        if(dot_product(n1(1:3), n2(1:3)) < 0.7071) then
-          cnt = cnt + 1
-          edge_data(e) = edge_data(e) + 1
-        end if
-      end if
-
-    end if
-
-    ! Edge is on this BC
-    if(grid % edges_bc(bc, e) .gt. 0) then
-
-      ! But there is more
-      if( sum(grid % edges_bc(1:grid % n_bnd_cond, e)) .gt. 1 ) then
+      !--------------------------------------------------!
+      !   Edge is geometrically sharp, still check if    !
+      !   is in convex or concave corner of a new face   !
+      !--------------------------------------------------!
+      else
         cnt = cnt + 1
-        edge_data(e) = edge_data(e) + 1
+
+        ! Calculate edges coordinates
+        n1 = grid % edges_n(1, e)
+        n2 = grid % edges_n(2, e)
+        xe = 0.5 * (grid % xn(n1) + grid % xn(n2))
+        ye = 0.5 * (grid % yn(n1) + grid % yn(n2))
+        ze = 0.5 * (grid % zn(n1) + grid % zn(n2))
+
+        ! Calculate midpoint between cell faces surrounding the edge
+        xs = 0.5 * (grid % xf(s1) + grid % xf(s2))
+        ys = 0.5 * (grid % yf(s1) + grid % yf(s2))
+        zs = 0.5 * (grid % zf(s1) + grid % zf(s2))
+
+        ! Vector connecting edge center with face mid-point
+        vec_ef(1) = xs - xe
+        vec_ef(2) = ys - ye
+        vec_ef(3) = zs - ze
+        vec_ef(1:3) = vec_ef(1:3) / norm2(vec_ef(1:3))  ! normalize it
+
+        ! Edge is convex
+        if(dot_product(vec_ef(1:3), norm_1(1:3)) < 0.0 .and.  &
+           dot_product(vec_ef(1:3), norm_2(1:3)) < 0.0) then
+          edge_data(e) = 1
+
+        ! Edge is concave
+        else if(dot_product(vec_ef(1:3), norm_1(1:3)) > 0.0 .and.  &
+                dot_product(vec_ef(1:3), norm_2(1:3)) > 0.0) then
+          edge_data(e) = -1
+
+        else
+          print '(A,99F12.3)', 'BAD', norm2(vec_ef(1:3)), norm_1, norm_2, dot_product(norm_1(1:3), norm_2(1:3))
+        end if
+
+      end if
+
+    end if  ! edge is between two boundary faces
+    end if  ! edge is in this boundary condition
+
+  end do  ! through edges
+
+
+  !------------------------!
+  !                        !
+  !   Edge is on this BC   !
+  !                        !
+  !------------------------!
+  do e = 1, grid % n_edges
+    if(grid % edges_bc(bc, e) .gt. 0) then
+    if( sum(grid % edges_bc(1:grid % n_bnd_cond, e)) .gt. 1 ) then
+      if(edge_data(e) .eq. 0) then  ! hasn't been marked yet
+        cnt = cnt + 1
+        edge_data(e) = 1
       end if
     end if
+    end if  ! it is in this bc
   end do
 
   ! Return a sum of all marked nodes
--- Sources/Convert/N_Sharp_Edges.f90
+++ Sources/Convert/N_Sharp_Edges.f90
@@ -12,8 +12,10 @@
   type(Grid_Type) :: grid
   integer         :: edge_data(grid % n_edges)
 !-----------------------------------[Locals]-----------------------------------!
-  integer :: e, cnt, s1, s2
+  integer :: e, cnt, s1, s2, n1, n2
   real    :: norm_1(3), norm_2(3)
+  real    :: xs, ys, zs, xe, ye, ze
+  real    :: vec_ef(3)  ! from edge to face mid-point
 !==============================================================================!
 
   ! Nullify on entry
@@ -21,15 +23,20 @@
   edge_data(:) = 0
 
   !---------------------------------------------!
+  !                                             !
   !   Fetch geometrically sharp edges (first)   !
+  !                                             !
   !---------------------------------------------!
   do e = 1, grid % n_edges
 
-    ! If edge is in-between two boundary faces
-    ! (yes, grid % edges_f stores only boundary faces, very bad name choice)
+    !--------------------------------------------------!
+    !   If edge is in-between two boundary faces       !
+    !   (grid % edges_fb stores only boundary faces)   !
+    !--------------------------------------------------!
     if(grid % edges_fb(1, e) .ne. 0 .and.  &
        grid % edges_fb(2, e) .ne. 0) then
 
+      ! Take face indices
       s1 = grid % edges_fb(1, e)
       s2 = grid % edges_fb(2, e)
 
@@ -40,23 +47,67 @@
       norm_1(1:3) = norm_1(1:3) / norm2(norm_1(1:3))
       norm_2(1:3) = norm_2(1:3) / norm2(norm_2(1:3))
 
-      ! If angle is less than 45 (135) degrees, or is simply sharp
-      if(dot_product(norm_1(1:3), norm_2(1:3)) < 0.7071) then
-        cnt = cnt + 1
-        edge_data(e) = edge_data(e) + 1
-      end if
-    end if
+      !----------------------------------------!
+      !   Edge is not sharp, mark it as zero   !
+      !----------------------------------------!
+      if(dot_product(norm_1, norm_2) >= 0.7071) then
+        edge_data(e) = 0
 
-  end do
+      !--------------------------------------------------!
+      !   Edge is geometrically sharp, still check if    !
+      !   is in convex or concave corner of a new face   !
+      !--------------------------------------------------!
+      else
+        cnt = cnt + 1
+
+        ! Calculate edges coordinates
+        n1 = grid % edges_n(1, e)
+        n2 = grid % edges_n(2, e)
+        xe = 0.5 * (grid % xn(n1) + grid % xn(n2))
+        ye = 0.5 * (grid % yn(n1) + grid % yn(n2))
+        ze = 0.5 * (grid % zn(n1) + grid % zn(n2))
+
+        ! Calculate midpoint between cell faces surrounding the edge
+        xs = 0.5 * (grid % xf(s1) + grid % xf(s2))
+        ys = 0.5 * (grid % yf(s1) + grid % yf(s2))
+        zs = 0.5 * (grid % zf(s1) + grid % zf(s2))
+
+        ! Vector connecting edge center with face mid-point
+        vec_ef(1) = xs - xe
+        vec_ef(2) = ys - ye
+        vec_ef(3) = zs - ze
+        vec_ef(1:3) = vec_ef(1:3) / norm2(vec_ef(1:3))  ! normalize it
+
+        ! Edge is convex
+        if(dot_product(vec_ef(1:3), norm_1(1:3)) < 0.0 .and.  &
+           dot_product(vec_ef(1:3), norm_2(1:3)) < 0.0) then
+          edge_data(e) = 1
+
+        ! Edge is concave
+        else if(dot_product(vec_ef(1:3), norm_1(1:3)) > 0.0 .and.  &
+                dot_product(vec_ef(1:3), norm_2(1:3)) > 0.0) then
+          edge_data(e) = -1
+
+        else
+          print '(A,99F12.3)', 'BAD', norm2(vec_ef(1:3)), norm_1, norm_2, dot_product(norm_1(1:3), norm_2(1:3))
+        end if
+
+      end if
+
+    end if  ! edge is between two boundary faces
+
+  end do  ! through edges
 
   !-------------------------------------------------------------------!
+  !                                                                   !
   !   But also fetch edges in-between different boundary conditions   !
+  !                                                                   !
   !-------------------------------------------------------------------!
   do e = 1, grid % n_edges
     if( sum(grid % edges_bc(1:grid % n_bnd_cond, e)) .gt. 1 ) then
       if(edge_data(e) .eq. 0) then  ! hasn't been marked yet
         cnt = cnt + 1
-        edge_data(e) = edge_data(e) + 1
+        edge_data(e) = 1
       end if
     end if
   end do
