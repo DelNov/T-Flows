542e80c6ad491860054c12f360ba292c3db83a39
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-17 16:22:32 +0100

    Recovered from erraneous commit and push :-/

--- Sources/Process/User_Mod/Beginning_Of_Time_Step.f90
+++ Sources/Process/User_Mod/Beginning_Of_Time_Step.f90
@@ -1,1 +1,23 @@
-../../../Tests/Communication/User_Mod/Beginning_Of_Time_Step.f90
+!==============================================================================!
+  subroutine User_Mod_Beginning_Of_Time_Step(flow, turb, mult, swarm, n, time)
+!------------------------------------------------------------------------------!
+!   This function is called at the beginning of time step.                     !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  integer, intent(in)           :: n     ! time step
+  real,    intent(in)           :: time  ! physical time
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type), pointer :: grid
+  type(Var_Type),  pointer :: u, v, w, t, phi, vof
+!==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
+
+  end subroutine
+
