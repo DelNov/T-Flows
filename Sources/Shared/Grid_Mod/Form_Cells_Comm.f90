c4c0db3e6d8390cd5db0f03402168249e8c217ad
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-18 20:33:59 +0100

    SUPER IMPORTANT: All new, shortened and revamped version of Form_Cells_Comm.  Has nothing to do with previous algorithm, works with any decomposition and should be as robust as they get.

--- Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
+++ Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
@@ -7,23 +7,19 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Grid_Type) :: grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: sub, i_nod, j_nod, ms, mr, n, ni, nj, sh
-  integer           :: c, c1, c2, s, n_buff_faces
-  real              :: xi, yi, zi, xj, yj, zj
-  real, allocatable :: send_cells(:), recv_cells(:)
+  integer              :: sub, ms, mr
+  integer              :: c, c1, c2, s, n_buff_faces, n_max_buff_cells, i_cel
+  integer, allocatable :: send_cells(:), recv_cells(:)
+  integer, allocatable :: buff_s(:,:), buff_r(:,:), need_cell(:,:)
 !==============================================================================!
 
   if(n_proc < 2) return
 
   ! Allocate memory for locally used arrays
+  allocate(buff_s(n_proc, n_proc)); buff_s(:,:) = 0
+  allocate(buff_r(n_proc, n_proc)); buff_r(:,:) = 0
   allocate(send_cells(-grid % n_bnd_cells:grid % n_cells))
   allocate(recv_cells(-grid % n_bnd_cells:grid % n_cells))
-
-  !--------------------------------------------------!
-  !   Allocate memory for send and receive buffers   !
-  !--------------------------------------------------!
-  allocate(grid % comm % cells_send(n_proc))
-  allocate(grid % comm % cells_recv(n_proc))
 
   !-------------------------------!
   !   Count buffer cells inside   !
@@ -48,114 +44,77 @@
     end if
   end do
 
-  allocate(grid % new_n(grid % n_nodes))
+  !----------------------------!
+  !   Form the buff_r matrix   !
+  !----------------------------!
+  do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
+         grid % n_cells
+    sub = grid % comm % cell_proc(c)
+    buff_r(this_proc, sub) = buff_r(this_proc, sub) + 1
+    recv_cells(c) = sub
+  end do
 
   !--------------------------------------------------!
-  !   Calculate size of buffers between subdomains   !
+  !   Allocate memory for matrix with needed cells   !
   !--------------------------------------------------!
+  n_max_buff_cells = grid % comm % n_buff_cells
+  call comm_mod_global_max_int(n_max_buff_cells)
+  allocate(need_cell(n_max_buff_cells, n_proc));  need_cell(:,:) = 0
+
+  !-------------------------------------------!
+  !   Store global number of cells you need   !
+  !-------------------------------------------!
+  i_cel = 0
+  do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
+         grid % n_cells
+    if(grid % comm % cell_proc(c) .ne. this_proc) then
+      i_cel = i_cel + 1
+      need_cell(i_cel, this_proc) = grid % comm % cell_glo(c)
+    end if
+  end do
+
+  !----------------------------------------------!
+  !   Inform all processors about needed cells   !
+  !----------------------------------------------!
+  need_cell = reshape(need_cell, (/n_max_buff_cells*n_proc, 1/))
+  call Comm_Mod_Global_Sum_Int_Array(n_max_buff_cells*n_proc, need_cell)
+  need_cell = reshape(need_cell, (/n_max_buff_cells, n_proc/))
+
+  !-----------------------------------------------------!
+  !   Identify cells you have to send, and count them   !
+  !-----------------------------------------------------!
+  do sub = 1, n_proc
+    if(sub .ne. this_proc) then
+      do i_cel = 1, n_max_buff_cells
+        do c = 1, grid % n_cells
+          if(grid % comm % cell_glo(c) .eq. need_cell(i_cel, sub)) then
+            send_cells(c) = sub                                  ! identify
+            buff_s(this_proc, sub) = buff_s(this_proc, sub) + 1  ! count
+          end if
+        end do
+      end do  ! i_cel
+    end if    ! sub .ne. this_proc
+  end do      ! sub
+
+  !--------------------------------------------------!
+  !   Allocate memory for send and receive buffers   !
+  !--------------------------------------------------!
+  allocate(grid % comm % cells_send(n_proc))
+  allocate(grid % comm % cells_recv(n_proc))
+
+  !-----------------------------------!
+  !   Form send and receive buffers   !
+  !-----------------------------------!
   do sub = 1, n_proc
 
     ! Initialize buffer size to zero
     grid % comm % cells_send(sub) % n_items = 0
     grid % comm % cells_recv(sub) % n_items = 0
 
-    ! Initialize send and recv cells
-    send_cells(:) = 0.0
-    recv_cells(:) = 0.0
-
     if(sub .ne. this_proc) then
 
-      ! Mark the nodes at subdomain interface
-      grid % new_n(:) = 0
-      do c = 1, grid % n_cells
-        if(grid % comm % cell_proc(c) .eq. sub) then
-          do i_nod = 1, abs(grid % cells_n_nodes(c))
-            grid % new_n(grid % cells_n(i_nod, c)) = -1
-          end do
-        end if
-      end do
-
-      ! Spread info to twin nodes
-      ! (This is not super elegant, but seems to work)
-      do s = 1, grid % n_faces
-        sh = grid % faces_s(s)
-        if(sh > 0) then
-          do i_nod = 1, grid % faces_n_nodes(s)
-            ni = grid % faces_n(i_nod, s)
-            if(grid % new_n(ni) .eq. -1) then
-              xi = grid % xn(ni)
-              yi = grid % yn(ni)
-              zi = grid % zn(ni)
-              do j_nod = 1, grid % faces_n_nodes(sh)
-                nj = grid % faces_n(j_nod, sh)
-                xj = grid % xn(nj) + grid % dx(sh)
-                yj = grid % yn(nj) + grid % dy(sh)
-                zj = grid % zn(nj) + grid % dz(sh)
-                if(Math_Mod_Distance(xi, yi, zi, xj, yj, zj) < NANO) then
-                  grid % new_n(nj) = -1
-                end if
-              end do
-            end if
-          end do
-          do j_nod = 1, grid % faces_n_nodes(sh)
-            nj = grid % faces_n(j_nod, sh)
-            if(grid % new_n(nj) .eq. -1) then
-              xj = grid % xn(nj) + grid % dx(sh)
-              yj = grid % yn(nj) + grid % dy(sh)
-              zj = grid % zn(nj) + grid % dz(sh)
-              do i_nod = 1, grid % faces_n_nodes(s)
-                ni = grid % faces_n(i_nod, s)
-                xi = grid % xn(ni)
-                yi = grid % yn(ni)
-                zi = grid % zn(ni)
-                if(Math_Mod_Distance(xi, yi, zi, xj, yj, zj) < NANO) then
-                  grid % new_n(ni) = -1
-                end if
-              end do
-            end if
-          end do
-        end if  ! sh > 0
-      end do
-
-      ! Count the boundary and inside cells in the buffers
-      ! Note #1: It works again with boundary cells, no idea
-      !          why it stopped in the first place.  One should,
-      !          however, always connect these lines with ones
-      !          below, in Note #2
-      ms = 0
-      mr = 0
-      do c = -grid % n_bnd_cells, grid % n_cells
-        if(grid % comm % cell_proc(c) .eq. this_proc) then
-          n = abs(grid % cells_n_nodes(c))
-          if( any( grid % new_n(grid % cells_n(1:n,c)) .eq. -1) ) then
-            ms = ms + 1
-            send_cells(c) = sub
-          end if
-        end if
-        if(grid % comm % cell_proc(c) .eq. sub) then
-          n = abs(grid % cells_n_nodes(c))
-          if( any( grid % new_n(grid % cells_n(1:n,c)) .eq. -1) ) then
-            mr = mr + 1
-            recv_cells(c) = sub
-          end if
-        end if
-      end do
-
-      ! Count buffer cells at boundaries by copying the inside cells
-      do s = 1, grid % n_faces
-        c1 = grid % faces_c(1,s)
-        c2 = grid % faces_c(2,s)
-        if(c2 < 0) then
-          if(send_cells(c1) > 0) then
-            ms = ms + 1
-            send_cells(c2) = send_cells(c1)
-          end if
-          if(recv_cells(c1) > 0) then
-            mr = mr + 1
-            recv_cells(c2) = recv_cells(c1)
-          end if
-        end if
-      end do
+      ms = buff_s(this_proc, sub)
+      mr = buff_r(this_proc, sub)
 
       if(ms > 0) then
         allocate(grid % comm % cells_send(sub) % map(ms));
@@ -170,20 +129,17 @@
         allocate(grid % comm % cells_recv(sub) % r_buff(mr));
       end if
 
-      ! Count the boundary and inside cells in the buffers
-      ! Note #2: It works again with boundary cells, no idea
-      !          why it stopped in the first place.  One should,
-      !          however, always connect these lines with ones
-      !          above, in Note #1
       ms = 0
       mr = 0
 
-      ! Store final buffer lengths
       do c = -grid % n_bnd_cells, grid % n_cells
         if(send_cells(c) .eq. sub) then
           ms = ms + 1
           grid % comm % cells_send(sub) % map(ms) = c
         end if
+      end do
+
+      do c = -grid % n_bnd_cells, grid % n_cells
         if(recv_cells(c) .eq. sub) then
           mr = mr + 1
           grid % comm % cells_recv(sub) % map(mr) = c
@@ -204,6 +160,8 @@
   grid % n_faces = grid % n_faces - n_buff_faces
 
   ! De-allocate locally used memory
+  deallocate(buff_s)
+  deallocate(buff_r)
   deallocate(send_cells)
   deallocate(recv_cells)
 
