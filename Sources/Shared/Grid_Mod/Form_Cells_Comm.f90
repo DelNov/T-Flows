0dcbf4f5d9657830607b4ad0d0aa101412c46913
Niceno <bojan.niceno.scientist@gmail.com>
2020-12-19 16:28:47 +0100

    Just fixed the revamped version of Form_Cells_Comm

--- Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
+++ Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
@@ -7,17 +7,19 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Grid_Type) :: grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer              :: sub, ms, mr
+  integer              :: sub, ms, mr, cnt
   integer              :: c, c1, c2, s, n_buff_faces, n_max_buff_cells, i_cel
   integer, allocatable :: send_cells(:), recv_cells(:)
-  integer, allocatable :: buff_s(:,:), buff_r(:,:), need_cell(:,:)
+  integer, allocatable :: buff_s(:,:), buff_r(:,:)
+  integer, allocatable :: need_cell(:,:), from_proc(:,:)
+  logical, parameter   :: DEBUG = .false.
 !==============================================================================!
 
   if(n_proc < 2) return
 
   ! Allocate memory for locally used arrays
-  allocate(buff_s(n_proc, n_proc)); buff_s(:,:) = 0
-  allocate(buff_r(n_proc, n_proc)); buff_r(:,:) = 0
+  allocate(buff_s(n_proc, n_proc))
+  allocate(buff_r(n_proc, n_proc))
   allocate(send_cells(-grid % n_bnd_cells:grid % n_cells))
   allocate(recv_cells(-grid % n_bnd_cells:grid % n_cells))
 
@@ -47,6 +49,7 @@
   !----------------------------!
   !   Form the buff_r matrix   !
   !----------------------------!
+  buff_r(:,:) = 0
   do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
          grid % n_cells
     sub = grid % comm % cell_proc(c)
@@ -54,15 +57,34 @@
     recv_cells(c) = sub
   end do
 
+  if(DEBUG) then
+    do sub=1, n_proc
+      if(sub .ne. this_proc) then
+        write(100*this_proc+sub, *) '#====================================' // &
+                                    '====================================='
+        write(100*this_proc+sub, '(a,i7,a,i7)')                   &
+              ' # There are        ', grid % comm % n_buff_cells,  &
+              ' buffer cells in processor ', this_proc
+        write(100*this_proc+sub, *) '#------------------------------------' // &
+                                    '-------------------------------------'
+        write(100*this_proc+sub, '(a,i7,a,i7)')               &
+              ' #   It needs       ', buff_r(this_proc, sub),  &
+              ' cells from processors     ', sub
+      end if
+    end do
+  end if
+
   !--------------------------------------------------!
   !   Allocate memory for matrix with needed cells   !
   !--------------------------------------------------!
   n_max_buff_cells = grid % comm % n_buff_cells
   call comm_mod_global_max_int(n_max_buff_cells)
   allocate(need_cell(n_max_buff_cells, n_proc));  need_cell(:,:) = 0
+  allocate(from_proc(n_max_buff_cells, n_proc));  from_proc(:,:) = 0
 
   !-------------------------------------------!
   !   Store global number of cells you need   !
+  !    and also from which processor it is    !
   !-------------------------------------------!
   i_cel = 0
   do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
@@ -70,31 +92,37 @@
     if(grid % comm % cell_proc(c) .ne. this_proc) then
       i_cel = i_cel + 1
       need_cell(i_cel, this_proc) = grid % comm % cell_glo(c)
+      from_proc(i_cel, this_proc) = grid % comm % cell_proc(c)
     end if
   end do
 
   !----------------------------------------------!
   !   Inform all processors about needed cells   !
+  !   and from which processor are they needed   !
   !----------------------------------------------!
   need_cell = reshape(need_cell, (/n_max_buff_cells*n_proc, 1/))
+  from_proc = reshape(from_proc, (/n_max_buff_cells*n_proc, 1/))
   call Comm_Mod_Global_Sum_Int_Array(n_max_buff_cells*n_proc, need_cell)
+  call Comm_Mod_Global_Sum_Int_Array(n_max_buff_cells*n_proc, from_proc)
   need_cell = reshape(need_cell, (/n_max_buff_cells, n_proc/))
+  from_proc = reshape(from_proc, (/n_max_buff_cells, n_proc/))
 
-  !-----------------------------------------------------!
-  !   Identify cells you have to send, and count them   !
-  !-----------------------------------------------------!
+  !--------------------------------------------------------------!
+  !   Form buff_s from the information in the matrix from_proc   !
+  !--------------------------------------------------------------!
+  buff_s(:,:) = 0
   do sub = 1, n_proc
     if(sub .ne. this_proc) then
       do i_cel = 1, n_max_buff_cells
-        do c = 1, grid % n_cells
-          if(grid % comm % cell_glo(c) .eq. need_cell(i_cel, sub)) then
-            send_cells(c) = sub                                  ! identify
-            buff_s(this_proc, sub) = buff_s(this_proc, sub) + 1  ! count
-          end if
-        end do
-      end do  ! i_cel
-    end if    ! sub .ne. this_proc
-  end do      ! sub
+        if(from_proc(i_cel, sub) .eq. this_proc) then
+          buff_s(this_proc, sub) = buff_s(this_proc, sub) + 1  ! count
+        end if
+      end do
+      if(DEBUG) write(100*this_proc + sub, '(2(a,i7))')                &
+                      ' #   It should send ', buff_s(this_proc, sub),  &
+                      ' cells to processor        ', sub
+    end if
+  end do
 
   !--------------------------------------------------!
   !   Allocate memory for send and receive buffers   !
@@ -103,7 +131,9 @@
   allocate(grid % comm % cells_recv(n_proc))
 
   !-----------------------------------!
+  !                                   !
   !   Form send and receive buffers   !
+  !                                   !
   !-----------------------------------!
   do sub = 1, n_proc
 
@@ -113,6 +143,9 @@
 
     if(sub .ne. this_proc) then
 
+      !---------------------------------!
+      !   Allocate memory for buffers   !
+      !---------------------------------!
       ms = buff_s(this_proc, sub)
       mr = buff_r(this_proc, sub)
 
@@ -122,6 +155,7 @@
         allocate(grid % comm % cells_send(sub) % l_buff(ms));
         allocate(grid % comm % cells_send(sub) % r_buff(ms));
       end if
+
       if(mr > 0) then
         allocate(grid % comm % cells_recv(sub) % map(mr));
         allocate(grid % comm % cells_recv(sub) % i_buff(mr));
@@ -129,9 +163,30 @@
         allocate(grid % comm % cells_recv(sub) % r_buff(mr));
       end if
 
+      !------------------------------------------!
+      !   Form send_cells from the information   !
+      !   in matrices from_proc and need_cell    !
+      !------------------------------------------!
       ms = 0
       mr = 0
 
+      cnt = 0
+      do i_cel = 1, n_max_buff_cells
+        if(from_proc(i_cel, sub) .eq. this_proc) then
+          do c = 1, grid % n_cells
+            if(grid % comm % cell_glo(c) .eq. need_cell(i_cel, sub)) then
+              send_cells(c) = sub                                  ! identify
+              cnt = cnt + 1
+            end if
+          end do
+
+        end if
+      end do
+      if(DEBUG) write(100*this_proc + sub, '(2(a,i7))')    &
+                      ' #   It did find    ', cnt,    &
+                      ' cells to send to processor', sub
+
+      ! This worries me.  Why should this be from -grid % n_bnd_cells or not
       do c = -grid % n_bnd_cells, grid % n_cells
         if(send_cells(c) .eq. sub) then
           ms = ms + 1
@@ -139,12 +194,18 @@
         end if
       end do
 
+      ! This worries me.  Why should this be from -grid % n_bnd_cells or not
       do c = -grid % n_bnd_cells, grid % n_cells
         if(recv_cells(c) .eq. sub) then
           mr = mr + 1
           grid % comm % cells_recv(sub) % map(mr) = c
         end if
       end do
+
+      if(DEBUG) then
+        write(100*this_proc+sub, '(a,i0.0,a,i0.0,a,i0.0,a,i0.0)')  &
+              ' #   send/recv (', this_proc, '/', sub, ') =  ', ms, ' / ', mr
+      end if
 
       ! Store final buffer lengths
       grid % comm % cells_send(sub) % n_items = ms
