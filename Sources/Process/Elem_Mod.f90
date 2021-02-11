3d93784d1b5ef1c57bf3649d15ce0aee0a8fe5c9
Niceno <bojan.niceno.scientist@gmail.com>
2021-02-11 19:27:11 +0100

    Added three more fields for sx, sy and sz, these are surface areas in Elem_Type.

--- Sources/Process/Elem_Mod.f90
+++ Sources/Process/Elem_Mod.f90
@@ -12,12 +12,15 @@
   type Elem_Type
 
     integer :: nne         ! number of neighbouring element
+    integer :: nv          ! number of vertices for the element
+    integer :: ns          ! number of sides for the element
     integer :: v(24)
     integer :: e(24)
     integer :: s(24)
     real    :: nx, ny, nz  ! surface normal vector
     real    :: xc, yc, zc  ! center of a sphere
     real    :: xe, ye, ze  ! center of element
+    real    :: sx, sy, sz  ! surface area components
     real    :: area
     real    :: curv
 
