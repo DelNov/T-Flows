4cf07be62fcd3051d2c737259cc52e3b06f7c807
Niceno <bojan.niceno.scientist@gmail.com>
2021-02-11 09:22:06 +0100

    Coppied Surf_Mod to Front_Mod.  (Ideally, it would be done with inheritance, but I am not sure if Fortran can handle it yet.)  In any case, this commit is the first version of Front_Mod which is working the same as Surf_Mod did.  After this point, simplificiation will follow, and probably some additions needed for phase change simulations.

---
+++ Sources/Process/Front_Mod.f90
@@ -0,0 +1,88 @@
+!==============================================================================!
+  module Front_Mod
+!------------------------------------------------------------------------------!
+!   Module for Lagrangian particle tracking                                    !
+!------------------------------------------------------------------------------!
+!----------------------------------[Modules]-----------------------------------!
+  use Field_Mod
+  use Vert_Mod
+  use Elem_Mod
+  use Side_Mod
+!------------------------------------------------------------------------------!
+  implicit none
+!==============================================================================!
+
+  !--------------------------------!
+  !   A few important parameters   !
+  !--------------------------------!
+  integer, parameter   :: MAX_ELEMENT_VERTICES =      6
+  integer, parameter   :: MAX_SURFACE_VERTICES = 131072
+  integer, parameter   :: MAX_SURFACE_ELEMENTS = 131072
+
+  !----------------!
+  !   Front type   !
+  !----------------!
+  type Front_Type
+
+    type(Grid_Type),  pointer :: pnt_grid  ! grid for which it is defined
+    type(Field_Type), pointer :: pnt_flow  ! flow field for which it is defined
+
+    integer                      :: n_elems
+    integer                      :: n_verts
+    integer                      :: n_sides
+    type(Vert_Type), allocatable :: vert(:)
+    type(Elem_Type), allocatable :: elem(:)
+    type(Side_Type), allocatable :: side(:)
+
+    ! Logical array if cell has particles
+    logical, allocatable :: cell_has_vertex(:)
+
+    ! Working arrays, buffers for parallel version
+    ! (Keyword "parameter: not allowed inside a type
+    ! declaration. One might think of making a function)
+    integer :: N_I_VARS = 3
+    integer :: N_L_VARS = 2
+    integer :: N_R_VARS = 8
+  end type
+
+  integer, allocatable :: i_work(:)
+  logical, allocatable :: l_work(:)
+  real,    allocatable :: r_work(:)
+
+  contains
+
+! include 'Front_Mod/Advance_Vertices.f90'
+  include 'Front_Mod/Allocate.f90'
+  include 'Front_Mod/Calculate_Element_Centroids.f90'
+  include 'Front_Mod/Calculate_Element_Normals.f90'
+  include 'Front_Mod/Clean.f90'
+  include 'Front_Mod/Count_Elements_Neighbours.f90'
+  include 'Front_Mod/Count_Vertex_Elements.f90'
+  include 'Front_Mod/Compress_Vertices.f90'
+  include 'Front_Mod/Compute_Distance_Function_And_Vof.f90'
+  include 'Front_Mod/Calculate_Curvatures_From_Edges.f90'
+  include 'Front_Mod/Calculate_Curvatures_From_Elems.f90'
+  include 'Front_Mod/Calculate_Curvatures_From_Verts.f90'
+  include 'Front_Mod/Find_Boundaries.f90'
+  include 'Front_Mod/Find_Sides.f90'
+  include 'Front_Mod/Find_Nearest_Cell.f90'
+  include 'Front_Mod/Find_Nearest_Node.f90'
+  include 'Front_Mod/Find_Vertex_Elements.f90'
+  include 'Front_Mod/Handle_3_Points.f90'
+  include 'Front_Mod/Handle_4_Points.f90'
+  include 'Front_Mod/Handle_5_Points.f90'
+  include 'Front_Mod/Handle_6_Points.f90'
+  include 'Front_Mod/Place_At_Var_Value.f90'
+  include 'Front_Mod/Print_Statistics.f90'
+  include 'Front_Mod/Refine.f90'
+  include 'Front_Mod/Relax_Geometry.f90'
+  include 'Front_Mod/Relax_Topology.f90'
+  include 'Front_Mod/Split_Element.f90'
+  include 'Front_Mod/Swap_Side.f90'
+  include 'Front_Mod/Smooth.f90'
+! include 'Front_Mod/Bounce_Particle.f90'
+! include 'Front_Mod/Check_Periodicity.f90'
+! include 'Front_Mod/Exchange_Particles.f90'
+! include 'Front_Mod/Move_Particle.f90'
+
+  end module
