403e895736a36330c66b4de55cf5ee75dfaa9ef7
Niceno <bojan.niceno.scientist@gmail.com>
2021-02-18 11:29:55 +0100

    Corrected links in User_Mod which were checked in by mistake
    typechange: User_Mod/End_Of_Time_Step.f90
    typechange: User_Mod/Initialize_Variables.f90

--- Sources/Process/User_Mod/Initialize_Variables.f90
+++ Sources/Process/User_Mod/Initialize_Variables.f90
@@ -1,1 +1,21 @@
-../../../Tests/Vof/Spurious_La_12000/User_Mod/Initialize_Variables.f90
+!==============================================================================!
+  subroutine User_Mod_Initialize_Variables(flow, turb, mult, swarm, sol)
+!------------------------------------------------------------------------------!
+!   User initialization of dependent variables.                                !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: flow
+  type(Turb_Type),       target :: turb
+  type(Multiphase_Type), target :: mult
+  type(Swarm_Type),      target :: swarm
+  type(Solver_Type),     target :: sol
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type), pointer :: grid
+  type(Var_Type),  pointer :: u, v, w, t, phi, vof
+!==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
+
+  end subroutine
