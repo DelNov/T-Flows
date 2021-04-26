674d72c760799ac95504315c8e48ac19acb3f305
Niceno <bojan.niceno.scientist@gmail.com>
2021-04-26 18:35:36 +0200

    Removed a big chunk of the function which was ... ... some fiddling of mine for cases with mass transfer. It was very messy and eventually didn't even work.
    modified:   Correct_Velocity.f90

--- Sources/Process/Correct_Velocity.f90
+++ Sources/Process/Correct_Velocity.f90
@@ -51,78 +51,10 @@
   !-----------------------------------------!
 
   ! Normal correction
-  if(.not. flow % mass_transfer) then
-    do c = 1, grid % n_cells
-      u % n(c) = u % n(c) - pp % x(c) * grid % vol(c) / m % sav(c)
-      v % n(c) = v % n(c) - pp % y(c) * grid % vol(c) / m % sav(c)
-      w % n(c) = w % n(c) - pp % z(c) * grid % vol(c) / m % sav(c)
-    end do
-
-  ! In case with mass transfer, dodge cells at the surface
-  else
-    do c = 1, grid % n_cells
-      if(mult % cell_at_elem(c) .eq. 0) then
-        u % n(c) = u % n(c) - pp % x(c) * grid % vol(c) / m % sav(c)
-        v % n(c) = v % n(c) - pp % y(c) * grid % vol(c) / m % sav(c)
-        w % n(c) = w % n(c) - pp % z(c) * grid % vol(c) / m % sav(c)
-      end if
-    end do
-
-    call Multiphase_Mod_Vof_Grad_Variable_With_Jump(mult, flow % u)
-    call Multiphase_Mod_Vof_Grad_Variable_With_Jump(mult, flow % v)
-    call Multiphase_Mod_Vof_Grad_Variable_With_Jump(mult, flow % w)
-
-    do c = 1, grid % n_cells
-      if(mult % cell_at_elem(c) .ne. 0) then
-
-        u % n(c) = 0.0
-        v % n(c) = 0.0
-        w % n(c) = 0.0
-
-        ! Browse through faces of the current cell
-        do i_fac = 1, grid % cells_n_faces(c)
-          s = grid % cells_f(i_fac, c)
-          c1 = grid % faces_c(1,s)
-          c2 = grid % faces_c(2,s)
-
-          if(c2 > 0) then
-
-            ! Cell 2 is at the front
-            if(mult % cell_at_elem(c2) .ne. 0 .and.  &
-               mult % cell_at_elem(c1) .eq. 0) then
-              wght = (1.0 - abs(mult % vof % n(c1) - mult % vof % n(c2)))
-              u % n(c2) = u % n(c2) + wght * u % n(c1)
-              v % n(c2) = v % n(c2) + wght * v % n(c1)
-              w % n(c2) = w % n(c2) + wght * w % n(c1)
-            end if
-
-            ! Cell 1 is at the front
-            if(mult % cell_at_elem(c1) .ne. 0 .and.  &
-               mult % cell_at_elem(c2) .eq. 0) then
-              wght = (1.0 - abs(mult % vof % n(c1) - mult % vof % n(c2)))
-              u % n(c1) = u % n(c1) + wght * u % n(c2)
-              v % n(c1) = v % n(c1) + wght * v % n(c2)
-              w % n(c1) = w % n(c1) + wght * w % n(c2)
-            end if
-
-          end if
-
-        end do
-      end if  ! cell at elem
-    end do    ! cells
-  end if      ! if flow % mass_transfer
-
-  do s = 1, grid % n_faces
-    c1 = grid % faces_c(1,s)
-    c2 = grid % faces_c(2,s)
-
-    if(c2  < 0) then
-      if( (Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. PRESSURE) ) then
-        u % n(c2) = u % n(c1)
-        v % n(c2) = v % n(c1)
-        w % n(c2) = w % n(c1)
-      end if
-    end if
+  do c = 1, grid % n_cells
+    u % n(c) = u % n(c) - pp % x(c) * grid % vol(c) / m % sav(c)
+    v % n(c) = v % n(c) - pp % y(c) * grid % vol(c) / m % sav(c)
+    w % n(c) = w % n(c) - pp % z(c) * grid % vol(c) / m % sav(c)
   end do
 
   !----------------------------------------------------------------!
