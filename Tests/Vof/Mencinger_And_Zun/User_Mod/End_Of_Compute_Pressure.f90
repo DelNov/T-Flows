188556c4abb1c17fd9c5a54e75f7d83b29df7cc8
Niceno <bojan.niceno.scientist@gmail.com>
2021-04-30 13:28:37 +0200

    More meaningful names for files with results.
    On branch membrane
    
    modified:   User_Mod/End_Of_Compute_Momentum.f90
    modified:   User_Mod/End_Of_Compute_Pressure.f90
    modified:   User_Mod/End_Of_Correct_Velocity.f90

--- Tests/Vof/Mencinger_And_Zun/User_Mod/End_Of_Compute_Pressure.f90
+++ Tests/Vof/Mencinger_And_Zun/User_Mod/End_Of_Compute_Pressure.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine User_Mod_End_Of_Compute_Pressure(flow, mult, curr_dt, ini)
+  subroutine User_Mod_End_Of_Compute_Pressure(flow, mult, sol, curr_dt, ini)
 !------------------------------------------------------------------------------!
 !   This function is called at the end of Compute_Pressure function.           !
 !------------------------------------------------------------------------------!
@@ -7,31 +7,38 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Field_Type),      target :: flow
   type(Multiphase_Type), target :: mult
+  type(Solver_Type),     target :: sol
   integer, intent(in)           :: curr_dt  ! current time step
   integer, intent(in)           :: ini      ! inner iteration
 !-----------------------------------[Locals]-----------------------------------!
-  type(Grid_Type), pointer :: grid
-  type(Var_Type),  pointer :: p, pp
-  integer                  :: c
-  character(30)            :: file_name = 'compute_pressure_xxxxx_yyy.dat'
+  type(Grid_Type),   pointer :: grid
+  type(Var_Type),    pointer :: p, pp
+  type(Matrix_Type), pointer :: a
+  real, contiguous,  pointer :: b(:)
+  integer                    :: c
+  character(22)              :: file_name = 'pressure_xxxxx_yyy.dat'
 !==============================================================================!
 
   ! Take aliases
   grid => flow % pnt_grid
   p    => flow % p
   pp   => flow % pp
+  a    => sol  % a
+  b    => sol  % b % val
 
-  write(file_name(18:22), '(i5.5)') curr_dt
-  write(file_name(24:26), '(i3.3)') ini
+  write(file_name(10:14), '(i5.5)') curr_dt
+  write(file_name(16:18), '(i3.3)') ini
 
   open(99, file=file_name)
-  write(99, '(a)') '# compute_pressure: 1:x, 2:rho, 3:pp, 4:p'
+  write(99, '(a)') '# compute_pressure: 1:x, 2:p, 3:pp, 4:p%x, 5:pp%x ' //  &
+                   ' 4:density  5:diag  6:b'
 
   do c = 1, grid % n_cells
     if(Math_Mod_Approx_Real(grid % yc(c), 0.0) .and.  &
        Math_Mod_Approx_Real(grid % zc(c), 0.0)) then
-      write(99, '(99e15.5)')  &
-        grid % xc(c), pp % n(c), p % n(c), flow % density (c)
+      write(99, '(99es15.5)')  &
+        grid % xc(c), p % n(c), pp % n(c), p % x(c), pp % x(c),  &
+        flow % density(c), a % val(a % dia(c)), b(c)
     end if
   end do
 
--- Tests/Vof/Mencinger_And_Zun/User_Mod/End_Of_Correct_Velocity.f90
+++ Tests/Vof/Mencinger_And_Zun/User_Mod/End_Of_Correct_Velocity.f90
@@ -15,8 +15,10 @@
   type(Var_Type),    pointer :: u
   type(Face_Type),   pointer :: v_flux          ! volume flux
   integer                    :: c, s
-  character(30)              :: fil1_name = 'correct_velocity_xxxxx_yyy.dat'
-  character(30)              :: fil2_name = 'correct_v_vluxes_xxxxx_yyy.dat'
+  character(39)              :: fil1_name =  &
+                                'velocity_after_correction_xxxxx_yyy.dat'
+  character(39)              :: fil2_name =  &
+                                'face_vel_after_correction_xxxxx_yyy.dat'
 !==============================================================================!
 
   ! Take aliases
@@ -27,8 +29,8 @@
   !---------------------------------------------------!
   !   Write down corrected cell-centered velocities   !
   !---------------------------------------------------!
-  write(fil1_name(18:22), '(i5.5)') curr_dt
-  write(fil1_name(24:26), '(i3.3)') ini
+  write(fil1_name(27:31), '(i5.5)') curr_dt
+  write(fil1_name(33:35), '(i3.3)') ini
 
   open(99, file=fil1_name)
   write(99, '(a)') '# User_Mod_End_Of_Correct_Velocity: 1:x  2:u  3:rho'
@@ -36,7 +38,7 @@
   do c = 1, grid % n_cells
     if(Math_Mod_Approx_Real(grid % yc(c), 0.0) .and.  &
        Math_Mod_Approx_Real(grid % zc(c), 0.0)) then
-      write(99, '(99e15.5)')  &
+      write(99, '(99es15.5)')  &
         grid % xc(c), u % n(c), flow % density(c)
     end if
   end do
@@ -46,8 +48,8 @@
   !---------------------------------------------------!
   !   Write down corrected face-centered velocities   !
   !---------------------------------------------------!
-  write(fil2_name(18:22), '(i5.5)') curr_dt
-  write(fil2_name(24:26), '(i3.3)') ini
+  write(fil2_name(27:31), '(i5.5)') curr_dt
+  write(fil2_name(33:35), '(i3.3)') ini
 
   open(99, file=fil2_name)
   write(99, '(a)') '# User_Mod_End_Of_Correct_Velocity: '  //  &  
@@ -56,7 +58,7 @@
     if(Math_Mod_Approx_Real(grid % yf(s), 0.0) .and.  &
        Math_Mod_Approx_Real(grid % zf(s), 0.0) .and.  &
        grid % faces_c(2,s) > 0) then
-      write(99, '(99e15.5)')  &
+      write(99, '(99es15.5)')  &
         grid % xf(s), v_flux % n(s) / grid % s(s), v_flux % n(s)
     end if
   end do
