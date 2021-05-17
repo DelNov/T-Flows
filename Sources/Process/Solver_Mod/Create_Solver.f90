9e442360ba22199a75d9ca505bed2036d51ef548
Niceno <bojan.niceno.scientist@gmail.com>
2021-05-17 21:34:27 +0200

    Changed the case for Grid which is now a class.
    On branch membrane
    
    modified:   Create_Solver.f90

--- Sources/Process/Solver_Mod/Create_Solver.f90
+++ Sources/Process/Solver_Mod/Create_Solver.f90
@@ -1,20 +1,20 @@
 !==============================================================================!
-  subroutine Create_Solver(Sol, grid)
+  subroutine Create_Solver(Sol, Grid)
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   class(Solver_Type)       :: Sol
-  type(Grid_Type),  target :: grid
+  type(Grid_Type),  target :: Grid
 !==============================================================================!
 
-  Sol % pnt_grid => grid
+  Sol % pnt_grid => Grid
 
   if(this_proc < 2) print *, '# Determining matrix topology.'
 
-  call Sol % A % Create_Matrix(grid)
-  call Sol % M % Create_Matrix(grid)
-  call Sol % D % Create_Matrix(grid)
-  call Vector_Mod_Allocate(Sol % b, grid)
+  call Sol % A % Create_Matrix(Grid)
+  call Sol % M % Create_Matrix(Grid)
+  call Sol % D % Create_Matrix(Grid)
+  call Vector_Mod_Allocate(Sol % b, Grid)
 
   if(this_proc < 2) print *, '# Finished !'
 
