835f77d39c059e779978f91cbbd4d4a7ba473748
Niceno <bojan.niceno.scientist@gmail.com>
2021-05-17 23:30:37 +0200

    IMPORTANT: Fixed a very subtle bug with o_buf!
    The o_buff (old buffer, used for testing if refresh of
    buffers was needed) was allocated with the size of sent
    message (ms), but it must me the size of receive buffer
    (mr), because that's what you want to check: values in
    receive buffers.
    
    On branch membrane
    
    modified:   Form_Cells_Comm.f90

--- Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
+++ Sources/Shared/Grid_Mod/Form_Cells_Comm.f90
@@ -1,11 +1,11 @@
 !==============================================================================!
-  subroutine Grid_Mod_Form_Cells_Comm(grid)
+  subroutine Form_Cells_Comm(Grid)
 !------------------------------------------------------------------------------!
 !   Find communication patterns for cells from Process                         !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type) :: grid
+  class(Grid_Type) :: Grid
 !-----------------------------------[Locals]-----------------------------------!
   integer              :: sub, ms, mr, cnt
   integer              :: c, c1, c2, s, n_buff_faces, n_max_buff_cells, i_cel
@@ -20,17 +20,17 @@
   ! Allocate memory for locally used arrays
   allocate(send_buff_cnt(n_proc, n_proc))
   allocate(recv_buff_cnt(n_proc, n_proc))
-  allocate(send_cells(-grid % n_bnd_cells:grid % n_cells))
-  allocate(recv_cells(-grid % n_bnd_cells:grid % n_cells))
+  allocate(send_cells(-Grid % n_bnd_cells:Grid % n_cells))
+  allocate(recv_cells(-Grid % n_bnd_cells:Grid % n_cells))
 
   !-------------------------------!
   !   Count buffer cells inside   !
   !-------------------------------!
-  grid % comm % n_buff_cells = 0
-  do c = 1, grid % n_cells
-    if(grid % comm % cell_proc(c) .ne. this_proc) then
-      grid % comm % n_buff_cells =    &
-      grid % comm % n_buff_cells + 1
+  Grid % comm % n_buff_cells = 0
+  do c = 1, Grid % n_cells
+    if(Grid % comm % cell_proc(c) .ne. this_proc) then
+      Grid % comm % n_buff_cells =    &
+      Grid % comm % n_buff_cells + 1
     end if
   end do
 
@@ -38,9 +38,9 @@
   !   Count buffer faces   !
   !------------------------!
   n_buff_faces = 0
-  do s = 1, grid % n_faces
-    c1 = grid % faces_c(1,s)
-    c2 = grid % faces_c(2,s)
+  do s = 1, Grid % n_faces
+    c1 = Grid % faces_c(1,s)
+    c2 = Grid % faces_c(2,s)
     if(c1 .eq. 0 .and. c2 .eq. 0) then
       n_buff_faces = n_buff_faces + 1
     end if
@@ -51,9 +51,9 @@
   !--------------------------------------------------------!
   recv_buff_cnt(:,:)   = 0
   recv_cells(:) = 0
-  do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
-         grid % n_cells
-    sub = grid % comm % cell_proc(c)
+  do c = Grid % n_cells - Grid % comm % n_buff_cells + 1,  &
+         Grid % n_cells
+    sub = Grid % comm % cell_proc(c)
     recv_buff_cnt(this_proc, sub) = recv_buff_cnt(this_proc, sub) + 1
     recv_cells(c) = sub
   end do
@@ -64,7 +64,7 @@
         write(100*this_proc+sub, *) '#====================================' // &
                                     '====================================='
         write(100*this_proc+sub, '(a,i7,a,i7)')                    &
-              ' # There are        ', grid % comm % n_buff_cells,  &
+              ' # There are        ', Grid % comm % n_buff_cells,  &
               ' buffer cells in processor ', this_proc
         write(100*this_proc+sub, *) '#------------------------------------' // &
                                     '-------------------------------------'
@@ -78,7 +78,7 @@
   !--------------------------------------------------!
   !   Allocate memory for matrix with needed cells   !
   !--------------------------------------------------!
-  n_max_buff_cells = grid % comm % n_buff_cells
+  n_max_buff_cells = Grid % comm % n_buff_cells
   call comm_mod_global_max_int(n_max_buff_cells)
   allocate(need_cell(n_max_buff_cells, n_proc));  need_cell(:,:) = 0
   allocate(from_proc(n_max_buff_cells, n_proc));  from_proc(:,:) = 0
@@ -88,12 +88,12 @@
   !    and also from which processor it is    !
   !-------------------------------------------!
   i_cel = 0
-  do c = grid % n_cells - grid % comm % n_buff_cells + 1,  &
-         grid % n_cells
-    if(grid % comm % cell_proc(c) .ne. this_proc) then
+  do c = Grid % n_cells - Grid % comm % n_buff_cells + 1,  &
+         Grid % n_cells
+    if(Grid % comm % cell_proc(c) .ne. this_proc) then
       i_cel = i_cel + 1
-      need_cell(i_cel, this_proc) = grid % comm % cell_glo(c)
-      from_proc(i_cel, this_proc) = grid % comm % cell_proc(c)
+      need_cell(i_cel, this_proc) = Grid % comm % cell_glo(c)
+      from_proc(i_cel, this_proc) = Grid % comm % cell_proc(c)
     end if
   end do
 
@@ -128,8 +128,8 @@
   !--------------------------------------------------!
   !   Allocate memory for send and receive buffers   !
   !--------------------------------------------------!
-  allocate(grid % comm % cells_send(n_proc))
-  allocate(grid % comm % cells_recv(n_proc))
+  allocate(Grid % comm % cells_send(n_proc))
+  allocate(Grid % comm % cells_recv(n_proc))
 
   !-----------------------------------!
   !                                   !
@@ -139,8 +139,8 @@
   do sub = 1, n_proc
 
     ! Initialize buffer size to zero
-    grid % comm % cells_send(sub) % n_items = 0
-    grid % comm % cells_recv(sub) % n_items = 0
+    Grid % comm % cells_send(sub) % n_items = 0
+    Grid % comm % cells_recv(sub) % n_items = 0
 
     if(sub .ne. this_proc) then
 
@@ -151,18 +151,18 @@
       mr = recv_buff_cnt(this_proc, sub)
 
       if(ms > 0) then
-        allocate(grid % comm % cells_send(sub) % map(ms));
-        allocate(grid % comm % cells_send(sub) % i_buff(ms));
-        allocate(grid % comm % cells_send(sub) % l_buff(ms));
-        allocate(grid % comm % cells_send(sub) % r_buff(ms));
+        allocate(Grid % comm % cells_send(sub) % map(ms));
+        allocate(Grid % comm % cells_send(sub) % i_buff(ms));
+        allocate(Grid % comm % cells_send(sub) % l_buff(ms));
+        allocate(Grid % comm % cells_send(sub) % r_buff(ms));
       end if
 
       if(mr > 0) then
-        allocate(grid % comm % cells_recv(sub) % map(mr));
-        allocate(grid % comm % cells_recv(sub) % i_buff(mr));
-        allocate(grid % comm % cells_recv(sub) % l_buff(mr));
-        allocate(grid % comm % cells_recv(sub) % r_buff(mr));
-        allocate(grid % comm % cells_recv(sub) % o_buff(ms));
+        allocate(Grid % comm % cells_recv(sub) % map(mr));
+        allocate(Grid % comm % cells_recv(sub) % i_buff(mr));
+        allocate(Grid % comm % cells_recv(sub) % l_buff(mr));
+        allocate(Grid % comm % cells_recv(sub) % r_buff(mr));
+        allocate(Grid % comm % cells_recv(sub) % o_buff(mr));
       end if
 
       !------------------------------------------!
@@ -176,8 +176,8 @@
       send_cells(:) = 0
       do i_cel = 1, n_max_buff_cells
         if(from_proc(i_cel, sub) .eq. this_proc) then
-          do c = 1, grid % n_cells
-            if(grid % comm % cell_glo(c) .eq. need_cell(i_cel, sub)) then
+          do c = 1, Grid % n_cells
+            if(Grid % comm % cell_glo(c) .eq. need_cell(i_cel, sub)) then
               send_cells(c) = sub                                  ! identify
               cnt = cnt + 1
             end if
@@ -189,19 +189,19 @@
                       ' #   It did find    ', cnt,    &
                       ' cells to send to processor', sub
 
-      ! This worries me.  Why should this be from -grid % n_bnd_cells or not
-      do c = 1, grid % n_cells
+      ! This worries me.  Why should this be from -Grid % n_bnd_cells or not
+      do c = 1, Grid % n_cells
         if(send_cells(c) .eq. sub) then
           ms = ms + 1
-          grid % comm % cells_send(sub) % map(ms) = c
+          Grid % comm % cells_send(sub) % map(ms) = c
         end if
       end do
 
-      ! This worries me.  Why should this be from -grid % n_bnd_cells or not
-      do c = 1, grid % n_cells
+      ! This worries me.  Why should this be from -Grid % n_bnd_cells or not
+      do c = 1, Grid % n_cells
         if(recv_cells(c) .eq. sub) then
           mr = mr + 1
-          grid % comm % cells_recv(sub) % map(mr) = c
+          Grid % comm % cells_recv(sub) % map(mr) = c
         end if
       end do
 
@@ -211,8 +211,8 @@
       end if
 
       ! Store final buffer lengths
-      grid % comm % cells_send(sub) % n_items = ms
-      grid % comm % cells_recv(sub) % n_items = mr
+      Grid % comm % cells_send(sub) % n_items = ms
+      Grid % comm % cells_recv(sub) % n_items = mr
 
     end if  ! sub .ne. this_proc
   end do    ! sub
@@ -221,7 +221,7 @@
   !   Avoid faces in the buffers only   !
   !   (This also leaves shadows out)    !
   !-------------------------------------!
-  grid % n_faces = grid % n_faces - n_buff_faces
+  Grid % n_faces = Grid % n_faces - n_buff_faces
 
   ! De-allocate locally used memory
   deallocate(send_buff_cnt)
