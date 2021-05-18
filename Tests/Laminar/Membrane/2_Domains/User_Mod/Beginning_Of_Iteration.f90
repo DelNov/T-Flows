df8c79fc45cbe94d19598e37b5a55eeaef6a47e1
Niceno <bojan.niceno.scientist@gmail.com>
2021-05-18 14:31:37 +0200

    Implemented sources Kerstin sent on March 21 2020
    On branch membrane
    
    new file:   User_Mod/Beginning_Of_Iteration.f90
    new file:   User_Mod/Brent_For_Jump_Cond.f90
    new file:   User_Mod/Initialize_Variables.f90
    modified:   User_Mod/Interface_Exchange.f90
    new file:   User_Mod/Jump_Cond.f90
    new file:   User_Mod/Pv_Sat.f90
    new file:   User_Mod/Pv_Sat_Salt.f90
    new file:   User_Mod/Read_Control_Boundary_Conditions.f90
    new file:   User_Mod/T_Sat.f90
    new file:   User_Mod/To_Buffer.f90
    new file:   User_Mod/Types.f90
    modified:   control.0
    modified:   control.1
    modified:   control.2

---
+++ Tests/Laminar/Membrane/2_Domains/User_Mod/Beginning_Of_Iteration.f90
@@ -0,0 +1,153 @@
+include '../User_Mod/T_Sat.f90'
+
+!==============================================================================!
+  subroutine User_Mod_Beginning_Of_Iteration(Flow, turb, Vof, swarm, n, time)
+!------------------------------------------------------------------------------!
+!   This function is called at the beginning of time step.                     !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),      target :: Flow
+  type(Turb_Type),       target :: turb
+  type(Vof_Type),        target :: Vof
+  type(Swarm_Type),      target :: swarm
+  integer                       :: n     ! time step
+  real                          :: time  ! physical time
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type),  pointer :: Grid
+  type(Var_Type),   pointer :: u, v, w, t, scalar
+  real, contiguous, pointer :: dens(:)
+  integer                   :: dataIndex, c, s, c1, c2
+  real                      :: dens_air(9), dens_h2o(9), temperat_int(9)
+  real                      :: weight, a0, a1, a2, a3, a4, a5, b0, b1, b2, c0
+  real                      :: aa, bb, cc, a_tmp, m_h2o, m_air, M, pv, t_int
+  real                      :: t_film, k_film, d_film, h_d, m_evap
+!==============================================================================!
+
+  ! Take aliases
+  Grid   => Flow % pnt_grid
+  t      => Flow % t
+  scalar => Flow % scalar(1)
+
+  ! Temperature (and salt) dependent density
+  temperat_int = (/10.0, 20.0, 30.0, 40.0, 50.0,  60.0,  70.0,   80.0, 90.0 /)
+  dens_air = (/1.246, 1.204, 1.166, 1.127, 1.097, 1.067, 1.034, 1.0, 0.9734 /)
+  dens_h2o = (/999.7, 998.3, 995.7, 992.3, 988.0, 983.0, 978.0, 972.0, 965.0/)
+
+  !--------------------------------!
+  !   Operations in upper domain   !
+  !--------------------------------!
+  if(Grid % name .eq. 'UPPER_DOM') then
+
+    do c = 1, Grid % n_cells
+      if(t % n(c) .gt. 90.0 .or. t % n(c) .lt. 10.0) then
+         error stop "temperature out of density interpolation range [10 - 90C]"
+      endif
+
+      dataIndex = floor(t % n(c) /10) 
+      if(t % n(c) .eq. 90.0) dataIndex = 8
+      weight = (t % n(c) - temperat_int(dataIndex))                   &
+             / (temperat_int(dataIndex+1) - temperat_int(dataIndex))
+
+      Flow % density(c) = (1.0 - weight) * dens_h2o(dataIndex)      &
+                        +        weight  * dens_h2o(dataIndex + 1)
+
+      ! Salt influence taken from Millero and Huang 2009
+      a_tmp = scalar % n(c) * 1000 ! convert to g/kg
+
+      a0 =  8.197247E-01
+      a1 = -3.779454E-03
+      a2 =  6.821795E-05
+      a3 = -8.009571E-07
+      a4 =  6.158885E-09
+      a5 = -2.001919E-11
+
+      b0 = -5.808305E-03
+      b1 =  5.354872E-05
+      b2 = -4.714602E-07
+
+      c0 =  5.249266E-04
+
+      if (t % n(c) .ge. 0.0 .and. t % n(c) .le. 90.0) then
+        aa = a0 + a1 * t % n(c) + a2 * t % n(c)**2 + &
+             a3 * t % n(c)**3 + a4 * t %n(c)**4 + &
+             a5 * t % n(c)**5
+
+        bb = b0 + b1 * t % n(c) + b2 * t % n(c)**2
+        cc = c0
+
+        Flow % density(c) = Flow % density(c) + aa * scalar % n(c) &
+                                              + bb * scalar % n(c)**1.5 &
+                                              + cc * scalar % n(c)**2
+      else
+        error stop 'Temperature value out of range for ' //  &
+                   'calculation of salt dependent density [0-90C]'
+      end if
+
+    end do
+
+  end if
+
+  !--------------------------------!
+  !   Operations in lower domain   !
+  !--------------------------------!
+  if(Grid % name .eq. 'LOWER_DOM') then
+
+    do c = 1, Grid % n_cells
+      if(t % n(c) .gt. 90.0 .or. t % n(c) .lt. 10.0) then
+         error stop 'Temperature out of density interpolation range [10 - 90C]'
+      endif
+
+      dataIndex = floor(t % n(c) /10) 
+      if(t % n(c) .eq. 90.0) dataIndex = 8
+      weight = (t % n(c) - temperat_int(dataIndex)) / &
+               (temperat_int(dataIndex+1) - temperat_int(dataIndex))
+
+      Flow % density(c)  = (1.0 - weight) * dens_air(dataIndex)      &
+                         +        weight  * dens_air(dataIndex + 1)
+    end do
+
+    ! As long as no film domain exists
+    m_h2o  = 18.0e-3
+    m_air  = 28.0e-3
+    t_film = 20.0
+    h_d    = 2454e3 ! J/kg
+    k_film = 0.6009 ! W/mK
+    d_film = 1e-3  ! m
+
+    do s = 1, Grid % n_faces
+      c1 = Grid % faces_c(1,s)
+      c2 = Grid % faces_c(2,s) !-> each face 2 cells, right cell
+      if (c2 .lt. 0) then ! at boundary
+
+        ! Check boundary by its name
+        if (Var_Mod_Bnd_Cond_Name(scalar,c2) .eq. 'BOTTOM_WALL') then
+          M = 1.0 / ( (1.0 - scalar % n(c1)) / m_air  &
+                           + scalar % n(c1)  / m_h2o )
+          pv = scalar % n(c1) * M/m_h2o * 1e5
+          !print * , pv!
+          call t_sat(t_int, pv)
+
+          ! Underrelaxation of condensation absolutely necessary!!
+          t_int = t_int * 0.1 + 0.9 * t_int_prev(c1)
+
+          t_int_prev(c1) = t_int
+          !t % n(c2) = t_int
+          m_evap = (Flow % conductivity(c1) &
+                  / Grid % wall_dist(c1) * (t % n(c1)-t_int)  &
+                 + k_film / d_film * (t_film - t_int)) / h_d
+
+          ! scalar % q(c2) = m_evap * Grid % sz(s)
+        end if
+      end if
+    end do
+
+    ! Positive for evaporation, negative for condensation
+    if(this_proc < 2) then
+      print * , 'm_evap =', m_evap * 3600, ' kg/m²h '
+      print * , 't_int = ', t_int, 'Celsius'
+    end if
+
+  end if
+
+  end subroutine
--- Tests/Laminar/Membrane/2_Domains/User_Mod/Interface_Exchange.f90
+++ Tests/Laminar/Membrane/2_Domains/User_Mod/Interface_Exchange.f90
@@ -1,3 +1,7 @@
+include '../User_Mod/Pv_Sat_Salt.f90'
+include '../User_Mod/Jump_Cond.f90'
+include '../User_Mod/Brent_For_Jump_Cond.f90'
+
 !==============================================================================!
   subroutine User_Mod_Interface_Exchange(inter, Flow, n_dom)
 !------------------------------------------------------------------------------!
@@ -7,21 +11,40 @@
 !---------------------------------[Arguments]----------------------------------!
   type(Interface_Type)        :: inter(MD, MD)
   type(Field_Type),    target :: Flow(MD)
-  integer, intent(in)         :: n_dom
+  integer                     :: n_dom
 !------------------------------[Local parameters]------------------------------!
   integer, parameter :: T  = 1,  &  ! store temperature as the first ...
                         K  = 2,  &  ! ... conductivity as the second ...
                         WD = 3,  &  ! ... and wall distance as the third ...
-                        C  = 4      ! ... and scalar as the fourth variable
+                        C  = 4,  &  ! ... and scalar as the fourth variable
+                        P  = 5,  &  ! ... and pressure as the fifth
+                        CB = 6      ! ... boundary value of scalar
 !-----------------------------------[Locals]-----------------------------------!
   integer :: d1, d2     ! counters for domains 1 and 2
   integer :: n1, n2, n  ! local counters and number at interfaces 1 and 2
   integer :: ic1, bc1   ! internal (i) and boundary (b) cells in domain 1
-  integer :: ic2, bc2   ! internal (i) and boundary (b) cells in domain 1
+  integer :: ic2, bc2   ! internal (i) and boundary (b) cells in domain 2
+  integer :: s1, s2     ! surface index in domains 1 and 2
   real    :: t1, t2     ! temperatures in domains 1 and 2
   real    :: k1, k2     ! conductivities in domains 1 and 2
   real    :: wd1, wd2   ! wall distances in domains 1 and 2
   real    :: sc1, sc2   ! scalars in domains 1 and 2
+  real    :: sc2b       ! scalar boundary value dom 2
+  real    :: p1, p2     ! pressure in domains 1 and 2
+  real    :: M, m_h2o, m_air, m_salt
+  real    :: p_v_air, p_v_h2o    ! partial vapor pressure in air domain
+  real    :: t_tmp, t_int
+  real    :: mem_eps, mem_r, mem_tau, mem_d, mem_t, mem_matkap, mem_p, mem_pv
+  real    :: mem_kap, k_res
+  real    :: diff, h_d  ! diffusivity of vapor in air and latent heat
+  real    :: c_k, c_m, c_t ! DGM coefficients
+  real    :: lhs_lin, lhs_fun, rhs ! variables for jump cond at membrane
+  real    :: res        ! residual of jump condition
+  real    :: mem_j_diff, mem_j_heat
+  real    :: sc1_new    ! accumulation of salt after evaporation helping variabl
+  real    :: p_test
+!------------------------------[Local parameters]------------------------------!
+  real, parameter :: R = 8.31446
 !==============================================================================!
 
   !-------------------------------------------!
@@ -50,11 +73,25 @@
                                    Flow(d2) % pnt_grid % wall_dist,  &
                                    WD)
 
-      ! Send scalar 1 as well
+      ! Send inside scalar 1 as well
       call Interface_Mod_To_Buffer(inter(d1, d2),             &
                                    Flow(d1) % scalar(1) % n,  &
                                    Flow(d2) % scalar(1) % n,  &
                                    C)
+
+      ! Send pressure as well
+      call Interface_Mod_To_Buffer(inter(d1, d2),             &
+                                   Flow(d1) % p % n,          &
+                                   Flow(d2) % p % n,          &
+                                   P)
+
+      ! Send boundary scalar 1 as well
+      call Interface_Mod_To_Buffer(inter(d1, d2),             &
+                                   Flow(d1) % scalar(1) % n,  &
+                                   Flow(d2) % scalar(1) % n,  &
+                                   CB,                        &
+                                   boundary = .true.)
+
     end do
   end do
 
@@ -64,7 +101,6 @@
   !    to impose boundary conditions for each domain.    !
   !                                                      !
   !------------------------------------------------------!
-
   do d1 = 1, n_dom
     do d2 = 1, n_dom
 
@@ -85,23 +121,99 @@
         k1  = Flow(d1) % conductivity(ic1)          ! conductivity in dom 1
         wd1 = Flow(d1) % pnt_grid % wall_dist(ic1)  ! wall distance in dom 1
         sc1 = Flow(d1) % scalar(1) % n(ic1)         ! scalar in dom 1
+        p1  = Flow(d1) % p % n(ic1)                 ! pressure in dom 1
 
         ! Fetch values from buffers (other domain)
         t2  = inter(d1, d2) % phi_2(n, T)           ! temperature in dom 2
         k2  = inter(d1, d2) % phi_2(n, K)           ! conductivity in dom 2
         wd2 = inter(d1, d2) % phi_2(n, WD)          ! wall distance in dom 2
         sc2 = inter(d1, d2) % phi_2(n, C)           ! scalar in dom 2
+        p2  = inter(d1, d2) % phi_2(n, P)           ! pressure in dom 2
+        sc2b= inter(d1, d2) % phi_2(n, CB)          ! scalar boundary in dom 2
 
         !---------------------------------------------!
         !   Implementation of your model comes here   !
         !---------------------------------------------!
 
-        ! Set temperature at the boundary of domain 1
-        Flow(d1) % t % n(bc1) = (t1 * k1 / wd1 + t2 * k2 / wd2)  &
-                              / (     k1 / wd1 +      k2 / wd2)
+        ! Molar mass and partial vapor pressure
+        m_h2o  = 18E-3 ! kg/mol
+        m_air  = 28E-3 ! kg/mol
+        m_salt = 58.4428e-3 ! kg/mol
 
-        ! Set scalar at the boundary of domain 1
-        Flow(d1) % scalar(1) % n(bc1) = (sc1 + sc2) * 0.5
+        ! partial vapor pressure on air side of membrane
+        M = 1.0/((1-sc2b)/m_air + sc2b/m_h2o)
+        p_v_air = sc2b * M/m_h2o *1e5
+
+        call Pv_Sat_Salt(t_int_mem_prev(n), m_h2o, m_salt, sc1, p_v_h2o)
+
+        ! Calc_membrane
+        h_d  = 2383e3  ! J/kg K latent heat
+        diff = 3.5e-5  ! diffusivity of vapor in air
+
+        ! Membrane values, hopefully one day read from
+        ! membrane file; until then hard-coded
+        mem_eps = 0.85
+        mem_r =  0.1e-6 ! m
+        mem_tau = 1.5  
+        mem_d = 65e-6 ! m
+        mem_matkap = 0.25 ! W/mK 
+
+        mem_p = (p1 + p2) * 0.5 + 1e5
+        mem_pv = (p_v_air + p_v_h2o) * 0.5
+        mem_kap = mem_matkap * (1-mem_eps) + k2 * mem_eps
+
+        k_res = k2*mem_d/mem_kap/wd2 ! in pyns const_mem_2
+        mem_t = ((t_int_mem_prev(n)  &
+              + (t_int_mem_prev(n) + k_res * t2)/(1+k_res)) * 0.5) + 273.15 ! K
+
+        ! DGM coefficients
+        c_k = 2.0 * mem_eps * mem_r  &
+            / (3.0*mem_tau*mem_d) * (8.0*m_h2o/(mem_t*R*PI))**0.5
+        c_m = mem_eps*mem_p*diff / (mem_d*mem_tau*R*mem_t*(mem_p-mem_pv))
+        c_t = 1.0/(1.0/c_k + 1.0/c_m)
+
+        ! lhs_lin * t_int + lhs_fun * pv_sat(t_int) = rhs --> solve for t_int
+        lhs_lin = (k1/wd1 + 1.0/(wd2/k2 + mem_d/mem_kap)) * mem_eps / h_d
+        lhs_fun = c_t * (1-m_h2o/m_salt * sc1)
+        rhs     = c_t * p_v_air + mem_eps/h_d  &
+                      * (k1 / wd1 * t1 + 1.0 / (wd2/k2+mem_d/mem_kap)*t2)
+
+        ! Arguments: function to solve:
+        !    initial guess, tolerance, resulting temperature, lower boundary,
+        !    upper boundary, verbose mode (0 or 1), args for function to solve
+        call Brent(jump_cond, t_int_mem_prev(n), 0.0000001,  &
+                   t_int, 0.0, 90.0, 0, lhs_lin, lhs_fun, rhs)
+
+        ! Test for function fzero
+        ! call Jump_Cond(t_int, res, lhs_lin, lhs_fun, rhs)
+        ! print * , res ! should give values close to zero
+
+        ! Update partial vapor pressure on membrane top
+        call Pv_Sat_Salt(t_int, m_h2o, m_salt, sc1, p_v_h2o)
+
+        ! Calculation of membrane flux
+        mem_j_diff = c_t * (p_v_h2o - p_v_air) ! kg/(s m2)
+        mem_j_heat = (  mem_eps*k1/wd1*(t1-t_int)  &
+                      - mem_eps/(mem_d/mem_kap + wd2/k2) * (t_int-t2)) / h_d
+        ! Test correct implementation of jump condition:
+        ! print *, mem_j_diff-mem_j_heat  ! should give values close to zero
+
+        ! Salt water: boundary velocity in z direction due to membrane flux
+        ! in upper loop
+
+        ! Salt water: source term in 1st domain cell for scalar
+        sc1_new = sc1 * Flow(d1) % density(ic1)  &
+                / (Flow(d1) % density(ic1) - mem_j_heat / (2*wd1)* Flow(d1) % dt)
+        ! Flow(d1) % scalar(1) % q(bc1) = Flow(d1) % density(ic1)  &
+        !                               / Flow(d1) % dt * (sc1_new - sc1)
+
+        ! Interface temperature
+        ! using the interface index n is very ugly but the easiest
+        ! way to have t_int for dom 2 in the loop below!!
+        t_int_mem_prev(n) = t_int
+
+        ! Set temperature at the boundary of domain 1
+        Flow(d1) % t % n(bc1) = t_int
 
       end do
 
@@ -114,34 +226,66 @@
 
         ! Fetch indexes
         n   = inter(d1, d2) % face_2(n2)   ! interface index
-        ic2 = inter(d1, d2) % cell_2(n2)   ! domain 1, cell inside the domain
-        bc2 = inter(d1, d2) % bcel_2(n2)   ! domain 1, cell on the boundary
+        ic2 = inter(d1, d2) % cell_2(n2)   ! domain 2, cell inside the domain
+        bc2 = inter(d1, d2) % bcel_2(n2)   ! domain 2, cell on the boundary
 
         ! Fetch dependent variables
         t2  = Flow(d2) % t % n(ic2)                 ! temperature in dom 2
         k2  = Flow(d2) % conductivity(ic2)          ! conductivity in dom 2
         wd2 = Flow(d2) % pnt_grid % wall_dist(ic2)  ! wall distance in dom 2
         sc2 = Flow(d2) % scalar(1) % n(ic2)         ! scalar in dom 2
+        p2  = Flow(d2) % p % n(ic2)                 ! pressure in dom 2
 
         ! Fetch values from buffers (other domain in this case 1)
         t1  = inter(d1, d2) % phi_1(n, T)           ! temperature in dom 1
         k1  = inter(d1, d2) % phi_1(n, K)           ! conductivity in dom 1
         wd1 = inter(d1, d2) % phi_1(n, WD)          ! wall distance in dom 1
         sc1 = inter(d1, d2) % phi_1(n, C)           ! scalar in dom 1
+        p1  = inter(d1, d2) % phi_1(n, P)           ! pressure in dom 1
 
         !---------------------------------------------!
         !   Implementation of your model comes here   !
         !---------------------------------------------!
+        mem_eps    = 0.85
+        mem_d      = 65e-6 ! m
+        mem_matkap = 0.25 ! W/mK 
+        mem_kap = mem_matkap * (1-mem_eps) + k2 * mem_eps
+
+        t_int = t_int_mem_prev(n) ! ugly to use n!!
+
+        mem_j_heat = (  mem_eps * k1 / wd1 * (t1-t_int)                       &
+                      - mem_eps / (mem_d / mem_kap + wd2 / k2) * (t_int-t2))  &
+                    / h_d
+
+        ! Air gap: source term in 1st domain cell for scalar
+        do s2 = 1, Flow(d2) % pnt_grid % n_faces
+          if (Flow(d2) % pnt_grid % faces_c(2,s2) == bc2) then
+            Flow(d2) % scalar(1) % q(bc2) = mem_j_heat  &
+                                          * Flow(d2) % pnt_grid % sz(s2)
+            ! Flow(d2) % scalar(1) % q(bc2) = 1e-9
+            ! print *, Flow(d2) % scalar(1) % n(bc2)
+            ! print *, 'source term scalar lower = ',  &
+            !          Flow(d2) % scalar(1) % q(bc2)
+          endif
+        end do
 
         ! Set temperature at the boundary of domain 2
-        Flow(d2) % t % n(bc2) = (t1 * k1 / wd1 + t2 * k2 / wd2)  &
-                              / (     k1 / wd1 +      k2 / wd2)
-
-        ! Set scalar at the boundary of domain 2
-        Flow(d2) % scalar(1) % n(bc2) = (sc1 + sc2) * 0.5
+        k_res = k2 * mem_d / mem_kap / wd2  ! in pyns const_mem_2
+        Flow(d2) % t % n(bc2) = (t_int + k_res * t2)/(1+k_res)
 
       end do
     end do
   end do
+
+  ! Control
+  if(this_proc < 2) then
+    print *, 'mem_j_diff = ' , mem_j_diff * 3600, ' kg/m²h'
+    print *, 'mem_j_heat = ' , mem_j_heat * 3600, ' kg/m²h'
+    print *, 'jump condition coefficients: ', lhs_lin, lhs_fun, rhs
+    print *, 'partial vapor pressure on water and air side: ', p_v_h2o, p_v_air
+    print *, 'scalars salt and vapor: ', sc1, sc2
+    print *, 'M in air gap: ', M
+    print *, 't_int_mem = ' , t_int, ' C'
+  end if
 
   end subroutine
