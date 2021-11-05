207923151508a8a5b570aa7354635a2de2ff6adb
mhadziabdic <muhamed.hadziabdic@gmail.com>
2021-11-05 13:53:08 +0100

    Syntax was not changed in BiCG.f90.

--- Sources/Process/Solver_Mod/Normalized_Solvers/Bicg.f90
+++ Sources/Process/Solver_Mod/Normalized_Solvers/Bicg.f90
@@ -157,8 +157,8 @@
     !   q = A   p   !
     !   q~= A^T p~  !  don't have A^T
     !---------------!
-    call Grid_Mod_Exchange_Cells_Real(A % pnt_grid, p1(-nb:ni))
-    call Grid_Mod_Exchange_Cells_Real(A % pnt_grid, p2(-nb:ni))
+    call A % pnt_grid % Exchange_Cells_Real(p1(-nb:ni))
+    call A % pnt_grid % Exchange_Cells_Real(p2(-nb:ni))
     do i = 1, ni
       q1(i) = 0.0
       q2(i) = 0.0
