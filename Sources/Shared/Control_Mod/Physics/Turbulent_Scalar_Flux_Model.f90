54a06097823d3ff58a3333978bb8a9e2d74c183f
mhadziabdic <muhamed.hadziabdic@gmail.com>
2021-11-05 19:52:59 +0100

    Initial commit.

---
+++ Sources/Shared/Control_Mod/Physics/Turbulent_Scalar_Flux_Model.f90
@@ -0,0 +1,16 @@
+!==============================================================================!
+  subroutine Control_Mod_Turbulent_Scalar_Flux_Model(val, verbose)
+!------------------------------------------------------------------------------!
+!   Reading turbulent heat flux model from the control file.                   !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  character(SL)     :: val
+  logical, optional :: verbose
+!==============================================================================!
+
+  call Control_Mod_Read_Char_Item('TURBULENT_SCALAR_FLUX_MODEL', 'SGDH',  &
+                                   val, verbose)
+  call To_Upper_Case(val)
+
+  end subroutine
