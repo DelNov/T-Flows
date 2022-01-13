e1e46678c2def67bec242e8b8e06556f44e39d10
mhadziabdic <muhamed.hadziabdic@gmail.com>
2022-01-13 11:14:49 +0100

    Dimensional solvers.

---
+++ Sources/Process/Solver_Mod/Dimensional_Solvers/Cg.f90
@@ -0,0 +1,185 @@
+!==============================================================================!
+  subroutine Cg(Sol, A, x, b, prec, miter, niter, tol, fin_res, norm)
+!------------------------------------------------------------------------------!
+!   Solves the linear systems of equations by a precond. CG Method.            !
+!------------------------------------------------------------------------------!
+!   Allows preconditioning of the system by:                                   !
+!     1. Diagonal preconditioning                                              !
+!     2. Incomplete Cholesky preconditioning                                   !
+!                                                                              !
+!   The type of precondtioning is chosen by setting the variable prec to 0     !
+!   (for no preconditioning), 1 (for diagonal preconditioning) or 2 (for       !
+!   incomplete Cholesky preconditioning)                                       !
+!------------------------------------------------------------------------------!
+!----------------------------------[Modules]-----------------------------------!
+  use Work_Mod, only: p1 => r_cell_01,  &
+                      q1 => r_cell_02,  &
+                      r1 => r_cell_03
+!------------------------------------------------------------------------------!
+!   When using Work_Mod, calling sequence should be outlined                   !
+!                                                                              !
+!   Main_Pro                         (allocates Work_Mod)                      !
+!     |                                                                        !
+!     +----> Compute_Pressure        (does not use Work_Mod)                   !
+!              |                                                               !
+!              +----> Solver_Mod_Cg  (safe to use r_cell_07..12)               !
+!                                                                              !
+!   Main_Pro                                    (allocates Work_Mod)           !
+!     |                                                                        !
+!     +----> Turb_Mod_Main                      (does not use Work_Mod)        !
+!              |                                                               !
+!              +---> Turb_Mod_Compute_F22       (does not use Work_Mod)        !
+!                      |                                                       !
+!                      +----> Solver_Mod_Cg     (safe to use r_cell_01..03)    !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  class(Solver_Type), target :: Sol
+  type(Matrix_Type)          :: A
+  real                       :: x(-Sol % pnt_grid % n_bnd_cells :  &
+                                   Sol % pnt_grid % n_cells)
+  real                       :: b( Sol % pnt_grid % n_cells)
+  character(SL)              :: prec     ! preconditioner
+  integer                    :: miter    ! maximum and actual ...
+  integer                    :: niter    ! ... number of iterations
+  real                       :: tol      ! tolerance
+  real                       :: fin_res  ! final residual
+  real, optional             :: norm     ! normalization
+!-----------------------------------[Locals]-----------------------------------!
+  type(Matrix_Type), pointer :: D
+  integer                    :: nt, ni, nb
+  real                       :: alfa, beta, rho, rho_old, bnrm2, res
+  integer                    :: i, j, k, iter
+!==============================================================================!
+
+  ! Take some aliases
+  D => Sol % D
+  nt = A % pnt_grid % n_cells
+  ni = A % pnt_grid % n_cells - A % pnt_grid % comm % n_buff_cells
+  nb = A % pnt_grid % n_bnd_cells
+
+  res = 0.0
+
+  !---------------------!
+  !   Preconditioning   !
+  !---------------------!
+  call Sol % Prec_Form(ni, A, D, prec)
+
+  !-----------------------------------!
+  !    This is quite tricky point.    !
+  !   What if bnrm2 is very small ?   !
+  !-----------------------------------!
+  if(.not. present(norm)) then
+    bnrm2 = Sol % Normalized_Root_Mean_Square(ni, b(1:nt), A, x(1:nt))
+  else
+    bnrm2 = Sol % Normalized_Root_Mean_Square(ni, b(1:nt), A, x(1:nt), norm)
+  end if
+
+  if(bnrm2 < tol) then
+    iter = 0
+    goto 1
+  end if
+
+  !----------------!
+  !   r = b - Ax   !
+  !----------------!
+  call Sol % Residual_Vector(ni, r1(1:nt), b(1:nt), A, x(1:nt))
+
+  !--------------------------------!
+  !   Calculate initial residual   !
+  !--------------------------------!
+  res = Sol % Normalized_Root_Mean_Square(ni, r1(1:nt), A, x(1:nt))
+
+  if(res < tol) then
+    iter = 0
+    goto 1
+  end if
+
+  !-----------!
+  !   p = r   !
+  !-----------!
+  p1(1:ni) = r1(1:ni)
+
+  !---------------!
+  !               !
+  !   Main loop   !
+  !               !
+  !---------------!
+  do iter = 1, miter
+
+    !----------------------!
+    !     solve Mz = r     !
+    !   (q instead of z)   !
+    !----------------------!
+    call Sol % Prec_Solve(ni, A, D, q1(1:nt), r1(1:nt), prec)
+
+    !-----------------!
+    !   rho = (r,z)   !
+    !-----------------!
+    rho = dot_product(r1(1:ni), q1(1:ni))
+    call Comm_Mod_Global_Sum_Real(rho)
+
+    if(iter .eq. 1) then
+      p1(1:ni) = q1(1:ni)
+    else
+      beta = rho / rho_old
+      p1(1:ni) = q1(1:ni) + beta * p1(1:ni)
+    end if
+
+    !------------!
+    !   q = Ap   !
+    !------------!
+    call A % pnt_grid % Exchange_Cells_Real(p1(-nb:ni))
+    do i = 1, ni
+      q1(i) = 0.0
+      do j = A % row(i), A % row(i+1)-1
+        k = A % col(j)
+        q1(i) = q1(i) + A % val(j) * p1(k)
+      end do
+    end do
+
+    !------------------------!
+    !   alfa = (r,z)/(p,q)   !
+    !------------------------!
+    alfa = dot_product(p1(1:ni), q1(1:ni))
+    call Comm_Mod_Global_Sum_Real(alfa)
+    alfa = rho/alfa
+
+    !---------------------!
+    !   x = x + alfa p    !
+    !   r = r - alfa Ap   !
+    !---------------------!
+    x (1:ni) = x (1:ni) + alfa * p1(1:ni)
+    r1(1:ni) = r1(1:ni) - alfa * q1(1:ni)
+
+    !-----------------------!
+    !   Check convergence   !
+    !-----------------------!
+    if(.not. present(norm)) then
+      res = Sol % Normalized_Root_Mean_Square(ni, r1(1:nt), A, x(1:nt))
+    else
+      res = Sol % Normalized_Root_Mean_Square(ni, r1(1:nt), A, x(1:nt), norm)
+    end if
+
+    if(res < tol) goto 1
+
+    rho_old = rho
+
+  end do ! iter
+
+  !----------------------------------!
+  !                                  !
+  !   Convergence has been reached   !
+  !                                  !
+  !----------------------------------!
+1 continue
+
+  !-------------------------------------------!
+  !   Refresh the solution vector's buffers   !
+  !-------------------------------------------!
+  call A % pnt_grid % Exchange_Cells_Real(x(-nb:ni))
+
+  fin_res = res
+  niter   = iter
+
+  end subroutine
