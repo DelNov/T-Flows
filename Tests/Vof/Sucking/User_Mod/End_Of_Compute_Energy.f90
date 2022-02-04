b3b7938fb65d21870f928f73b1d15c2b7403eeb6
Niceno <bojan.niceno.scientist@gmail.com>
2022-02-04 12:35:01 +0100

    Expanded: writes temperature profiles at each dt
    On branch bojan
    
    modified:   User_Mod/End_Of_Compute_Energy.f90

--- Tests/Vof/Sucking/User_Mod/End_Of_Compute_Energy.f90
+++ Tests/Vof/Sucking/User_Mod/End_Of_Compute_Energy.f90
@@ -13,28 +13,77 @@
   integer, intent(in)         :: ini      ! inner iteration
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type), pointer :: Grid
-  integer                  :: s, fu
+  integer                  :: c1, c2, s, fui, fut
+  character(33)            :: profile_name
   logical, save            :: first_entry = .true.
 !==============================================================================!
 
   ! Take aliases
   Grid => Flow % pnt_grid
 
+  !----------------------------------------!
+  !   Write out temperature distribution   !
+  !----------------------------------------!
+  profile_name = 'temperature_distribution_0000.num'
+  write(profile_name(26:29), '(i4.4)')  curr_dt
+  call File % Open_For_Writing_Ascii(profile_name, fut)
+
+  do s = 1, Grid % n_faces
+    c1 = Grid % faces_c(1, s)
+    c2 = Grid % faces_c(2, s)
+    if(c2 > 0) then
+      if(Math % Approx_Real(Grid % yf(s), 0.0) .and.  &
+         Math % Approx_Real(Grid % zf(s), 0.0)) then
+        write(fut, '(99(es12.4))') Grid % xc(c1), Flow % t % n(c1)
+      end if
+      if(any(Vof % Front % face_at_elem(1:2,s) .ne. 0)) then
+        if(Math % Approx_Real(Grid % ys(s), 0.0) .and.  &
+           Math % Approx_Real(Grid % zs(s), 0.0)) then
+          write(fut, '(99(es12.4))') Grid % xs(s), Vof % t_sat
+        end if
+      end if
+    end if
+  end do
+
+  close(fut)
+
+  !-------------------------------------!
+  !   Write out temperature gradients   !
+  !-------------------------------------!
+  profile_name = 'temperature_gradients_0000.num'
+  write(profile_name(23:26), '(i4.4)')  curr_dt
+  call File % Open_For_Writing_Ascii(profile_name, fut)
+
+  do s = 1, Grid % n_faces
+    c1 = Grid % faces_c(1, s)
+    c2 = Grid % faces_c(2, s)
+    if(c2 > 0) then
+      if(Math % Approx_Real(Grid % yf(s), 0.0) .and.  &
+         Math % Approx_Real(Grid % zf(s), 0.0)) then
+        write(fut, '(99(es12.4))') Grid % xc(c1), Flow % t % x(c1)
+      end if
+    end if
+  end do
+
+  close(fut)
+
+  !----------------------------------!
+  !   Write out interface position   !
+  !----------------------------------!
   if(first_entry) then
-    call File % Open_For_Writing_Ascii('sucking_solution.dat', fu)
+    call File % Open_For_Writing_Ascii('interface_position.num', fui)
   else
-    call File % Append_For_Writing_Ascii('sucking_solution.dat', fu)
+    call File % Append_For_Writing_Ascii('interface_position.num', fui)
   end if
 
   do s = 1, Grid % n_faces
-
     if(any(Vof % Front % face_at_elem(1:2,s) .ne. 0)) then
 
       ! Write down Stefan's solution
       if(ini .eq. 1                            .and.  &
-         Math % Approx_Real(grid % ys(s), 0.0) .and.  &
-         Math % Approx_Real(grid % zs(s), 0.0)) then
-        write(fu,  '(99(es12.4))') curr_dt * Flow % dt, Grid % xs(s)
+         Math % Approx_Real(Grid % ys(s), 0.0) .and.  &
+         Math % Approx_Real(Grid % zs(s), 0.0)) then
+        write(fui,  '(99(es12.4))') curr_dt * Flow % dt, Grid % xs(s)
       end if
 
     end if
@@ -43,6 +92,6 @@
 
   first_entry = .false.
 
-  close(fu)
+  close(fui)
 
   end subroutine
