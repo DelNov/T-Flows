9a45accc8bd579c24b8bfda22c508716da760e1d
Niceno <bojan.niceno.scientist@gmail.com>
2022-03-02 11:29:58 +0100

    COOL!  Divide now accepts command line arguments.
    On branch bojan_petsc_solvers_almost_alpha
    
    modified:   Divide/Main_Div.f90

--- Sources/Divide/Main_Div.f90
+++ Sources/Divide/Main_Div.f90
@@ -12,15 +12,42 @@
   type(Grid_Type) :: Grid           ! grid to be divided
   integer         :: n_sub
   real            :: start, finish  ! variables to time the program
+  character(SL)   :: arg
 !==============================================================================!
 
   call cpu_time(start)
 
   call Logo_Div
 
-  print *, '# Input problem name: (without extension)'
-  call File % Read_Line(5)
-  read(line % tokens(1), *)  problem_name(1)
+  !-------------------------------------------!
+  !   User specified command line arguments   !
+  !-------------------------------------------!
+  if(command_argument_count() .eq. 2) then
+
+    call get_command_argument(1, arg)
+    problem_name(1) = arg
+
+    call get_command_argument(2, arg)
+    read(arg, *) n_sub
+
+  !-----------------------------------------!
+  !    User didn't specify command line     !
+  !   arguments, prompt him/her for input   !
+  !-----------------------------------------!
+  else
+
+    print *, '# Input problem name: (without extension)'
+    call File % Read_Line(5)
+    read(line % tokens(1), *)  problem_name(1)
+
+    print *, '# Number of subdomains:'
+    read(*,*)  n_sub
+
+  end if
+
+  !-------------------------------!
+  !   Perform the decomposition   !
+  !-------------------------------!
 
   ! Load the finite volume grid
   call Grid % Load_Cfn(0)
@@ -28,9 +55,6 @@
 
   ! Initialize processor numbers (poor idea to put it here)
   Grid % Comm % cell_proc(-Grid % n_bnd_cells:Grid % n_cells) = 1
-
-  print *, '# Number of subdomains:'
-  read(*,*)  n_sub
 
   call Grid % Decompose(n_sub)
 
