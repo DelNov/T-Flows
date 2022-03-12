399f9673308e4a65a2395343a7ebac974d9596e9
Niceno <bojan.niceno.scientist@gmail.com>
2022-03-12 15:12:17 +0100

    NEAT: Can be called with command line arguments
    On branch bojan_petsc_solvers_almost_alpha
    
    modified:   Permutations.f90

--- Sources/Utilities/Permutations.f90
+++ Sources/Utilities/Permutations.f90
@@ -1,57 +1,88 @@
-
-subroutine Swap_Int(a, b)
-
+!==============================================================================!
+  subroutine Swap_Int(a, b)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
   integer :: a, b
+!-----------------------------------[Locals]-----------------------------------!
   integer :: t
+!==============================================================================!
 
   t = a
   a = b
   b = t
 
-end subroutine
+  end subroutine
 
-recursive subroutine Generate(k, A)
-
+!==============================================================================!
+  recursive subroutine Generate(k, a)
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
   integer :: k
-  integer :: A(:)
+  integer :: a(:)
+!-----------------------------------[Locals]-----------------------------------!
+  integer :: i
+!==============================================================================!
 
   if(k .eq. 1) then
-    print *, A
+    print *, a
   else
     ! Generate permutations with kth unaltered
-    ! Initially k == length(A)
-    call Generate(k - 1, A)
+    ! Initially k == length(a)
+    call Generate(k - 1, a)
 
     ! Generate permutations for kth swapped with each k-1 initial
     do i = 1, k - 1
       ! Swap choice dependent on parity of k (even or odd)
       if(mod(k,2) .eq. 0) then
-        call Swap_Int(A(i), A(k)) !  zero-indexed, the kth is at k-1
+        call Swap_Int(a(i), a(k)) !  zero-indexed, the kth is at k-1
       else
-        call Swap_Int(A(1), A(k))
+        call Swap_Int(a(1), a(k))
      end if
-     call Generate(k - 1, A)
+     call Generate(k - 1, a)
 
     end do
   end if
 
-end subroutine
+  end subroutine
 
-program Caller
-
+!==============================================================================!
+  program Caller
+!----------------------------------[Calling]-----------------------------------!
   interface
-    recursive subroutine Generate(k, A)
+    recursive subroutine Generate(k, a)
     integer :: k
-    integer :: A(:)
+    integer :: a(:)
     end subroutine
   end interface
+!-----------------------------------[Locals]-----------------------------------!
+  integer              :: i, k
+  integer, allocatable :: a(:)
+  character(80)        :: arg
+!==============================================================================!
 
+  ! Proper invocation
+  if(command_argument_count() .eq. 1) then
 
-  integer :: k
+    ! Get k
+    call get_command_argument(1, arg);  read(arg, *) k
 
-  integer :: A(3) = (/1, 2, 3/)
+    allocate(a(k))
+    do i = 1, k
+      a(i) = i
+    end do
 
-  call Generate(3, A)
+    call Generate(k, a)
 
-end program
+  ! Wrong invocation
+  else
+    print *, '# You failed to invoke the program properly.'
+    print *, '# Correct invocation:'
+    print *, './Permutations  number'
+    print *, '# where "number" is the range for permutations'
+    stop
+  end if
+
+  end program
 
