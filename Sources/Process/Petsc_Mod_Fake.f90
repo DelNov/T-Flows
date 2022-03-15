bb67914c4adcee0e95e358e2be121918ed7c8ce8
Niceno <bojan.niceno.scientist@gmail.com>
2022-03-15 05:51:18 +0100

    Got rid of procedure definitions inside modules
    That is, procedure definitions should be in the sub-folder
    with the same name as the module, and included from the
    definition of the module.
    
    If it is not like that, PyFA gets confused and makes messy
    diagrams.
    
    On branch bojan_petsc_solvers_almost_alpha
    
    modified:   Process/Petsc_Mod_Fake.f90
    new file:   Process/Petsc_Mod_Fake/Create_Petsc.f90
    new file:   Process/Petsc_Mod_Fake/Solve_Petsc.f90
    modified:   Process/Petsc_Mod_True.f90
    new file:   Process/Petsc_Mod_True/Create_Petsc.f90
    new file:   Process/Petsc_Mod_True/Solve_Petsc.f90
    modified:   Process/makefile_explicit_dependencies
    modified:   Shared/Swap_Mod.f90
    new file:   Shared/Swap_Mod/Int.f90
    new file:   Shared/Swap_Mod/Real.f90

--- Sources/Process/Petsc_Mod_Fake.f90
+++ Sources/Process/Petsc_Mod_Fake.f90
@@ -9,8 +9,6 @@
   implicit none
 !==============================================================================!
 
-  logical, parameter :: PETSC_ACTIVE = .false.
-
   !----------------!
   !   Petsc type   !
   !----------------!
@@ -27,63 +25,11 @@
 
   end type
 
+  logical, parameter :: PETSC_ACTIVE = .false.
+
   contains
 
-!==============================================================================!
-  subroutine Create_Petsc(Pet, Nat, Grid)
-!------------------------------------------------------------------------------!
-  implicit none
-!---------------------------------[Arguments]----------------------------------!
-  class(Petsc_Type)        :: Pet
-  type(Native_Type)        :: Nat
-  type(Grid_Type),  target :: Grid
-!==============================================================================!
-
-  Pet % pnt_grid => Grid
-
-  if(this_proc < 2) then
-    print *, '# NOTE! This version was compiled without PETSc, so the '  //  &
-             'PETSc class was not created ...'
-    print *, '# ... which is OK as long as you don''t specify to use ' //  &
-             'PETSc solvers in control file.'
-  end if
-
-  end subroutine
-
-!==============================================================================!
-  subroutine Solve_Petsc(Pet,                      &
-                         solver, prec, prec_opts,  &
-                         A, x, b,                  &
-                         miter, niter,             &
-                         tol, fin_res,             &
-                         norm)
-!------------------------------------------------------------------------------!
-  implicit none
-!---------------------------------[Arguments]----------------------------------!
-  class(Petsc_Type)    :: Pet
-  character(*)         :: solver          ! solver
-  character(*)         :: prec            ! preconditioner
-  character(SL)        :: prec_opts(MSI)  ! preconditioner options
-  type(Matrix_Type)    :: A
-  real                 :: x(-Pet % pnt_grid % n_bnd_cells :  &
-                             Pet % pnt_grid % n_cells)
-  real                 :: b( Pet % pnt_grid % n_cells)
-  integer, intent(in)  :: miter
-  integer, intent(out) :: niter
-  real,    intent(in)  :: tol      ! tolerance
-  real,    intent(out) :: fin_res  ! final residual
-  real,    optional    :: norm     ! normalization
-!==============================================================================!
-
-  if(this_proc < 2) then
-    print *, '# This version was compiled without PETSc, '  //  &
-             'and yet they were specified in the control file.'
-    print *, '# This error is critical, exiting. Fix the control file.'
-  end if
-
-  call Comm_Mod_End
-  stop
-
-  end subroutine
+    include 'Petsc_Mod_Fake/Create_Petsc.f90'
+    include 'Petsc_Mod_Fake/Solve_Petsc.f90'
 
   end module
