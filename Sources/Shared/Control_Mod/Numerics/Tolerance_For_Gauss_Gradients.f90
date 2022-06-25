4df90042f4a726eac473dd6ca1d30240ef731786
Niceno <bojan.niceno.scientist@gmail.com>
2022-06-25 07:14:28 +0200

    NEAT: As the case of horizonatl cylinder taught me the tolerance for pressure gradients should be the same as tolerance for pressure solver.  It is very apparent, actuall, what is the point of solving pressure with high accuracy if you are going to use inaccurrate pressure gradients.
    On branch has_pressure
    
    modified:   ../Documentation/all_control_keywords
    modified:   Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90

--- Sources/Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90
+++ Sources/Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90
@@ -3,11 +3,11 @@
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  real              :: val
+  real, intent(out) :: val
   logical, optional :: verbose
 !==============================================================================!
 
   call Control_Mod_Read_Real_Item('TOLERANCE_FOR_GAUSS_GRADIENTS',  &
-                                   1.0e-3, val, verbose)
+                                   1.0e-6, val, verbose)
 
   end subroutine
