5826793f05ad1393c1221e2badeaa0d06e28e225
Niceno <bojan.niceno.scientist@gmail.com>
2022-06-27 02:07:30 +0200

    Reverted Gauss gradient tolerance back to 1.0e-3
    On branch has_pressure
    
    modified:   Documentation/all_control_keywords
    modified:   Sources/Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90

--- Sources/Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90
+++ Sources/Shared/Control_Mod/Numerics/Tolerance_For_Gauss_Gradients.f90
@@ -8,6 +8,6 @@
 !==============================================================================!
 
   call Control_Mod_Read_Real_Item('TOLERANCE_FOR_GAUSS_GRADIENTS',  &
-                                   1.0e-6, val, verbose)
+                                   1.0e-3, val, verbose)
 
   end subroutine
