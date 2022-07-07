ac447116a061fd3847be058e2e8183815408899b
Niceno <bojan.niceno.scientist@gmail.com>
2022-07-07 09:15:15 +0200

    Added User_Mod_Bulk_Velocity function
    On branch has_pressure
    
    modified:   User_Mod.f90
    new file:   User_Mod/Bulk_Velocity.f90

---
+++ Sources/Process/User_Mod/Bulk_Velocity.f90
@@ -0,0 +1,15 @@
+!==============================================================================!
+  subroutine User_Mod_Bulk_Velocity(Flow, bulk_vel)
+!------------------------------------------------------------------------------!
+!   This function is called after bulk velocity is computed in the function    !
+!   Convective_Outflow, so that user can specify custom bulk velocity.  This   !
+!   is usefull in non-trivial geometries where monitoring planes cannot be     !
+!   defined in an obvious way.                                                 !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type) :: Flow
+  real             :: bulk_vel
+!==============================================================================!
+
+  end subroutine
