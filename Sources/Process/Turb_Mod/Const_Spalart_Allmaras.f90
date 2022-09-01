9f3b2f0a0fc63880f02b27513910a57ec8ce5524
Niceno <bojan.niceno.scientist@gmail.com>
2022-09-01 10:18:27 +0200

    Cosmetic improvements in programming lines.
    On branch has_pressure
    
    modified:   Process/Turb_Mod/Const_Spalart_Allmaras.f90

--- Sources/Process/Turb_Mod/Const_Spalart_Allmaras.f90
+++ Sources/Process/Turb_Mod/Const_Spalart_Allmaras.f90
@@ -10,19 +10,19 @@
 
   Turb % vis % sigma = TWO_THIRDS
 
-  kappa  = 0.41
-  c_b1   = 0.1355
-  c_b2   = 0.622
-  c_v1   = 7.1
-  c_w1   = c_b1 / kappa**2 + (1 + c_b2) / Turb % vis % sigma
-  c_w2   = 0.3
-  c_w3   = 2.0
+  kappa = 0.41
+  c_b1  = 0.1355
+  c_b2  = 0.622
+  c_v1  = 7.1
+  c_w1  = c_b1 / kappa**2 + (1 + c_b2) / Turb % vis % sigma
+  c_w2  = 0.3
+  c_w3  = 2.0
 
   ! Constants for GGDH turbulent flux
   c_theta =  0.22
 
   ! Constants for AFM turbulent flux
-  afm_psi      = 0.1 
-  afm_eta      = 0.1 
+  afm_psi = 0.1
+  afm_eta = 0.1
 
   end subroutine
