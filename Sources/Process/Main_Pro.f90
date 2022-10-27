4c07acfc8c05d8f90b5dcb452e42f62c4a377d94
Niceno <bojan.niceno.scientist@gmail.com>
2022-10-27 18:52:52 +0200

    Increase number of real cells back to 30
    On branch adjust_first_dim
    
    modified:   Process/Main_Pro.f90

--- Sources/Process/Main_Pro.f90
+++ Sources/Process/Main_Pro.f90
@@ -111,7 +111,7 @@
   call Control_Mod_Switch_To_Root()
 
   ! Allocate memory for working arrays
-  call Work % Allocate_Work(Grid, rc=16, rf=6, rn=2, ic=4, if=6, in=2)
+  call Work % Allocate_Work(Grid, rc=30, rf=6, rn=2, ic=4, if=6, in=2)
 
   ! Initialize first and current and read the last time step
   curr_dt  = 0
@@ -446,7 +446,8 @@
     call User_Mod_Before_Exit(Grid(d))
   end do
 
-  ! Looks a bit ugly: call Work % Finalize_Work()
+  ! Looks a bit ugly
+  call Work % Finalize_Work()
 
   ! Finalize the program profler
   call Profiler % Stop('Main')
