c5da34661c314842af4eb2ce925b383fa09c86d0
Niceno <bojan.niceno.scientist@gmail.com>
2022-10-28 04:40:24 +0200

    Output fractionally nicer.
    On branch adjust_first_dim
    
    modified:   Shared/Work_Mod/Finalize_Work.f90

--- Sources/Shared/Work_Mod/Finalize_Work.f90
+++ Sources/Shared/Work_Mod/Finalize_Work.f90
@@ -6,22 +6,30 @@
   class(Work_Type) :: Work
 !==============================================================================!
 
-  print '(a)', ' #===========================#'
-  print '(a)', ' #       Work_Mod usage      #'
-  print '(a)', ' #---------------------------#'
-  print '(a,i2,a3,i2,a2)', ' # Real cell arrays: ', Work % max_r_cell, ' / ',  &
-                                                    Work % req_r_cell, ' #'
-  print '(a,i2,a3,i2,a2)', ' # Real face arrays: ', Work % max_r_face, ' / ',  &
-                                                    Work % req_r_face, ' #'
-  print '(a,i2,a3,i2,a2)', ' # Real node arrays: ', Work % max_r_node, ' / ',  &
-                                                    Work % req_r_node, ' #'
-  print '(a,i2,a3,i2,a2)', ' # Int. cell arrays: ', Work % max_i_cell, ' / ',  &
-                                                    Work % req_i_cell, ' #'
-  print '(a,i2,a3,i2,a2)', ' # Int. face arrays: ', Work % max_i_face, ' / ',  &
-                                                    Work % req_i_face, ' #'
-  print '(a,i2,a3,i2,a2)', ' # Int. node arrays: ', Work % max_i_node, ' / ',  &
-                                                    Work % req_i_node, ' #'
-  print '(a)', ' #---------------------------#'
+  if(this_proc < 2) then
+    print '(a)', ' #==============================#'
+    print '(a)', ' #        Work_Mod usage        #'
+    print '(a)', ' #------------------------------#'
+    print '(a,i2,a3,i2,a3)', ' # - Real cell arrays: ',  &
+                            Work % max_r_cell, ' / ',    &
+                            Work % req_r_cell, '  #'
+    print '(a,i2,a3,i2,a3)', ' # - Real face arrays: ',  &
+                            Work % max_r_face, ' / ',    &
+                            Work % req_r_face, '  #'
+    print '(a,i2,a3,i2,a3)', ' # - Real node arrays: ',  &
+                            Work % max_r_node, ' / ',    &
+                            Work % req_r_node, '  #'
+    print '(a,i2,a3,i2,a3)', ' # - Int. cell arrays: ',  &
+                            Work % max_i_cell, ' / ',    &
+                            Work % req_i_cell, '  #'
+    print '(a,i2,a3,i2,a3)', ' # - Int. face arrays: ',  &
+                            Work % max_i_face, ' / ',    &
+                            Work % req_i_face, '  #'
+    print '(a,i2,a3,i2,a3)', ' # - Int. node arrays: ',  &
+                            Work % max_i_node, ' / ',    &
+                            Work % req_i_node, '  #'
+    print '(a)', ' #------------------------------#'
+  end if
 
   end subroutine
 
