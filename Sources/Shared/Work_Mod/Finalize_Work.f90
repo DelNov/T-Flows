50d1d0de56fce60e8b0e5fd2482e014f69d8166a
Niceno <bojan.niceno.scientist@gmail.com>
2022-10-28 05:29:12 +0200

    Introduced indentation.
    On branch adjust_first_dim
    
    modified:   Sources/Shared/Work_Mod/Finalize_Work.f90

--- Sources/Shared/Work_Mod/Finalize_Work.f90
+++ Sources/Shared/Work_Mod/Finalize_Work.f90
@@ -4,31 +4,48 @@
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   class(Work_Type) :: Work
+!------------------------------[Local parameters]------------------------------!
+  integer, parameter :: T = 10
+!-----------------------------------[Locals]-----------------------------------!
+  character(DL) :: line
 !==============================================================================!
 
   if(this_proc < 2) then
-    print '(a)', ' #==============================#'
-    print '(a)', ' #        Work_Mod usage        #'
-    print '(a)', ' #------------------------------#'
-    print '(a,i2,a3,i2,a3)', ' # - Real cell arrays: ',  &
-                            Work % max_r_cell, ' / ',    &
-                            Work % req_r_cell, '  #'
-    print '(a,i2,a3,i2,a3)', ' # - Real face arrays: ',  &
-                            Work % max_r_face, ' / ',    &
-                            Work % req_r_face, '  #'
-    print '(a,i2,a3,i2,a3)', ' # - Real node arrays: ',  &
-                            Work % max_r_node, ' / ',    &
-                            Work % req_r_node, '  #'
-    print '(a,i2,a3,i2,a3)', ' # - Int. cell arrays: ',  &
-                            Work % max_i_cell, ' / ',    &
-                            Work % req_i_cell, '  #'
-    print '(a,i2,a3,i2,a3)', ' # - Int. face arrays: ',  &
-                            Work % max_i_face, ' / ',    &
-                            Work % req_i_face, '  #'
-    print '(a,i2,a3,i2,a3)', ' # - Int. node arrays: ',  &
-                            Work % max_i_node, ' / ',    &
-                            Work % req_i_node, '  #'
-    print '(a)', ' #------------------------------#'
+    line( 1:160) = ' '
+
+    write(line(T+1:T+33),'(a)'), ' #==============================#'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a)'), ' #        Work_Mod usage        #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a)'), ' #------------------------------#'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Real cell arrays: ',  &
+                                             Work % max_r_cell, ' / ',   &
+                                             Work % req_r_cell, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Real face arrays: ',  &
+                                             Work % max_r_face, ' / ',   &
+                                             Work % req_r_face, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Real node arrays: ',  &
+                                             Work % max_r_node, ' / ',   &
+                                             Work % req_r_node, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Int. cell arrays: ',  &
+                                             Work % max_i_cell, ' / ',   &
+                                             Work % req_i_cell, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Int. face arrays: ',  &
+                                             Work % max_i_face, ' / ',   &
+                                             Work % req_i_face, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a,i2,a3,i2,a3)'), ' # - Int. node arrays: ',  &
+                                             Work % max_i_node, ' / ',   &
+                                             Work % req_i_node, '  #'
+    print '(a)', trim(line)
+    write(line(T+1:T+33),'(a)'), ' #------------------------------#'
+    print '(a)', trim(line)
+
   end if
 
   end subroutine
