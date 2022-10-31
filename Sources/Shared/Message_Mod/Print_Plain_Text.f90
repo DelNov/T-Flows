6de90cc0eb2f687f504eecb0348afed7f707fc70
Niceno <bojan.niceno.scientist@gmail.com>
2022-10-31 16:11:52 +0100

    Skips to new lines when it encounters \n in input.
    I had to change do...end do with do while()...end do
    to achieve that.
    
    On branch adjust_first_dim
    
    modified:   Shared/Message_Mod/Print_Plain_Text.f90

--- Sources/Shared/Message_Mod/Print_Plain_Text.f90
+++ Sources/Shared/Message_Mod/Print_Plain_Text.f90
@@ -12,7 +12,9 @@
 !==============================================================================!
 
   !---------------------------------------------------!
+  !                                                   !
   !   Load the argument into tokenizer and parse it   !
+  !                                                   !
   !---------------------------------------------------!
   Tok % whole = message_text
   call Tok % Parse()
@@ -22,29 +24,49 @@
   line(2:2) = '#'
   cur_p = 4
 
-  ! Browse through tokens and print them one by one
-  do i = 1, Tok % n_tokens
+  !-----------------------------------------------------!
+  !                                                     !
+  !   Browse through tokens and print them one by one   !
+  !                                                     !
+  !-----------------------------------------------------!
+  i = 1
+  do while (i .le. Tok % n_tokens)
 1   continue
     nex_p = cur_p + len_trim(Tok % tokens(i))
 
-    ! Keep on filling up this line, it still fits
-    if(nex_p < width) then
+    !-------------------------------------------------!
+    !   Keep on filling up this line, it still fits   !
+    !-------------------------------------------------!
+    if(nex_p < width .and. Tok % tokens(i) .ne. '\n') then
       write(line(cur_p:nex_p), '(a)')  trim(Tok % tokens(i)) // ' '
 
-    ! Line is too long, print it and reset for new tokens
+    !---------------------------------------------------------!
+    !   Line is too long, print it and reset for new tokens   !
+    !---------------------------------------------------------!
     else
+
+      ! Print what you have up to now
       print '(a)', trim(line)
 
+      ! (Re)initialize the line
       line      = ' '
       line(2:2) = '#'
       cur_p = 4
+
+      ! If you came here because of new line character, skip it.
+      if(Tok % tokens(i) .eq. '\n') i = i + 1
       goto 1
     end if
 
     cur_p = nex_p + 1
+    i = i + 1
   end do
 
-  ! Print the last line which was still in the forming
+  !--------------------------------------------------------!
+  !                                                        !
+  !   Print the last line which was still in the forming   !
+  !                                                        !
+  !--------------------------------------------------------!
   print '(a)', trim(line)
 
   end subroutine
