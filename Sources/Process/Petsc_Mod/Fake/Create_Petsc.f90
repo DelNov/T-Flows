311d29bec7200b20701339988f00cd839c0de379
Niceno <bojan.niceno.scientist@gmail.com>
2022-11-07 02:09:06 +0100

    Improved the output for Intel compiler.
    On branch boil_bojan
    
    modified:   Petsc_Mod/Fake/Create_Petsc.f90

--- Sources/Process/Petsc_Mod/Fake/Create_Petsc.f90
+++ Sources/Process/Petsc_Mod/Fake/Create_Petsc.f90
@@ -11,10 +11,10 @@
   Pet % pnt_grid => Grid
 
   if(this_proc < 2) then
-    print *, '# NOTE! This version was compiled without PETSc, so the '  //  &
-             'PETSc class was not created ...'
-    print *, '# ... which is OK as long as you don''t specify to use ' //  &
-             'PETSc solvers in control file.'
+    print '(a)', ' # NOTE! This version was compiled without PETSc,'  //  &
+                 ' so the PETSc class was not created ...'
+    print '(a)', ' # ... which is OK as long as you don''t specify'   //  &
+                 ' to use PETSc solvers in control file.'
   end if
 
   end subroutine
