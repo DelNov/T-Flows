ae49610dfb65c712a3830edc4386e7691f95de4a
Niceno <bojan.niceno.scientist@gmail.com>
2022-11-21 11:59:51 +0100

    Added Porosity_Mod to User_Mod
    On branch boil_bojan
    
    modified:   User_Mod.f90

--- Sources/Process/User_Mod.f90
+++ Sources/Process/User_Mod.f90
@@ -4,6 +4,7 @@
 !   This is embrio of a future User module, a place where user can
 !   define his/her variables and pass them around his functions
 !------------------------------------------------------------------------------!
+  use Porosity_Mod
   use Swarm_Mod
   use Interface_Mod
 !------------------------------------------------------------------------------!
