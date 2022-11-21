eb87a6dd1f5825f152bb7cc0b5c574acc55d8800
Niceno <bojan.niceno.scientist@gmail.com>
2022-11-21 16:07:04 +0100

    Further steps in Tests/Les/Forrest case
    On branch boil_bojan
    
    deleted:    Les/Forrest/User_Mod/Beginning_Of_Simulation.f90
    new file:   Les/Forrest/User_Mod/Beginning_Of_Time_Step.f90
    new file:   Les/Forrest/User_Mod/End_Of_Time_Step.f90
    new file:   Les/Forrest/User_Mod/Force.f90
    new file:   Les/Forrest/User_Mod/Save_Results.f90
    deleted:    Les/Forrest/User_Mod/Types.f90
    modified:   Les/Forrest/control
    modified:   Les/Forrest/convert.scr

---
+++ Tests/Les/Forrest/User_Mod/Beginning_Of_Time_Step.f90
@@ -0,0 +1,41 @@
+!==============================================================================!
+  subroutine User_Mod_Beginning_Of_Time_Step(Flow, Turb, Vof, Swarm,  &
+                                             curr_dt, time)
+!------------------------------------------------------------------------------!
+!   This function is called at the beginning of time step.                     !
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Field_Type),    target :: Flow
+  type(Turb_Type),     target :: Turb
+  type(Vof_Type),      target :: Vof
+  type(Swarm_Type),    target :: Swarm
+  integer, intent(in)         :: curr_dt  ! time step
+  real,    intent(in)         :: time     ! physical time
+!-----------------------------------[Locals]-----------------------------------!
+  type(Grid_Type), pointer :: Grid
+  type(Var_Type),  pointer :: u, v, w, t, phi
+  integer                  :: c1, c2, s
+!==============================================================================!
+
+  ! Take aliases
+  Grid => Flow % pnt_grid
+  u    => Flow % u
+  v    => Flow % u
+  w    => Flow % u
+
+  ! Impose slip condition
+  do s = 1, Grid % n_faces
+    c1 = Grid % faces_c(1,s)
+    c2 = Grid % faces_c(2,s)
+    if(c2 < 0) then
+      if(Grid % Bnd_Cond_Name(c2) .eq. 'TOP') then
+        u % n(c2) = u % n(c1)
+        v % n(c2) = v % n(c1)
+        w % n(c2) = 0.0
+      end if
+    end if ! c2 < 0
+  end do
+
+  end subroutine
+
