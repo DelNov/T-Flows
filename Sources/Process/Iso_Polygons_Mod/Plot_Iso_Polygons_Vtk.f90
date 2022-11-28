5e90a4d8e8dda74c0e0317662dcfbf083b0a343d
Niceno <bojan.niceno.scientist@gmail.com>
2022-11-28 16:03:37 +0100

    Modernized the way files are opened with "newunit"
    On branch boil_bojan
    
    modified:   Iso_Polygons_Mod/Plot_Iso_Polygons_Vtk.f90
    modified:   Polyhedron_Mod/Plot_Polyhedron_Vtk.f90

--- Sources/Process/Iso_Polygons_Mod/Plot_Iso_Polygons_Vtk.f90
+++ Sources/Process/Iso_Polygons_Mod/Plot_Iso_Polygons_Vtk.f90
@@ -6,54 +6,54 @@
   class(Iso_Polygons_Type) :: Iso
   integer, intent(in)      :: rank
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: i, ip, is, iv
+  integer           :: i, ip, is, iv, fu
   integer           :: npoly, ndata, ntp
   character(len=80) :: filename  ! don't use SL for separate compilation
 !==============================================================================!
 
   write(filename,'("iso-",i7.7,".vtk")') rank
-  open(11, file=filename)
+  open(newunit=fu, file=filename)
 
-  write(11,'(a26)')     '# vtk DataFile Version 2.0'
-  write(11,'(a6,i7.7)') 'File: ', rank
-  write(11,'(a5)')      'ASCII'
-  write(11,*)           ' '
-  write(11,'(a16)')     'DATASET POLYDATA'
+  write(fu,'(a26)')     '# vtk DataFile Version 2.0'
+  write(fu,'(a6,i7.7)') 'File: ', rank
+  write(fu,'(a5)')      'ASCII'
+  write(fu,*)           ' '
+  write(fu,'(a16)')     'DATASET POLYDATA'
 
   ! Count the vertices (points)
   ntp = 0
   do i = 1, Iso % n_polys
-     ntp = ntp + Iso % polys_n_verts(i)
+    ntp = ntp + Iso % polys_n_verts(i)
   end do
 
   ! Write the points out
-  write(11,'(a6,i7,a6)') 'POINTS', NTP, ' float'
+  write(fu,'(a6,i7,a6)') 'POINTS', NTP, ' float'
   do ip = 1, ntp
-     write(11,'(3f12.6)') Iso % verts_xyz(ip,1),  &
-                          Iso % verts_xyz(ip,2),  &
-                          Iso % verts_xyz(ip,3)
+    write(fu,'(3f12.6)') Iso % verts_xyz(ip,1),  &
+                         Iso % verts_xyz(ip,2),  &
+                         Iso % verts_xyz(ip,3)
   end do
 
   ! Count the polygons and data
   npoly = 0
   ndata = 0
   do is = 1, Iso % n_polys
-     if(Iso % polys_n_verts(is) .gt. 0) then
-        npoly = npoly + 1
-        ndata = ndata + Iso % polys_n_verts(is) + 1
-     end if
+    if(Iso % polys_n_verts(is) .gt. 0) then
+      npoly = npoly + 1
+      ndata = ndata + Iso % polys_n_verts(is) + 1
+    end if
   end do
 
   ! Write polygons and data out
-  write(11,'(a8,i7,i7)') 'POLYGONS', npoly, ndata
+  write(fu,'(a8,i7,i7)') 'POLYGONS', npoly, ndata
   do is = 1, Iso % n_polys
-     if(Iso % polys_n_verts(is) .gt. 0) then
-        write(11,'(i7)') Iso % polys_n_verts(is)
-        do iv = 1, Iso % polys_n_verts(is)
-           write(11,'(i7)') Iso % polys_v(is,iv)-1
-        end do
-     end if
+    if(Iso % polys_n_verts(is) .gt. 0) then
+      write(fu,'(i7)') Iso % polys_n_verts(is)
+      do iv = 1, Iso % polys_n_verts(is)
+        write(fu,'(i7)') Iso % polys_v(is,iv)-1
+      end do
+    end if
   end do
-  close(11)
+  close(fu)
 
   end
--- Sources/Process/Polyhedron_Mod/Plot_Polyhedron_Vtk.f90
+++ Sources/Process/Polyhedron_Mod/Plot_Polyhedron_Vtk.f90
@@ -8,72 +8,76 @@
   character(*)           :: head
   integer, intent(in)    :: rank
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: ip, is, iv, ndata, npoly
+  integer           :: ip, is, iv, fu, ndata, npoly
   character(len=80) :: filename  ! don't use SL for separate compilation
 !==============================================================================!
 
   write(filename,'(a,"-",i7.7,".vtk")') trim(head), rank
 
-  open(11, file=filename)
-  write(11,'(a26)')     '# vtk DataFile Version 2.0'
-  write(11,'(a6,i7.7)') 'File: ', rank
-  write(11,'(a5)')      'ASCII'
-  write(11,*)           ' '
-  write(11,'(a16)')     'DATASET POLYDATA'
+  open(newunit=fu, file=filename)
+  write(fu,'(a26)')     '# vtk DataFile Version 2.0'
+  write(fu,'(a6,i7.7)') 'File: ', rank
+  write(fu,'(a5)')      'ASCII'
+  write(fu,*)           ' '
+  write(fu,'(a16)')     'DATASET POLYDATA'
 
   ! Write the points out
-  write(11,'(a6,i7,a6)') 'POINTS', Pol % n_nodes, ' float'
+  write(fu,'(a6,i7,a6)') 'POINTS', Pol % n_nodes, ' float'
   do ip = 1, Pol % n_nodes
-     write(11,'(3f12.6)') Pol % nodes_xyz(ip,1),  &
-                          Pol % nodes_xyz(ip,2),  &
-                          Pol % nodes_xyz(ip,3)
+    write(fu,'(3f12.6)') Pol % nodes_xyz(ip,1),  &
+                         Pol % nodes_xyz(ip,2),  &
+                         Pol % nodes_xyz(ip,3)
   end do
 
   ! Count the polygons and data
   npoly = 0
   ndata = 0
   do is = 1, Pol % n_faces
-     if(Pol % faces_n_nodes(is) .gt. 0) then
-        npoly = npoly + 1
-        ndata = ndata + Pol % faces_n_nodes(is)+1
-     end if
+    if(Pol % faces_n_nodes(is) .gt. 0) then
+      npoly = npoly + 1
+      ndata = ndata + Pol % faces_n_nodes(is)+1
+    end if
   end do
 
   ! Write polygons and data out
-  write(11,'(a8,i7,i7)') 'POLYGONS', npoly, ndata
+  write(fu,'(a8,i7,i7)') 'POLYGONS', npoly, ndata
   do is = 1, Pol % n_faces
-     if(Pol % faces_n_nodes(is) .gt. 0) then
-        write(11,'(i7)') Pol % faces_n_nodes(is)
-        do iv = 1, Pol % faces_n_nodes(is)
-           write(11,'(i7)') Pol % faces_n(is,iv)-1
-        end do
-     end if
+    if(Pol % faces_n_nodes(is) .gt. 0) then
+      write(fu,'(i5)') Pol % faces_n_nodes(is)
+      do iv = 1, Pol % faces_n_nodes(is)
+        write(fu,'(i7)') Pol % faces_n(is,iv)-1
+      end do
+    end if
   end do
 
   ! Start section for point data
-  write(11,'(a10,i7)') 'POINT_DATA', Pol % n_nodes
+  write(fu,'(a10,i7)') 'POINT_DATA', Pol % n_nodes
 
   ! Write out the phi data
-  write(11,'(a19)')    'SCALARS phi float 1'
-  write(11,'(a20)')    'LOOKUP_TABLE default'
+  write(fu,'(a19)')    'SCALARS phi float 1'
+  write(fu,'(a20)')    'LOOKUP_TABLE default'
   do ip = 1, Pol % n_nodes
-     write(11,'(f12.6)') Pol % phi(ip)
-  end do
-
-  ! Write out the cell number (couldn't do it at cell/polygon)
-  write(11,'(a18)')    'SCALARS cell int 1'
-  write(11,'(a20)')    'LOOKUP_TABLE default'
-  do ip = 1, Pol % n_nodes
-     write(11,'(i9)') rank
+    write(fu,'(f12.6)') Pol % phi(ip)
   end do
 
   ! Write out node indices
-  write(11,'(a18)')    'SCALARS node int 1'
-  write(11,'(a20)')    'LOOKUP_TABLE default'
+  write(fu,'(a18)')    'SCALARS node int 1'
+  write(fu,'(a20)')    'LOOKUP_TABLE default'
   do ip = 1, Pol % n_nodes
-     write(11,'(i7)') ip
+    write(fu,'(i7)') ip
   end do
 
-  close(11)
+  ! Write out the cell number (couldn't do it at cell/polygon)
+  write(fu,'(a9,i7)') 'CELL_DATA', npoly
+
+  write(fu,'(a18)')    'SCALARS cell int 1'
+  write(fu,'(a20)')    'LOOKUP_TABLE default'
+  do is = 1, Pol % n_faces
+    if(Pol % faces_n_nodes(is) .gt. 0) then
+      write(fu,'(i9)') rank
+    end if
+  end do
+
+  close(fu)
 
   end
