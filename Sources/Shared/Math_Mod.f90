dc096159b6d4ecbacd487f1f85429503d9947a4c
Niceno <bojan.niceno.scientist@gmail.com>
2022-11-28 16:14:44 +0100

    Added new member function to Math_Mod
    However, I am not too enthusiastic about it.  It is only
    used when initializing VOF, and even there the usage is
    hardly justified since Isoap libraries use the inverted
    adressing.  So, I don't know, let it be for the time being
    but it is not the most important thing ever.
    
    On branch boil_bojan
    
    modified:   Shared/Math_Mod.f90
    new file:   Shared/Math_Mod/Approx_Three_Reals.f90

--- Sources/Shared/Math_Mod.f90
+++ Sources/Shared/Math_Mod.f90
@@ -19,6 +19,7 @@
 
     contains
       procedure          :: Approx_Real
+      procedure          :: Approx_Three_Reals
       procedure          :: Approx_String
       procedure          :: Cross_Product
       procedure          :: Distance
@@ -47,6 +48,7 @@
   contains
 
 #   include "Math_Mod/Approx_Real.f90"
+#   include "Math_Mod/Approx_Three_Reals.f90"
 #   include "Math_Mod/Approx_String.f90"
 #   include "Math_Mod/Cross_Product.f90"
 #   include "Math_Mod/Distance.f90"
