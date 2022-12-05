7de2b63af6819974964f1b862dcc18f98e259ef7
Niceno <bojan.niceno.scientist@gmail.com>
2022-12-05 20:05:41 +0100

    For the first time managed to initialize from STL
    On branch boil_bojan
    
    modified:   Functionality/Init_Vof_From_Stl/User_Mod/Beginning_Of_Simulation.f90

--- Tests/Functionality/Init_Vof_From_Stl/User_Mod/Beginning_Of_Simulation.f90
+++ Tests/Functionality/Init_Vof_From_Stl/User_Mod/Beginning_Of_Simulation.f90
@@ -30,96 +30,60 @@
   integer, intent(in)         :: curr_dt  ! time step
   real,    intent(in)         :: time     ! physical time
 !------------------------------[Local parameters]------------------------------!
-  integer, parameter :: NO  = 0
-  integer, parameter :: YES = 1
+  integer, parameter :: NO    = 0
+  integer, parameter :: YES   = 1
+  logical, parameter :: DEBUG = .true.
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type), pointer     :: Grid
   type(Stl_Type)               :: Stl
   type(Polyhedron_Type)        :: Pol(0:1)
-  real,    pointer, contiguous :: vof_at_nodes(:), vof_in_cells(:)
   real,    pointer, contiguous :: dis_nod_dom(:), dis_nod(:)
-  integer, pointer, contiguous :: dis_nod_cnt(:), cut_cel(:)
+  integer, pointer, contiguous :: dis_nod_int(:), cut_cel(:), set_cel(:)
   integer                      :: c, s, i_nod, j_nod, i, j, k1, k2, m
-  integer                      :: i_iso, i_ver, i_fac, fac, p, n_cut_facets
-  integer                      :: cut_facets(1024)
+  integer                      :: i_iso, i_ver, i_fac, fac, p
+  integer                      :: n_cut_facets, cut_facets(1024)
   real                         :: vol_1, vol_2, vol_3, vol_4, vol_5
   real                         :: cell_vol, cel0_vol, cel1_vol
   real                         :: p1(3), p2(3), p3(3), qi(3), qj(3), qn(3)
-  real                         :: f(3), n(3), l(3)
+  real                         :: f(3), n(3)
   integer                      :: ij_cut(MAX_ISOAP_VERTS, MAX_ISOAP_VERTS)
-  integer                      :: new_faces_n_nodes
+  integer                      :: new_faces_n_nodes, cnt_p, cnt_m
   integer                      :: new_faces_n(MAX_ISOAP_VERTS)
+  logical                      :: flooding
 !==============================================================================!
 
-  call Work % Connect_Int_Node(dis_nod_cnt)
-  call Work % Connect_Real_Node(vof_at_nodes, dis_nod, dis_nod_dom)
   call Work % Connect_Int_Cell(cut_cel)
-  call Work % Connect_Real_Cell(vof_in_cells)
+  call Work % Connect_Int_Cell(set_cel)
+  call Work % Connect_Int_Node(dis_nod_int)
+  call Work % Connect_Real_Node(dis_nod)
+  call Work % Connect_Real_Node(dis_nod_dom)
 
   ! Take alias(es)
   Grid => Flow % pnt_grid
 
   !-----------------------!
+  !                       !
   !   Read the STL file   !
+  !                       !
   !-----------------------!
   call Stl % Create_From_File("2_spheres.stl")
 
-  !-----------------------!
-  !   Find vof in nodes   !
-  !-----------------------!
-  do i = 1, Grid % n_nodes
-
-    vof_at_nodes(i) = 0  ! assume node is in the stl
-
-    do fac = 1, Stl % N_Facets()
-
-      f = Stl % Facet_Coords(fac)
-      n = Stl % Facet_Normal(fac)
-
-      ! Vector connecting facet centroid with the node
-      qn(1:3) = (/Grid % xn(i)-f(1), Grid % yn(i)-f(2), Grid % zn(i)-f(3)/)
-
-      ! First time this product is positive, node is outside of STL
-      if(dot_product(qn, n) > 0) then
-        vof_at_nodes(i) = vof_at_nodes(i) + 1.0
-      end if
-    end do
-
-    vof_at_nodes(i) = vof_at_nodes(i) / real(Stl % N_Facets())
-    if(vof_at_nodes(i) < 0.01) then
-      vof_at_nodes(i) = 0.0
-    else
-      vof_at_nodes(i) = 1.0
-    end if
-  end do
-  call Grid % Save_Debug_Vtu(append="vof_at_nodes",       &
-                             scalar_node=vof_at_nodes,    &
-                             scalar_name="vof_at_nodes")
-
-  !-----------------------!
-  !   Find vof in cells   !
-  !-----------------------!
-  do c = 1, Grid % n_cells
-    vof_in_cells(c) = 0.0
-    do i_nod = 1, abs(Grid % cells_n_nodes(c))
-      i = Grid % cells_n(i_nod, c)
-      vof_in_cells(c) = vof_in_cells(c) + vof_at_nodes(i)
-    end do
-    vof_in_cells(c) = vof_in_cells(c) / real(abs(Grid % cells_n_nodes(c)))
-  end do
-  ! Exchange is needed here
-  call Grid % Save_Debug_Vtu(append="vof_in_cells_tentative",  &
-                             scalar_cell=vof_in_cells,         &
-                             scalar_name="vof_in_cells")
-
-  !--------------------------!
-  !   Browse through cells   !
-  !--------------------------!
+  !----------------------------------------------------!
+  !                                                    !
+  !   Browse through cells to find out which are cut   !
+  !   If a cell is cut, calculate distance from the    !
+  !   STL file for all of its nodes.  Then normalize   !
+  !   the node distance for all nodes in the grid      !
+  !                                                    !
+  !----------------------------------------------------!
   do c = 1, Grid % n_cells
 
     n_cut_facets  = 0  ! how many facets cut in this cell
     cut_facets(:) = 0
-    cut_cel(c) = NO
+    cut_cel(c)    = NO
+
+    write(*,'(a2,f5.0,a14,a1)', advance='no')  &
+      ' #', (100. * real(c)/real(Grid % n_cells)), ' % complete...', achar(13)
 
     !---------------------------------------------------------------!
     !   Browse through cells' nodes to find out if cells were cut   !
@@ -129,12 +93,16 @@
     do i_nod = 1, abs(Grid % cells_n_nodes(c))
       i = Grid % cells_n(i_nod, c)
 
-      qi(1:3) = (/Grid % xc(c), Grid % yc(c), Grid % zc(c)/)
-      qj(1:3) = (/Grid % xn(i), Grid % yn(i), Grid % zn(i)/)
+      qi(1) = Grid % xc(c)
+      qi(2) = Grid % yc(c)
+      qi(3) = Grid % zc(c)
+      qj(1) = Grid % xn(i)
+      qj(2) = Grid % yn(i)
+      qj(3) = Grid % zn(i)
 
       do fac = 1, Stl % N_Facets()
 
-        ! STL vertex coordinates
+        ! STL vertex coordinates (this is three times slower)
         p1(1:3) = Stl % Facets_Vert_Coords(fac, 1)
         p2(1:3) = Stl % Facets_Vert_Coords(fac, 2)
         p3(1:3) = Stl % Facets_Vert_Coords(fac, 3)
@@ -174,7 +142,9 @@
         dis_nod(i) = 0.0
 
         ! Node coordinates
-        qn(1:3) = (/Grid % xn(i), Grid % yn(i), Grid % zn(i)/)
+        qn(1) = Grid % xn(i)
+        qn(2) = Grid % yn(i)
+        qn(3) = Grid % zn(i)
 
         ! Accumulate distance to the STL facets
         do i_fac = 1, n_cut_facets
@@ -196,18 +166,20 @@
       do i_nod = 1, abs(Grid % cells_n_nodes(c))
         i = Grid % cells_n(i_nod, c)
         dis_nod_dom(i) = dis_nod_dom(i) + dis_nod(i)
-        dis_nod_cnt(i) = dis_nod_cnt(i) + 1
+        dis_nod_int(i) = dis_nod_int(i) + 1
       end do
 
     end if
   end do    ! through cells
 
-  !------------------------------------------------------!
-  !   Equalize distance at nodes for the entire domain   !
-  !------------------------------------------------------!
+  !--------------------------------------------------!
+  !   Equalize distance at nodes for entire domain   !
+  !   (Note that this will spread distances a bit    !
+  !    even to the cells which are not cut.)         !
+  !--------------------------------------------------!
   do i = 1, Grid % n_nodes
-    if(dis_nod_cnt(i) > 0) then
-      dis_nod_dom(i) = dis_nod_dom(i) / real(dis_nod_cnt(i))
+    if(dis_nod_int(i) > 0) then
+      dis_nod_dom(i) = dis_nod_dom(i) / real(dis_nod_int(i))
     else
       dis_nod_dom(i) = 0.0
     end if
@@ -217,17 +189,117 @@
   call Grid % Save_Debug_Vtu(append="dis_nod_dom",       &
                              scalar_node=dis_nod_dom,    &
                              scalar_name="dis_nod_dom")
+  call Grid % Save_Debug_Vtu(append="cut_cell",             &
+                             scalar_cell=real(cut_cel,RP),  &
+                             scalar_name="cut_cel")
 
-  ! Set dis_nod_dom to usable values
+  !-------------------------------------!
+  !                                     !
+  !   Find vof in cells by flood fill   !
+  !                                     !
+  !-------------------------------------!
+
+  ! Set set_cel to be the same as cut_cel as ...
+  ! ... cells which are not cut are also not set
+  set_cel(1:Grid % n_cells) = cut_cel(1:Grid % n_cells)
+
+  ! Turn distances to integers
+  do i = 1, Grid % n_nodes
+    if(Math % Approx_Real(dis_nod_dom(i), 0.0)) then
+      dis_nod_int(i) = 0
+    else
+      if(dis_nod_dom(i) > 0.0) then
+        dis_nod_int(i) = 1
+      else
+        dis_nod_int(i) = -1
+      end if
+    end if
+  end do
+  call Grid % Save_Debug_Vtu(append="dis_nod_int",              &
+                             scalar_node=real(dis_nod_int,RP),  &
+                             scalar_name="dis_nod_int")
+
+  !---------------------------------------!
+  !   The actual flood fill starts here   !
+  !---------------------------------------!
+  print '(a)', ' # Flooding ...'
+  m = 0
+1 continue
+  m = m + 1
+  flooding = .false.
+    write(*,'(a2,f5.0,a14,a1)', advance='no')  &
+      ' #', (100. * real(sum(set_cel))/real(Grid % n_cells)), ' % complete...', achar(13)
+
+  do c = 1, Grid % n_cells
+    if( set_cel(c) .eq. NO ) then
+
+      flooding = .true.      ! flooding is still going on
+      Vof % fun % n(c) = 0.0  ! initialize to zero
+
+      ! Count nodes with integer distance -1 and +1
+      cnt_m = 0
+      cnt_p = 0
+      do i_nod = 1, abs(Grid % cells_n_nodes(c))
+        i = Grid % cells_n(i_nod, c)
+        if(dis_nod_int(i) .eq. +1) cnt_p = cnt_p + 1
+        if(dis_nod_int(i) .eq. -1) cnt_m = cnt_m + 1
+      end do
+
+      ! If there are more positive cell, set VOF to 1
+      if(cnt_p > cnt_m) then
+        Vof % fun % n(c) = 1.0
+        set_cel(c)      = YES  ! now it is set
+        do i_nod = 1, abs(Grid % cells_n_nodes(c))
+          i = Grid % cells_n(i_nod, c)
+          dis_nod_int(i) = +1
+        end do
+
+      ! If there are more negative cell, set VOF to 0
+      else if(cnt_p < cnt_m) then
+        Vof % fun % n(c) = 0.0  ! probably not needed, it was set to 0 above
+        set_cel(c)       = YES  ! now it is set
+        do i_nod = 1, abs(Grid % cells_n_nodes(c))
+          i = Grid % cells_n(i_nod, c)
+          dis_nod_int(i) = -1
+        end do
+
+      ! Throw an error if you come to this
+      else   ! cnt_p == cnt_m
+        if(cnt_p .gt. 0 .and. cnt_m .gt. 0) then
+        PRINT *, 'HOW ON EARTH?  CELL: ', C, CNT_P, CNT_M
+        end if
+      end if
+
+    end if
+  end do
+
+  ! Flood fill still going, go back
+  if(flooding) goto 1
+
+  ! Exchange is needed here
+  if(DEBUG) then
+    call Grid % Save_Debug_Vtu(append="set_cel",                &
+                               scalar_cell=real(set_cel, RP),   &
+                               scalar_name="set_cel")
+    call Grid % Save_Debug_Vtu(append="vof_tent",          &
+                               scalar_cell=Vof % fun % n,  &
+                               scalar_name="vof_tent")
+  end if
+
+  !------------------------------------!
+  !                                    !
+  !   Calculate VOF in all cut cells   !
+  !                                    !
+  !------------------------------------!
+
+  ! Set dis_nod_dom to usable values; values needed by Isoap
   do i = 1, Grid % n_nodes
     dis_nod_dom(i) = dis_nod_dom(i) + 0.5
   end do
 
-  !------------------------------------!
-  !                                    !
-  !   Browse through cut cells again   !
-  !                                    !
-  !------------------------------------!
+  !-----------------------------------!
+  !   Browse through cut cells only   !
+  !-----------------------------------!
   do c = 1, Grid % n_cells
     if( cut_cel(c) .eq. YES ) then
 
@@ -235,7 +307,9 @@
       !   Extract polyhedron   !
       !------------------------!
       call Polyhedron % Extract_From_Grid(Grid, c, dis_nod_dom)
-      call Polyhedron % Plot_Polyhedron_Vtk("dis_nod_dom", c)
+      if(DEBUG) then
+        call Polyhedron % Plot_Polyhedron_Vtk("dis_nod_dom", c)
+      end if
 
       !------------------------------!
       !   Call the Isoap algorithm   !
@@ -305,7 +379,9 @@
         Polyhedron % faces_n_nodes(s) = new_faces_n_nodes
         Polyhedron % faces_n(s, 1:new_faces_n_nodes)  &
                   = new_faces_n(1:new_faces_n_nodes)
-        call Polyhedron % Plot_Polyhedron_Vtk("geo", c)
+        if(DEBUG) then
+          call Polyhedron % Plot_Polyhedron_Vtk("geo", c)
+        end if
 
       end do  ! s
 
@@ -338,8 +414,10 @@
         ! What if there is a face without any nodes between 0 and 0.5+MICRO?
         ! Well, faces_n_nodes will be zero for that face
 
-        if(p .eq. 0) call Pol(0) % Plot_Polyhedron_Vtk("pol0_hollow", c)
-        if(p .eq. 1) call Pol(1) % Plot_Polyhedron_Vtk("pol1_hollow", c)
+        if(DEBUG) then
+          if(p .eq. 0) call Pol(0) % Plot_Polyhedron_Vtk("pol0_hollow", c)
+          if(p .eq. 1) call Pol(1) % Plot_Polyhedron_Vtk("pol1_hollow", c)
+        end if
 
         ! Try to add the missing face
         new_faces_n_nodes = 0
@@ -363,8 +441,10 @@
         Pol(p) % faces_n(s,1:new_faces_n_nodes)  &     ! copy the list of nodes
            = new_faces_n(1:new_faces_n_nodes)
 
-        if(p .eq. 0) call Pol(0) % Plot_Polyhedron_Vtk("pol0_full", c)
-        if(p .eq. 1) call Pol(1) % Plot_Polyhedron_Vtk("pol1_full", c)
+        if(DEBUG) then
+          if(p .eq. 0) call Pol(0) % Plot_Polyhedron_Vtk("pol0_full", c)
+          if(p .eq. 1) call Pol(1) % Plot_Polyhedron_Vtk("pol1_full", c)
+        end if
       end do  ! through p
 
       ! Calculate volume of the Pol(0)
@@ -372,22 +452,24 @@
       call Pol(1)     % Calculate_Cell_Volume(cel1_vol)
       call Polyhedron % Calculate_Cell_Volume(cell_vol)
       if(cel0_vol > cel0_vol) then
-        vof_in_cells(c) = (cell_vol - cel0_vol) / (cell_vol)
+        Vof % fun % n(c) = (cell_vol - cel0_vol) / (cell_vol)
       else
-        vof_in_cells(c) = cel1_vol / (cell_vol)
+        Vof % fun % n(c) = cel1_vol / (cell_vol)
       end if
 
     end if  ! cut_cel(c) .eq. YES
   end do
 
   ! Exchange is needed here
-  call Grid % Save_Debug_Vtu(append="vof_in_cells_final",  &
-                             scalar_cell=vof_in_cells,     &
-                             scalar_name="vof_in_cells")
+  call Grid % Save_Debug_Vtu(append="vof_final",         &
+                             scalar_cell=Vof % fun % n,  &
+                             scalar_name="vof_final")
 
-  call Work % Disconnect_Int_Node(dis_nod_cnt)
-  call Work % Disconnect_Real_Node(vof_at_nodes, dis_nod, dis_nod_dom)
-  call Work % Disconnect_Real_Cell(vof_in_cells)
+  call Work % Disconnect_Int_Cell(cut_cel)
+  call Work % Disconnect_Int_Cell(set_cel)
+  call Work % Disconnect_Int_Node(dis_nod_int)
+  call Work % Disconnect_Real_Node(dis_nod)
+  call Work % Disconnect_Real_Node(dis_nod_dom)
 
   call Comm_Mod_End
   stop
