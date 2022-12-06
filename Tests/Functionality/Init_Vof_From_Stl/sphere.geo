7974a6d5cc49281e3f86bcb8202c6ed46a27fdd3
Niceno <bojan.niceno.scientist@gmail.com>
2022-12-06 15:58:23 +0100

    Updated the test case.
    On branch boil_bojan
    
    deleted:    Functionality/Init_Vof_From_Stl/User_Mod/Beginning_Of_Simulation.f90
    modified:   Functionality/Init_Vof_From_Stl/control_cube_hex
    new file:   Functionality/Init_Vof_From_Stl/readme
    deleted:    Functionality/Init_Vof_From_Stl/sphere.geo
    deleted:    Functionality/Init_Vof_From_Stl/sphere.stl.gz
    new file:   Functionality/Init_Vof_From_Stl/spheres.py

--- Tests/Functionality/Init_Vof_From_Stl/sphere.geo
+++
@@ -1,3 +0,0 @@
-
-SetFactory("OpenCASCADE");
-Sphere(1) = {0.0, 0.0, 0.0, 0.4, -Pi/2, Pi/2, 2*Pi};
---
+++ Tests/Functionality/Init_Vof_From_Stl/spheres.py
@@ -0,0 +1,41 @@
+#===============================================================================
+#   This is to create eight spheres in Blender
+#
+#   You can run it interactivelly with:
+#   > blender --background --python ./spheres.py
+#-------------------------------------------------------------------------------
+
+import bpy
+
+# Delete initial cube in Blender
+bpy.ops.object.delete(use_global=False, confirm=False)
+
+# Set some parameters ...
+S  = 4
+P  = 0.2
+R1 = 0.19
+R2 = 0.17
+numb = [-1, 1]
+
+# ... and run!
+for i in numb:
+  for j in numb:
+    for k in numb:
+      x = i * P
+      y = j * P
+      z = k * P
+      if i * j * k > 0:
+        R = R1
+      else:
+        R = R2
+      bpy.ops.mesh.primitive_ico_sphere_add(subdivisions   = S,         \
+                                            radius         = R,         \
+                                            enter_editmode = False,     \
+                                            align          ='WORLD',    \
+                                            location       =(x, y, z),  \
+                                            scale          =(1, 1, 1))
+
+# Finally export what you got
+bpy.ops.export_mesh.stl(filepath='spheres.stl')
+
+
