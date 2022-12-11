0eaf41711c6d7368e9480200fd0f96c187304391
Niceno <bojan.niceno.scientist@gmail.com>
2022-12-11 06:59:22 +0100

    Added new member function to Stl_Mod.
    On branch boil_bojan
    
    modified:   Shared/Stl_Mod.f90
    new file:   Shared/Stl_Mod/Facet_Body.f90

--- Sources/Shared/Stl_Mod.f90
+++ Sources/Shared/Stl_Mod.f90
@@ -14,12 +14,14 @@
   !--------------!
   type, extends(Grid_Type) :: Stl_Type
     real,    allocatable :: nx(:), ny(:), nz(:)  ! facet normal
+    integer              :: n_boddies            ! number of boddies
     integer, allocatable :: body_c(:)            ! body at facets
     integer, allocatable :: body_n(:)            ! body at verticesl
 
     contains
       procedure, private :: Allocate_Stl
       procedure          :: Create_From_File
+      procedure          :: Facet_Body
       procedure          :: Facet_Coords
       procedure          :: Facet_Normal
       procedure          :: Facets_Vert_Coords
@@ -32,6 +34,7 @@
   contains
 #   include "Stl_Mod/Allocate_Stl.f90"
 #   include "Stl_Mod/Create_From_File.f90"
+#   include "Stl_Mod/Facet_Body.f90"
 #   include "Stl_Mod/Facet_Coords.f90"
 #   include "Stl_Mod/Facet_Normal.f90"
 #   include "Stl_Mod/Facets_Vert_Coords.f90"
