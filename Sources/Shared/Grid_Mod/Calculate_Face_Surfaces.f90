9a2267a7e034051c3b96fba311753c05d47ca417
Niceno <bojan.niceno.scientist@gmail.com>
2022-12-11 21:55:34 +0100

    Added an assertion for face directions
    On branch boil_bojan
    
    modified:   Shared/Grid_Mod/Calculate_Face_Surfaces.f90

--- Sources/Shared/Grid_Mod/Calculate_Face_Surfaces.f90
+++ Sources/Shared/Grid_Mod/Calculate_Face_Surfaces.f90
@@ -7,7 +7,8 @@
 !---------------------------------[Arguments]----------------------------------!
   class(Grid_Type) :: Grid
 !-----------------------------------[Locals]-----------------------------------!
-  integer           :: s, i_nod, j_nod, m, n
+  integer           :: c1, c2, s, i_nod, j_nod, m, n
+  real              :: dx, dy, dz
   real, allocatable :: xf(:), yf(:), zf(:)
 !==============================================================================!
 
@@ -32,6 +33,11 @@
     Grid % sx(s) = 0.0
     Grid % sy(s) = 0.0
     Grid % sz(s) = 0.0
+
+    ! I am afraid that this algorithm computes surfaces with wrong sign, but
+    ! it might work afterall since the nodes in "fn" structures (HEX, TET,
+    ! WED and PYR in Convert_Mod) also have nodes sorted in the direction
+    ! which points inwards a cell and not outwards
     do i_nod = 1, Grid % faces_n_nodes(s)
       j_nod = i_nod + 1
       if(j_nod > Grid % faces_n_nodes(s)) j_nod = 1
@@ -45,6 +51,18 @@
     Grid % sx(s) = 0.5 * Grid % sx(s)
     Grid % sy(s) = 0.5 * Grid % sy(s)
     Grid % sz(s) = 0.5 * Grid % sz(s)
+
+    ! Perform assertion for inside faces
+    ! (Boundary cells and faces are not
+    !  formed at this point yet anyhow.)
+    c1 = Grid % faces_c(1, s)
+    c2 = Grid % faces_c(2, s)
+    if(c2 .gt. 0) then
+      dx = grid % xc(c2) - grid % xc(c1)
+      dy = grid % yc(c2) - grid % yc(c1)
+      dz = grid % zc(c2) - grid % zc(c1)
+      Assert(Grid % sx(s) * dx + Grid % sy(s) * dy + Grid % sz(s) * dz > 0.0)
+    end if
 
   end do
 
