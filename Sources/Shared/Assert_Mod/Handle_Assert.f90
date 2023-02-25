96e5d756b9413ba339d1d0d7abcb74bd6e23d02e
Niceno <bojan.niceno.scientist@gmail.com>
2023-02-25 11:43:39 +0100

    VERY GOOD: Assert adapted for parallel runs
    On branch bojan_fixed_cell_ranges
    
    modified:   Shared/Assert_Mod/Handle_Assert.f90

--- Sources/Shared/Assert_Mod/Handle_Assert.f90
+++ Sources/Shared/Assert_Mod/Handle_Assert.f90
@@ -8,17 +8,35 @@
   character(*) :: file
   integer      :: line
 !-----------------------------------[Locals]-----------------------------------!
-  character(16) :: numb
+  character(16) :: numb, proc
 !==============================================================================!
 
-  if(fail) then
-    write(numb, '(i16)') line
-    print '(7a)', '  Assert(',         text,                 &
-                  ') failed in file ', file,                 &
-                  ' at line ',         trim(adjustl(numb)),  &
-                  '.'
-    call Comm_Mod_End
-    stop
+  ! Sequential run
+  if(n_proc < 2) then
+    if(fail) then
+      write(numb, '(i16)') line
+      print '(7a)', '  Assert(',         text,                 &
+                    ') failed in file ', file,                 &
+                    ' at line ',         trim(adjustl(numb)),  &
+                    '.'
+      call Comm_Mod_End
+      stop
+    end if
+
+  ! Parallel run
+  else
+    if(fail) then
+      write(numb, '(i16)') line
+      write(proc, '(i16)') this_proc
+      print '(9a)', '  Assert(',         text,                 &
+                    ') failed in file ', file,                 &
+                    ' at line ',         trim(adjustl(numb)),  &
+                    ' in processor ',    trim(adjustl(proc)),  &
+                    '.'
+      call Comm_Mod_End
+      stop
+    end if
+
   end if
 
   end subroutine
