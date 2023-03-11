b9acdfdcd568133bce13d1670529e013c8c405c1
Niceno <bojan.niceno.scientist@gmail.com>
2023-03-11 09:08:19 +0100

    Transfered improved gradients for pressure to here
    These are essentially in commits 217e85ddbacf2dd088e0714d5b570a3c420f6db8
    and dc5c39a55d28782856a554e0540f61cb2c945af2 in the branch
    bojan_weighted_gradients.  I didn't do a pull, but did a
    simple copying of the files here.
    
    On branch bojan_fixed_cell_ranges
    
    modified:   Sources/Process/Field_Mod/Gradients/Grad_Gauss_Pressure.f90
    modified:   Tests/Functionality/Gradients/User_Mod/Initialize_Variables.f90

--- Sources/Process/Field_Mod/Gradients/Grad_Gauss_Pressure.f90
+++ Sources/Process/Field_Mod/Gradients/Grad_Gauss_Pressure.f90
@@ -9,137 +9,105 @@
   type(Var_Type),    target :: p     ! should be pressure or pressure correction
 !----------------------------------[Locals]------------------------------------!
   type(Grid_Type),     pointer :: Grid
-  integer                      :: s, c, c1, c2
+  integer                      :: s, c, c1, c2, iter
   integer                      :: i_fac, c1_prim, c2_prim, s_prim
-  integer, contiguous, pointer :: c_at_bnd(:), c_cnt(:)
+  integer, contiguous, pointer :: c_computed(:), c_visited(:)
 !------------------------------[Local parameters]------------------------------!
   integer, parameter :: YES = 1
   integer, parameter :: NO  = YES-1
 !==============================================================================!
 
-  call Work % Connect_Int_Cell(c_at_bnd, c_cnt)
+  call Work % Connect_Int_Cell(c_computed, c_visited)
 
   ! Take alias
   Grid => Flow % pnt_grid
-
-  !--------------------------------------!
-  !   Initialization of c_at_bnd array   !
-  !--------------------------------------!
-  c_at_bnd(:) = NO
-  c_cnt(:)    = 0
-  do s = 1, Grid % n_faces
-    c1 = Grid % faces_c(1,s)
-    c2 = Grid % faces_c(2,s)
-    if(c2 < 0) c_at_bnd(c1) = YES
-  end do
 
   ! Initialize with some gradients with the most robust and reliable tool
   ! you have at your disposal - least square cell-based method.  These
   ! gradients should be properly calculated inside the domain.
   call Flow % Grad_Least_Pressure(p)
 
-  !--------------------------------------------------------------------!
-  !   Step 1: Extrapolate interior gradient values to boundary cells   !
-  !           using only values from interior cells - which are good   !
-  !           This step will leave the boundary cells surrounded by    !
-  !           other boundary cells unchanged (reset to zero, really)   !
-  !--------------------------------------------------------------------!
+  !-----------------------------------------------------------------------!
+  !   Extrapolate interior gradient values to boundary cells using only   !
+  !   values which are known, either from interior cells, or cells near   !
+  !   boundaries computed in previous iterations.                         !
+  !-----------------------------------------------------------------------!
+
+  ! First assume all are computed although you know the cells near the
+  ! bounderies are not computed well; but they will be treated below
+  c_computed(:) = YES
+
+  ! Mark cells which are not computed for the 1st time
+  ! At this point, these are cells near the boundaries
+  do s = 1, Grid % n_faces
+    c1 = Grid % faces_c(1, s)
+    c2 = Grid % faces_c(2, s)
+    if(c2 < 0) c_computed(c1) = NO
+  end do
+
+  call Grid % Exchange_Cells_Int(c_computed)
+
+  ! Nullify gradients for cells near boundaries
+  ! (where gradients are not computed properly),
+  ! and initialize c_visited
   do c = 1, Grid % n_cells
-
-    ! Cell is at the boundary, intervene here
-    if(c_at_bnd(c) .eq. YES) then
-
-      ! Nullify its gradients, and the counter for cells from which you interpolate
+    if(c_computed(c) .eq. NO) then  ! if not computed
       p % x(c) = 0.0
       p % y(c) = 0.0
       p % z(c) = 0.0
-
-      ! Browse through this cell's faces
-      do i_fac = 1, Grid % cells_n_faces(c)
-        s_prim  = Grid % cells_f(i_fac, c)
-        c1_prim = Grid % faces_c(1, s_prim)
-        c2_prim = Grid % faces_c(2, s_prim)
-
-        ! Consider c1_prim if it is not cell at boundary
-        if(c_at_bnd(c1_prim) .eq. NO) then
-          p % x(c) = p % x(c) + p % x(c1_prim)
-          p % y(c) = p % y(c) + p % y(c1_prim)
-          p % z(c) = p % z(c) + p % z(c1_prim)
-          c_cnt(c) = c_cnt(c) + 1
-        end if
-
-        ! Consider c2_prim if it is not cell at boundary
-        ! and not a boundary cell.
-        if(c_at_bnd(c2_prim) .eq. NO .and.  &
-           c2_prim .gt. 0) then
-          p % x(c) = p % x(c) + p % x(c2_prim)
-          p % y(c) = p % y(c) + p % y(c2_prim)
-          p % z(c) = p % z(c) + p % z(c2_prim)
-          c_cnt(c) = c_cnt(c) + 1
-        end if
-
-      end do
-
-      ! Work out the average
-      if(c_cnt(c) .gt. 0) then
-        p % x(c) = p % x(c) / c_cnt(c)
-        p % y(c) = p % y(c) / c_cnt(c)
-        p % z(c) = p % z(c) / c_cnt(c)
-      end if
-
-    end if  ! c at bnd
-
+      c_visited(c) = 0
+    end if
   end do
 
-  !-----------------------------------------------------------------------!
-  !   Step 2: Previous step left the cells at boundary, which are only    !
-  !           surrounded by other cells at boundary, untreated.  Here,    !
-  !           you are less selective and extrapolate even from cells at   !
-  !           boundaries, which were interpolated in the Step 1 above.    !
-  !-----------------------------------------------------------------------!
-  do c = 1, Grid % n_cells
+  !------------------------------------------------!
+  !   Extrapolate in several iterations, as long   !
+  !   as there are cells which are not computed.   !
+  !------------------------------------------------!
+  do iter = 1, 3
 
-    ! Cell is at the boundary, and hasn'p been treated yet
-    if(c_at_bnd(c) .eq. YES .and. c_cnt(c) .eq. 0) then
+    ! Browse through faces to find the cells which need ...
+    ! ... updating, and accumulate gradients in them as well
+    do s = 1, Grid % n_faces
+      c1 = Grid % faces_c(1, s)
+      c2 = Grid % faces_c(2, s)
 
-      ! Browse through this cell's faces
-      do i_fac = 1, Grid % cells_n_faces(c)
-        s_prim  = Grid % cells_f(i_fac, c)
-        c1_prim = Grid % faces_c(1, s_prim)
-        c2_prim = Grid % faces_c(2, s_prim)
-
-        if(c1_prim .ne. c  .and.  &     ! skip your own self
-           c_cnt(c1_prim) .gt. 0) then  ! consider only cells with values
-          p % x(c) = p % x(c) + p % x(c1_prim)
-          p % y(c) = p % y(c) + p % y(c1_prim)
-          p % z(c) = p % z(c) + p % z(c1_prim)
-          c_cnt(c) = c_cnt(c) + 1
+      if(c2 > 0) then
+        if(c_computed(c1) .eq. NO .and. c_computed(c2) .eq. YES) then
+          p % x(c1) = p % x(c1) + p % x(c2)
+          p % y(c1) = p % y(c1) + p % y(c2)
+          p % z(c1) = p % z(c1) + p % z(c2)
+          c_visited(c1) = c_visited(c1) + 1
         end if
 
-        if(c2_prim .ne. c  .and.  &     ! skip your own self
-           c2_prim .gt. 0  .and.  &     ! consider only cells with values
-           c_cnt(c2_prim) .gt. 0) then  ! don'p use boundary cells
-          p % x(c) = p % x(c) + p % x(c2_prim)
-          p % y(c) = p % y(c) + p % y(c2_prim)
-          p % z(c) = p % z(c) + p % z(c2_prim)
-          c_cnt(c) = c_cnt(c) + 1
+        if(c_computed(c2) .eq. NO .and. c_computed(c1) .eq. YES) then
+          p % x(c2) = p % x(c2) + p % x(c1)
+          p % y(c2) = p % y(c2) + p % y(c1)
+          p % z(c2) = p % z(c2) + p % z(c1)
+          c_visited(c2) = c_visited(c2) + 1
         end if
-
-      end do
-
-      if(c_cnt(c) .gt. 0) then
-        p % x(c) = p % x(c) / c_cnt(c)
-        p % y(c) = p % y(c) / c_cnt(c)
-        p % z(c) = p % z(c) / c_cnt(c)
       end if
+    end do
 
-    end if  ! c at bnd
+    ! Browse throough cells, and calculate final values ...
+    ! ... of gradients in the cells which have been visited
+    do c = 1, Grid % n_cells
+      if(c_visited(c) > 0) then
+        p % x(c) = p % x(c) / c_visited(c)
+        p % y(c) = p % y(c) / c_visited(c)
+        p % z(c) = p % z(c) / c_visited(c)
+        c_visited(c)  = 0
+        c_computed(c) = YES  ! mark it as computed for the next iteration
+      end if
+    end do
+    call Grid % Exchange_Cells_Real(p % x)
+    call Grid % Exchange_Cells_Real(p % y)
+    call Grid % Exchange_Cells_Real(p % z)
 
-  end do
+  end do    ! iter
 
   ! Perform Gauss from gradients which are good inside obtained above
   call Flow % Grad_Gauss_Variable(p)
 
-  call Work % Disconnect_Int_Cell(c_at_bnd, c_cnt)
+  call Work % Disconnect_Int_Cell(c_computed, c_visited)
 
   end subroutine
