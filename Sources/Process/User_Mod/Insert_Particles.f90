8339a68370ee964c52d246a67e35bb388c9eb7f9
Niceno <bojan.niceno.scientist@gmail.com>
2023-03-13 19:35:06 +0100

    Fixed output @ default user function for particles
    On branch bojan_fixed_cell_ranges
    
    modified:   User_Mod/Insert_Particles.f90

--- Sources/Process/User_Mod/Insert_Particles.f90
+++ Sources/Process/User_Mod/Insert_Particles.f90
@@ -20,15 +20,11 @@
   Grid => Flow % pnt_grid
 
   ! Remove the following six lines in real Lagrangian tracking simulations
-  if(this_proc < 2) then
-    print *, '#======================================================'     //  &
-             '======================================================='
-    print *, '# WARNING: You are running a Lagrangian tracking simulation' //  &
-             'with the default version of Insert_Particles.'
-    print *, '# You have probably forgotten to compile Process with '      //  &
-             'DIR_CASE=<full_or_relative_path_to_case> directive.'
-    print *, '#------------------------------------------------------'     //  &
-             '-------------------------------------------------------'
-  end if
+  call Message % Warning(72,                                             &
+             'You are running a Lagrangian tracking simulation  '    //  &
+             'with the default version of Insert_Particles. You '    //  &
+             'have probably forgotten to compile Process with '      //  &
+             'DIR_CASE=<full_or_relative_path_to_case> directive.',      &
+             file=__FILE__, line=__LINE__)
 
   end subroutine
