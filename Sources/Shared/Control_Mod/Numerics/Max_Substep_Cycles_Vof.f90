3795e28002b290770d71f90a43660c4be9a7d949
Niceno <bojan.niceno.scientist@gmail.com>
2023-03-16 01:19:15 +0100

    Insignificant: Corrected a case for variable.
    On branch bojan_fixed_cell_ranges
    
    modified:   Shared/Control_Mod/Numerics/Max_Substep_Cycles_Vof.f90

--- Sources/Shared/Control_Mod/Numerics/Max_Substep_Cycles_Vof.f90
+++ Sources/Shared/Control_Mod/Numerics/Max_Substep_Cycles_Vof.f90
@@ -7,7 +7,7 @@
   logical, optional    :: verbose
 !==============================================================================!
 
-  call Control_Mod_Read_int_Item('MAX_SUBSTEP_CYCLES_VOF',  &
+  call Control_Mod_Read_Int_Item('MAX_SUBSTEP_CYCLES_VOF',  &
                                    100, val, verbose)
 
   end subroutine
