bd8352160f00536c7b6db48d7f5f1941f226b240
Niceno <bojan.niceno.scientist@gmail.com>
2023-03-28 14:47:19 +0200

    IMPORTANT: Fixed the profiler!!!
    It seems that previously_running should be an array (for
    nested calls), not just a single integer number.
    
    On branch bojan_more_robust_communicator
    
    modified:   Shared/Profiler_Mod.f90
    modified:   Shared/Profiler_Mod/Start.f90
    modified:   Shared/Profiler_Mod/Stop.f90

--- Sources/Shared/Profiler_Mod/Start.f90
+++ Sources/Shared/Profiler_Mod/Start.f90
@@ -11,30 +11,30 @@
   integer :: i_fun  ! function counter
 !==============================================================================!
 
-  !-----------------------------------------------------!
-  !   Store the function which was previously running   !
-  !-----------------------------------------------------!
-  Profiler % previously_running = Profiler % currently_running
-
-  !----------------------------------------------!
-  !   If called for the first time, get system   !
-  !   clock clock rate and initial clock count   !
-  !----------------------------------------------!
-  if(Profiler % previously_running .eq. 0) then
+  !---------------------------------------------------------------------------!
+  !   If profiles is called for the first time, get system clock count rate   !
+  !---------------------------------------------------------------------------!
+  if(Profiler % n_functions .eq. 0) then
     call system_clock(count_rate = Profiler % sys_count_rate)
   end if
 
   !-------------------------------------------------------!
+  !                                                       !
   !   Determine the function which is currently running   !
+  !        (In other words, the caller function.)         !
+  !                                                       !
   !-------------------------------------------------------!
 
-  ! Check if this function was called before
+  !----------------------------------------------!
+  !   Check if this function was called before   !
+  !----------------------------------------------!
   do i_fun = 1, Profiler % n_functions
-    if(f_name .eq. Profiler % funct_name(i_fun)) then  ! found the function
-      Profiler % currently_running = i_fun
-      goto 1
-    end if
+    if(f_name .eq. Profiler % funct_name(i_fun)) goto 1
   end do
+
+  !-----------------------------------------------------------------!
+  !   If you happen to be here, the function wasn't called before   !
+  !-----------------------------------------------------------------!
 
   ! It wasn't called before, add it to the suite of analyzed function
   Profiler % n_functions = Profiler % n_functions + 1
@@ -43,14 +43,31 @@
   ! Initialize times spent in the new function
   Profiler % funct_time(Profiler % n_functions) = 0.0
 
-  ! Currently running function is the new one
-  Profiler % currently_running = Profiler % n_functions
+  ! Store currenlty running function to i_fun
+  i_fun = Profiler % n_functions
 
+  !--------------------------------------------------------------------------!
+  !                                                                          !
+  !   Meeting point: you are here no matter if function was called before,   !
+  !   or if it was called for the first timei.  Also important to note,      !
+  !   variable i_fun stores the currenly invoked function at this point.     !
+  !                                                                          !
+  !--------------------------------------------------------------------------!
 1 continue
+
+  !-----------------------------------------------------!
+  !   Store the function which was previously running   !
+  !-----------------------------------------------------!
+  Profiler % previously_running(i_fun) = Profiler % currently_running
 
   !-------------------------------------------------------------------!
   !   Update the timer in the function which was previously running   !
   !-------------------------------------------------------------------!
-  call Profiler % Update_By_Rank(Profiler % previously_running)
+  call Profiler % Update_By_Rank(Profiler % previously_running(i_fun))
+
+  !---------------------------------------!
+  !   Set the currentl running function   !
+  !---------------------------------------!
+  Profiler % currently_running = i_fun
 
   end subroutine
--- Sources/Shared/Profiler_Mod/Stop.f90
+++ Sources/Shared/Profiler_Mod/Stop.f90
@@ -12,23 +12,32 @@
 !==============================================================================!
 
   !----------------------------------------------------------!
+  !                                                          !
   !   Find the rank (number) of function which is stopping   !
+  !                                                          !
   !----------------------------------------------------------!
 
-  ! Browse through stored functions
+  !-------------------------------------!
+  !   Browse through stored functions   !
+  !-------------------------------------!
   do i_fun = 1, Profiler % n_functions
-    if(f_name .eq. Profiler % funct_name(i_fun)) then
-      goto 2
-    end if
+    if(f_name .eq. Profiler % funct_name(i_fun)) goto 2
   end do
 
-  ! If here, Profiler_Start wasn't invoked for this function
+  !----------------------------------------------!
+  !   If you happen to be here, Profiler_Start   !
+  !   wasn't invoked for this function           !
+  !----------------------------------------------!
   print *, 'CRITICAL ERROR in ''Profiler_End'':'
   print *, 'For function ''', trim(f_name), ''', ''Cpu_Ti' // &
            'mer_Start'' wasn''t invoked.  Exiting!'
   stop
 
-  ! Function has been found, continue
+  !------------------------------------------------------------------------!
+  !                                                                        !
+  !   If here, function has been found and variable i_fun holds its rank   !
+  !                                                                        !
+  !------------------------------------------------------------------------!
 2 continue
 
   !-------------------------------------------------------------!
@@ -39,6 +48,6 @@
   !-------------------------------------------------------!
   !   Restart the function which was previously running   !
   !-------------------------------------------------------!
-  Profiler % currently_running = Profiler % previously_running
+  Profiler % currently_running = Profiler % previously_running(i_fun)
 
   end subroutine
