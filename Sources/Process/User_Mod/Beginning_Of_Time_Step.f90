eda9627bac1b29d5eb1716ebdcfcb9ff384ca80e
Niceno <bojan.niceno.scientist@gmail.com>
2023-10-01 09:38:01 +0200

    Made it somewhat more useful than before.
    On branch bojan_sped_up_saving_big_time
    
    modified:   Sources/Process/User_Mod/Beginning_Of_Time_Step.f90

--- Sources/Process/User_Mod/Beginning_Of_Time_Step.f90
+++ Sources/Process/User_Mod/Beginning_Of_Time_Step.f90
@@ -9,22 +9,31 @@
   type(Turb_Type),  target :: Turb
   type(Vof_Type),   target :: Vof
   type(Swarm_Type), target :: Swarm
+!------------------------------[Local parameters]------------------------------!
+! real, parameter :: U_BULK = 1.0           ! bulk velocity
+! real, parameter :: U_MAX  = 1.5 * U_BULK  ! max velocity
+! real, parameter :: HALF_W = 2.05          ! half channel width
+! real, parameter :: TAU    = 1.0           ! characteristic time period
 !-----------------------------------[Locals]-----------------------------------!
-  type(Grid_Type), pointer :: Grid
-  type(Var_Type),  pointer :: u, v, w, t, phi
-  integer                  :: c
+! type(Grid_Type), pointer :: Grid
+! type(Var_Type),  pointer :: u, v, w, t, phi
+! integer                  :: reg, c2
+! real                     :: y
 !==============================================================================!
 
-  ! Take aliases
-  Grid => Flow % pnt_grid
-  t    => Flow % t
+! ! Take aliases
+! Grid => Flow % pnt_grid
+! u    => Flow % u
 
-! ! A way to implement boundary condition which varies in time
-! do c = -Grid % n_bnd_cells, -1
-!   if(Grid % Bnd_Cond_Name(c) .eq. 'HOT_WALL') then
-!     t % n(c) = min(1.0, time / 300.0)
-!   end if
-! end do
+! do reg = Boundary_Regions()
+!   if(Grid % region % type(reg) .eq. INFLOW) then
+!     do c2 = Cells_In_Region(reg)
+!       y = Grid % yc(c2)
+!       u % n(c2) = U_MAX * (1.0 - ( (y-HALF_W)/HALF_W )**2)  &
+!                 * (1.0 - exp(-Time % Get_Time() / TAU))
+!     end do  ! through boundary cells of the region
+!   end if    ! if at inflow region
+! end do      ! through all boundary regions
 
   end subroutine
 
