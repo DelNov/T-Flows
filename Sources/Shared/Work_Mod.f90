c89b8d222af2813db0f1970a6ee7b0488f42b073
Niceno <bojan.niceno.scientist@gmail.com>
2023-11-28 11:35:47 +0100

    Expanded FORD a little bit.
    On branch big_merge_in_november_2023_attempt_01
    
    modified:   Work_Mod.f90

--- Sources/Shared/Work_Mod.f90
+++ Sources/Shared/Work_Mod.f90
@@ -20,6 +20,8 @@
   !---------------------!
   !  Real poiner type   !
   !---------------------!
+  !> Simple container of a real, contiguous pointer.  It was introduced to
+  !> facilitate creation of variable size arrays containing pointers only.
   type Real_Pointer_Type
     real, pointer, contiguous :: ptr(:)
   end type
@@ -27,6 +29,8 @@
   !------------------------!
   !  Integer poiner type   !
   !------------------------!
+  !> Simple container of an integer contiguous pointer.  It was introduced to
+  !> facilitate creation of variable size arrays containing pointers only.
   type Int_Pointer_Type
     integer, pointer, contiguous :: ptr(:)
   end type
@@ -40,43 +44,43 @@
   type Work_Type
 
     ! Real working arrays
-    type(Real_Pointer_Type), allocatable :: r_cell(:)
-    type(Real_Pointer_Type), allocatable :: r_face(:)
-    type(Real_Pointer_Type), allocatable :: r_node(:)
+    type(Real_Pointer_Type), allocatable :: r_cell(:)  !! cell-based work array
+    type(Real_Pointer_Type), allocatable :: r_face(:)  !! face-based work array
+    type(Real_Pointer_Type), allocatable :: r_node(:)  !! node-based work array
 
     ! Integer working arrays
-    type(Int_Pointer_Type), allocatable :: i_cell(:)
-    type(Int_Pointer_Type), allocatable :: i_face(:)
-    type(Int_Pointer_Type), allocatable :: i_node(:)
+    type(Int_Pointer_Type), allocatable :: i_cell(:)  !! cell-based work array
+    type(Int_Pointer_Type), allocatable :: i_face(:)  !! face-based work array
+    type(Int_Pointer_Type), allocatable :: i_node(:)  !! node-based work array
 
     ! Floating "pointers" for all arrays
-    integer, private :: last_r_cell
-    integer, private :: last_r_face
-    integer, private :: last_r_node
+    integer, private :: last_r_cell  !! pointer to last used real cell-array
+    integer, private :: last_r_face  !! pointer to last used real face-array
+    integer, private :: last_r_node  !! pointer to last used real node-array
 
-    integer, private :: last_i_cell
-    integer, private :: last_i_face
-    integer, private :: last_i_node
+    integer, private :: last_i_cell  !! pointer to last used integer cell-array
+    integer, private :: last_i_face  !! pointer to last used integer face-array
+    integer, private :: last_i_node  !! pointer to last used integer node-array
 
     ! Maximum number of used arrays
 #ifdef CHECK_USAGE
-    integer, private :: max_r_cell = 0
-    integer, private :: max_r_face = 0
-    integer, private :: max_r_node = 0
+    integer, private :: max_r_cell = 0  !! number of real cell-arrays used
+    integer, private :: max_r_face = 0  !! number of real face-arrays used
+    integer, private :: max_r_node = 0  !! number of real node-arrays used
 
-    integer, private :: max_i_cell = 0
-    integer, private :: max_i_face = 0
-    integer, private :: max_i_node = 0
+    integer, private :: max_i_cell = 0  !! number of integer cell-arrays used
+    integer, private :: max_i_face = 0  !! number of integer face-arrays used
+    integer, private :: max_i_node = 0  !! number of integer node-arrays used
 #endif
 
     ! Requested number of used arrays
-    integer, private :: req_r_cell = 0
-    integer, private :: req_r_face = 0
-    integer, private :: req_r_node = 0
+    integer, private :: req_r_cell = 0  !! number of requested real cell-arrays
+    integer, private :: req_r_face = 0  !! number of requested real face-arrays
+    integer, private :: req_r_node = 0  !! number of requested real node-arrays
 
-    integer, private :: req_i_cell = 0
-    integer, private :: req_i_face = 0
-    integer, private :: req_i_node = 0
+    integer, private :: req_i_cell = 0  !! number of requested int. cell-arrays
+    integer, private :: req_i_face = 0  !! number of requested int. face-arrays
+    integer, private :: req_i_node = 0  !! number of requested int. node-arrays
 
     logical :: allocated = .false.
 
@@ -105,7 +109,8 @@
   end type
 
   ! Singleton object Work
-  type(Work_Type) :: Work
+  type(Work_Type) :: Work  !! singleton object Work introduced for seamless
+    !! synchronization of local data and easy access to public member function
 
   contains
 
