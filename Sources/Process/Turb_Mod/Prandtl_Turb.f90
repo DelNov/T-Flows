5e1a94ef80cfe26da275f39ec2e05d0cae8659ce
Niceno <bojan.niceno.scientist@gmail.com>
2023-12-12 08:43:18 +0100

    Sets turbulent Pr to 0.4 in case of LES.
    On branch big_merge_in_november_2023_attempt_02
    
    modified:   Process/Turb_Mod/Prandtl_Turb.f90

--- Sources/Process/Turb_Mod/Prandtl_Turb.f90
+++ Sources/Process/Turb_Mod/Prandtl_Turb.f90
@@ -2,29 +2,36 @@
   real function Prandtl_Turb(Turb, c)
 !------------------------------------------------------------------------------!
 !   Computes turbulent Prandtl number according to Kays, W. M. and             !
-!   Crawford, M. E., Convective Heat and Mass Transfer,                        !
-!   3rd edn. McGraw-Hill, New York, 1993.                                      !
+!   Crawford, M. E., Convective Heat and Mass Transfer, 3rd ed. McGraw-Hill,   !
+!   New York, 1993 if it is not an LES, otherwise return simply 0.4.           !
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   class(Turb_Type), target :: Turb
-  integer                  :: c
-  real                     :: pr, pr_t_inf, pe_t
+  integer,      intent(in) :: c
 !-----------------------------------[Locals]-----------------------------------!
   type(Field_Type), pointer :: Flow
+  real                      :: pr, pr_t_inf, pe_t
 !==============================================================================!
 
   ! Take alias to the Flow to access its properties
   Flow => Turb % pnt_flow
 
-  pr = Flow % Prandtl_Numb(c)
-  pr_t_inf = 0.85
-  pe_t     = max(pr * Turb % vis_t(c) / Flow % viscosity(c), TINY)
+  ! In case an LES model is used, set it simply to 0.4
+  if(Turb % Les()) then
+    Prandtl_Turb = 0.4
 
-  Prandtl_Turb =                                                        &
-    1.0 / (   1.0/(2.0*pr_t_inf)                                        &
-           +  0.3*pe_t * sqrt(1.0/pr_t_inf)                             &
-           - (0.3*pe_t)**2 * (1.0-exp(-1.0/(0.3*pe_t*sqrt(pr_t_inf))))  &
-          )
+  ! Otherwise, use the correlation proposed by Kays and Crawford
+  else
+    pr = Flow % Prandtl_Numb(c)
+    pr_t_inf = 0.85
+    pe_t     = max(pr * Turb % vis_t(c) / Flow % viscosity(c), TINY)
+
+    Prandtl_Turb =                                                        &
+      1.0 / (   1.0/(2.0*pr_t_inf)                                        &
+             +  0.3*pe_t * sqrt(1.0/pr_t_inf)                             &
+             - (0.3*pe_t)**2 * (1.0-exp(-1.0/(0.3*pe_t*sqrt(pr_t_inf))))  &
+            )
+  end if
 
   end function
