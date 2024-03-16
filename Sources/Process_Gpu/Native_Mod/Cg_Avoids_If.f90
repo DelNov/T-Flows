6a3a0009ad57356f08bbac57ef15199a82d4b99d
Niceno <bojan.niceno.scientist@gmail.com>
2024-03-16 16:39:48 +0100

    On the path to MPI parallel verion: Native
    To port the native solvers to MPI, it was important to
    set the bounds in calls to Linalg's functions:
    
    - Mat_X_Vec:       bounds are set to 1:nt
    - Vec_Copy:        bounds are set to 1:ni
    - Vec_D_Vec:       bounds are set to 1:ni
    - Vec_P_Sca_X_Vec: bounds are set to 1:ni
    
    where nt is "total" number of cells, meaning cells inside plus
    the buffer cells, whereas ni is number of cells "inside", no
    buffer cells included.
    
    Keep in mind that Mat_X_Vec refreshes buffers before starting
    the accelerated operation, whereas Vec_D_Vec performs a global
    sum after the atomic operation on the device.
    
    On branch bojan_gpu_development
    
    modified:   Sources/Process_Gpu/Native_Mod.f90
    modified:   Sources/Process_Gpu/Native_Mod/Cg.f90
    modified:   Sources/Process_Gpu/Native_Mod/Cg_Avoids_If.f90
    modified:   Sources/Process_Gpu/Native_Mod/Cg_With_If.f90

--- Sources/Process_Gpu/Native_Mod/Cg_Avoids_If.f90
+++ Sources/Process_Gpu/Native_Mod/Cg_Avoids_If.f90
@@ -12,18 +12,18 @@
   class(Native_Type),    target, intent(inout) :: Nat      !! parent class
   type(Sparse_Con_Type), target, intent(in)    :: Acon     !! connect matrix
   type(Sparse_Val_Type), target, intent(in)    :: Aval     !! values matrix
-  real,                       intent(out)   :: x(-Nat % pnt_grid % n_bnd_cells:&
-                                                  Nat % pnt_grid % n_cells)
-    !! unknown vector, the solution of the linear system
-  real,                          intent(inout) :: b( Nat % pnt_grid % n_cells)
-    !! right-hand side vector
-  integer,                       intent(in)    :: miter    !! max iterations
-  real,                          intent(in)    :: tol
+  real, intent(out)   :: x(-Nat % pnt_grid % n_bnd_cells:&
+                            Nat % pnt_grid % n_cells)
+                         !! unknown vector, the solution of the linear system
+  real, intent(inout) :: b( Nat % pnt_grid % n_cells)
+                         !! right-hand side vector
+  integer, intent(in)    :: miter    !! maximum iterations
+  real,    intent(in)    :: tol      !! target solver tolerance
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type),   pointer :: Grid
   real, contiguous,  pointer :: r(:), p(:), q(:), d_inv(:)
   real                       :: alpha, beta, pq, rho, rho_old, res
-  integer                    :: nc, iter
+  integer                    :: nt, ni, iter
 !==============================================================================!
 
   ! Take aliases
@@ -32,32 +32,33 @@
   q     => Nat % q
   r     => Nat % r
   Grid  => Nat % pnt_grid
-  nc    =  Grid % n_cells
+  nt    =  Grid % n_cells
+  ni    =  Grid % n_cells - Grid % Comm % n_buff_cells
 
   !----------------!
   !   r = b - Ax   !     =-->  (q used for temporary storing Ax)
   !----------------!
-  call Linalg % Mat_X_Vec(nc, q, Acon, Aval, x(1:nc))  ! Ax = A * x
-  call Linalg % Vec_P_Sca_X_Vec(nc, r, b, -1.0, q)     ! r  = b - Ax
+  call Linalg % Mat_X_Vec(nt, q, Acon, Aval, x(1:nt))  ! q = A * x
+  call Linalg % Vec_P_Sca_X_Vec(ni, r(1:ni), b(1:ni), -1.0, q(1:ni))
 
   ! Check first residual
-  call Linalg % Vec_D_Vec(nc, res, r, r)  ! res = r * r
+  call Linalg % Vec_D_Vec(ni, res, r(1:ni), r(1:ni))  ! res = r * r
   if(res < tol) return
 
   !---------------!
   !   z = r \ M   !    =--> (q used for z)
   !---------------!
-  call Linalg % Vec_X_Vec(nc, q, r, d_inv)
+  call Linalg % Vec_X_Vec(ni, q(1:ni), r(1:ni), d_inv(1:ni))
 
   !-----------------!
   !   rho = r * z   !  =--> (q used for z)
   !-----------------!
-  call Linalg % Vec_D_Vec(nc, rho, r, q)  ! rho = r * q
+  call Linalg % Vec_D_Vec(ni, rho, r(1:ni), q(1:ni))  ! rho = r * q
 
   !-----------!
   !   p = z   !  =--> (q used for z)
   !-----------!
-  call Linalg % Vec_Copy(nc, p, q)
+  call Linalg % Vec_Copy(ni, p(1:ni), q(1:ni))
 
   !--------------------------!
   !                          !
@@ -69,45 +70,47 @@
     !------------!
     !   q = Ap   !
     !------------!
-    call Linalg % Mat_X_Vec(nc, q, Acon, Aval, p)   ! q  = A * p
+    call Linalg % Mat_X_Vec(nt, q(1:nt), Acon, Aval, p(1:nt))
 
     !---------------------------!
     !   alfa =  rho / (p * q)   !
     !---------------------------!
-    call Linalg % Vec_D_Vec(nc, pq, p, q)  ! pq = p * q
+    call Linalg % Vec_D_Vec(ni, pq, p(1:ni), q(1:ni))  ! pq = p * q
     alpha = rho / pq
 
     !---------------------!
     !   x = x + alfa p    !
     !   r = r - alfa q    !
     !---------------------!
-    call Linalg % Vec_P_Sca_X_Vec(nc, x(1:nc), x(1:nc), +alpha, p)  ! x = x + alpha p
-    call Linalg % Vec_P_Sca_X_Vec(nc, r,       r,       -alpha, q)  ! r = r - alpha q
+    call Linalg % Vec_P_Sca_X_Vec(ni, x(1:ni), x(1:ni), +alpha, p(1:ni))
+    call Linalg % Vec_P_Sca_X_Vec(ni, r(1:ni), r(1:ni), -alpha, q(1:ni))
 
     !---------------!
     !   z = r \ M   !    =--> (q used for z)
     !---------------!
-    call Linalg % Vec_X_Vec(nc, q, r, d_inv)
+    call Linalg % Vec_X_Vec(ni, q(1:ni), r(1:ni), d_inv(1:ni))
 
     !-----------------!
     !   rho = r * z   !  =--> (q used for z)
     !-----------------!
     rho_old = rho
-    call Linalg % Vec_D_Vec(nc, rho, r, q)  ! rho = r * q
+    call Linalg % Vec_D_Vec(ni, rho, r(1:ni), q(1:ni))
 
     !--------------------------!
     !   beta = rho / rho_old   !
     !   p = z + beta * p       !  =--> (q used for p)
     !--------------------------!
     beta = rho / rho_old
-    call Linalg % Vec_P_Sca_X_Vec(nc, p, q, beta, p)   ! p = q + beta p
+    call Linalg % Vec_P_Sca_X_Vec(ni, p(1:ni), q(1:ni), beta, p(1:ni))
 
     !--------------------!
     !   Check residual   !
     !--------------------!
-    call Linalg % Vec_D_Vec(nc, res, r, r)  ! res = r * r
+    call Linalg % Vec_D_Vec(ni, res, r(1:ni), r(1:ni))
 
-    if(mod(iter,32) .eq. 0) print '(a,i12,es12.3)', ' iter, res = ', iter, res
+    if(mod(iter,32) .eq. 0) then
+      O_Print '(a,i12,es12.3)', ' iter, res = ', iter, res
+    end if
     if(res .lt. tol) goto 1
 
     rho_old = rho
@@ -120,11 +123,11 @@
   !                       !
   !-----------------------!
 1 continue
-  print '(a,i12,es12.3)', ' iter, res = ', iter, res
+  O_Print '(a,i12,es12.3)', ' iter, res = ', iter, res
 
   !-------------------------------------------!
   !   Refresh the solution vector's buffers   !
   !-------------------------------------------!
-  call Grid % Exchange_Inside_Cells_Real(x(1:nc))
+  call Grid % Exchange_Inside_Cells_Real(x(1:nt))
 
   end subroutine
--- Sources/Process_Gpu/Native_Mod/Cg_With_If.f90
+++ Sources/Process_Gpu/Native_Mod/Cg_With_If.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Cg(Nat, A, x, b, miter, tol)
+  subroutine Cg(Nat, Acon, Aval, x, b, miter, tol)
 !------------------------------------------------------------------------------!
 !   Note: This algorithm is based on: "Templates for the Solution of Linear    !
 !         Systems: Building Blocks for Iterative Methods", available for       !
@@ -7,44 +7,46 @@
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  class(Native_Type), target, intent(inout) :: Nat      !! parent class
-  type(Sparse_Type),  target, intent(in)    :: A        !! system matrix
-  real,                       intent(out)   :: x(-Nat % pnt_grid % n_bnd_cells:&
-                                                  Nat % pnt_grid % n_cells)
-    !! unknown vector, the solution of the linear system
-  real,                       intent(inout) :: b( Nat % pnt_grid % n_cells)
-    !! right-hand side vector
-  integer,                    intent(in)    :: miter    !! maximum iterations
-  real,                       intent(in)    :: tol
+  class(Native_Type),    target, intent(inout) :: Nat      !! parent class
+  type(Sparse_Con_Type), target, intent(in)    :: Acon     !! connect matrix
+  type(Sparse_Val_Type), target, intent(in)    :: Aval     !! values matrix
+  real, intent(out)   :: x(-Nat % pnt_grid % n_bnd_cells:&
+                            Nat % pnt_grid % n_cells)
+                         !! unknown vector, the solution of the linear system
+  real, intent(inout) :: b( Nat % pnt_grid % n_cells)
+                         !! right-hand side vector
+  integer, intent(in)    :: miter    !! maximum iterations
+  real,    intent(in)    :: tol      !! target solver tolerance
 !-----------------------------------[Locals]-----------------------------------!
   type(Grid_Type),   pointer :: Grid
   real, contiguous,  pointer :: r(:), p(:), q(:), d_inv(:)
   real                       :: alpha, beta, pq, rho, rho_old, res
-  integer                    :: nc, iter
+  integer                    :: nt, ni, iter
 !==============================================================================!
 
   ! Take aliases
-  d_inv => A % d_inv
+  d_inv => Aval % d_inv
   p     => Nat % p
   q     => Nat % q
   r     => Nat % r
   Grid  => Nat % pnt_grid
-  nc    =  Grid % n_cells
+  nt    =  Grid % n_cells
+  ni    =  Grid % n_cells - Grid % Comm % n_buff_cells
 
   !----------------!
   !   r = b - Ax   !     =-->  (q used for temporary storing Ax)
   !----------------!
-  call Linalg % Mat_X_Vec(nc, q, Acon, Aval, x(1:nc))  ! Ax = A * x
-  call Linalg % Vec_P_Sca_X_Vec(nc, r, b, -1.0, q)     ! r  = b - Ax
+  call Linalg % Mat_X_Vec(nt, q, Acon, Aval, x(1:nt))  ! q = A * x
+  call Linalg % Vec_P_Sca_X_Vec(ni, r(1:ni), b(1:ni), -1.0, q(1:ni))
 
   ! Check first residual
-  call Linalg % Vec_D_Vec(nc, res, r, r)  ! res = r * r
+  call Linalg % Vec_D_Vec(ni, res, r(1:ni), r(1:ni))  ! res = r * r
   if(res < tol) return
 
   !-----------!
   !   p = r   !
   !-----------!
-  call Linalg % Vec_Copy(nc, p, r)
+  call Linalg % Vec_Copy(ni, p(1:ni), r(1:ni))
 
   !--------------------------!
   !                          !
@@ -56,19 +58,19 @@
     !---------------!
     !   z = r \ M   !    =--> (q used for z)
     !---------------!
-    call Linalg % Vec_X_Vec(nc, q, r, d_inv)
+    call Linalg % Vec_X_Vec(ni, q(1:ni), r(1:ni), d_inv(1:ni))
 
     !-----------------!
     !   rho = r * z   !  =--> (q used for z)
     !-----------------!
-    call Linalg % Vec_D_Vec(nc, rho, r, q)  ! rho = r * q
+    call Linalg % Vec_D_Vec(ni, rho, r(1:ni), q(1:ni))  ! rho = r * q
 
     if(iter .eq. 1) then
 
       !-----------!
       !   p = z   !  =--> (q used for z)
       !-----------!
-      call Linalg % Vec_Copy(nc, p, q)  ! p = q
+      call Linalg % Vec_Copy(ni, p(1:ni), q(1:ni))  ! p = q
     else
 
       !--------------------------!
@@ -76,33 +78,35 @@
       !   p = z + beta * p       !  =--> (q used for p)
       !--------------------------!
       beta = rho / rho_old
-      call Linalg % Vec_P_Sca_X_Vec(nc, p, q, beta, p)   ! p = q + beta p
+      call Linalg % Vec_P_Sca_X_Vec(ni, p(1:ni), q(1:ni), beta, p(1:ni))
     end if
 
     !------------!
     !   q = Ap   !
     !------------!
-    call Linalg % Mat_X_Vec(nc, q, Acon, Aval, p)   ! q  = A * p
+    call Linalg % Mat_X_Vec(nt, q(1:nt), Acon, Aval, p(1:nt))
 
     !---------------------------!
     !   alfa =  rho / (p * q)   !
     !---------------------------!
-    call Linalg % Vec_D_Vec(nc, pq, p, q)  ! pq = p * q
+    call Linalg % Vec_D_Vec(ni, pq, p(1:ni), q(1:ni))  ! pq = p * q
     alpha = rho / pq
 
     !---------------------!
     !   x = x + alfa p    !
     !   r = r - alfa q    !
     !---------------------!
-    call Linalg % Vec_P_Sca_X_Vec(nc, x(1:nc), x(1:nc), +alpha, p)  ! x = x + alpha p
-    call Linalg % Vec_P_Sca_X_Vec(nc, r,       r,       -alpha, q)  ! r = r - alpha q
+    call Linalg % Vec_P_Sca_X_Vec(ni, x(1:ni), x(1:ni), +alpha, p(1:ni))
+    call Linalg % Vec_P_Sca_X_Vec(ni, r(1:ni), r(1:ni), -alpha, q(1:ni))
 
     !--------------------!
     !   Check residual   !
     !--------------------!
-    call Linalg % Vec_D_Vec(nc, res, r, r)  ! res = r * r
+    call Linalg % Vec_D_Vec(ni, res, r(1:ni), r(1:ni))
 
-    if(mod(iter,32) .eq. 0) print '(a,i12,es12.3)', ' iter, res = ', iter, res
+    if(mod(iter,32) .eq. 0) then
+      O_Print '(a,i12,es12.3)', ' iter, res = ', iter, res
+    end if
     if(res .lt. tol) goto 1
 
     rho_old = rho
@@ -120,6 +124,6 @@
   !-------------------------------------------!
   !   Refresh the solution vector's buffers   !
   !-------------------------------------------!
-  call Grid % Exchange_Inside_Cells_Real(x(1:nc))
+  call Grid % Exchange_Inside_Cells_Real(x(1:nt))
 
   end subroutine
