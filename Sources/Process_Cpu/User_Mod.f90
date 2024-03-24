77b02ec46acacc04cc5bc6c21b97b98fa8aabf84
Niceno <bojan.niceno.scientist@gmail.com>
2024-03-24 15:51:37 +0100

    Nothing, updated comments a bit.
    On branch bojan_gpu_development
    
    modified:   Sources/Process_Cpu/Face_Mod.f90
    modified:   Sources/Process_Cpu/User_Mod.f90

--- Sources/Process_Cpu/User_Mod.f90
+++ Sources/Process_Cpu/User_Mod.f90
@@ -7,6 +7,7 @@
 !   This is embrio of a future User module, a place where user can
 !   define his/her variables and pass them around his functions
 !------------------------------------------------------------------------------!
+!----------------------------------[Modules]-----------------------------------!
   use Porosity_Mod
   use Swarm_Mod
   use Interface_Mod
