b1817f1efd0938d91a792a84f5acf8c1ecf47240
Niceno <bojan.niceno.scientist@gmail.com>
2024-11-29 13:49:30 +0100

    Removed some debugging info and print statements.
    On branch bojan_gpu_development_SA
    
    modified:   Turb_Mod/Src_Vis_Spalart_Allmaras.f90

--- Sources/Process/Gpu/Turb_Mod/Src_Vis_Spalart_Allmaras.f90
+++ Sources/Process/Gpu/Turb_Mod/Src_Vis_Spalart_Allmaras.f90
@@ -18,9 +18,6 @@
   integer                      :: c
   real                         :: x_rat, f_v1, f_v2, f_w, ss
   real                         :: dist_v, prod_v, r, gg, dif, dist
-# if T_FLOWS_DEBUG == 1                                                          
-    real, allocatable :: temp(:)                                                 
-# endif 
 !==============================================================================!
 
   !------------------------------------------------------------!
@@ -43,7 +40,6 @@
                                vector_cell = (/vis_x, vis_y, vis_z/))
 # endif
 
-
   if(Turb % model .eq. SPALART_ALLMARAS .or.  &
      Turb % model .eq. DES_SPALART) then
 
@@ -65,7 +61,6 @@
              + Turb % vis % n(c) * f_v2 / (Turb % kappa**2 * dist**2)
       prod_v = Turb % c_b1 * Flow % density(c) * ss * Turb % vis % n(c)
       b(c)   = b(c) + prod_v * Grid % vol(c)
-! debug PRINT *, __FILE__, __LINE__, prod_v
 
       !----------------------------------!
       !   Compute the destruction term   !
@@ -87,48 +82,8 @@
             / Turb % vis % sigma
       b(c)  = b(c) + dif * Grid % vol(c)
 
-      if(b(c) < 0.0) then
-        PRINT *, 'b(c) = ', b(c)
-      end if
-      ! Assert(b(c) .ge. 0)
-
     end do
 
-# if T_FLOWS_DEBUG == 1                                                          
-    allocate(temp(Grid % n_cells));  temp(:) = 0.0                               
-    do c = Cells_In_Domain()  ! this is for debugging, don't do it on GPU        
-      dist = Grid % wall_dist(c)                                                 
-      x_rat  = Turb % vis % n(c) / (Flow % viscosity(c) / Flow % density(c))        
-      f_v1   = x_rat**3 / (x_rat**3 + Turb % c_v1**3)                            
-      f_v2   = 1.0 - x_rat/(1.0 + x_rat*f_v1)                                    
-      ss     = Flow % vort(c)   &                                                
-             + Turb % vis % n(c) * f_v2 / (Turb % kappa**2 * dist**2)            
-      prod_v = Turb % c_b1 * Flow % density(c) * ss * Turb % vis % n(c)          
-                                                                                 
-      !----------------------------------!
-      !   Compute the destruction term   !
-      !----------------------------------!
-      r      = Turb % vis % n(c) / (ss * Turb % kappa**2 * dist**2)
-      gg     = r + Turb % c_w2*(r**6 - r)
-      f_w    = gg*((1.0 + Turb % c_w3**6)  &
-             / (gg**6 + Turb % c_w3**6))**ONE_SIXTH
-      dist_v = Turb % c_w1 * Flow % density(c) * f_w  &
-             * (Turb % vis % n(c) / dist**2)
-      !--------------------------------------------!
-      !   Compute the first-order diffusion term   !
-      !--------------------------------------------!
-      dif   = Turb % c_b2                                &
-            * Flow % density(c)                          &
-            * (vis_x(c)**2 + vis_y(c)**2 + vis_z(c)**2)  &
-            / Turb % vis % sigma
-      temp(c) = Flow % density(c) * f_v1 * Turb % vis % n(c)
-      if(prod_v < 0.0) write(*,*) dist, x_rat, f_v1, f_v2, ss                    
-    end do                                                                       
-    call Grid % Exchange_Inside_Cells_Real(temp)                                 
-    call Grid % Save_Debug_Vtu("Prod",              &                            
-                               inside_name = "Prod",  &                          
-                               inside_cell = temp)                               
-# endif                                
   end if
 
   end subroutine
