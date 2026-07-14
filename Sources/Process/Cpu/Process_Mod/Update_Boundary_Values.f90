7ffd3df0413683cebceb174064b17d0a63292d43
mhadziabdic <mhadziabdic@ius.edu.ba>
2026-07-14 12:30:06 +0200

    Remove remaining merge conflict markers

--- Sources/Process/Cpu/Process_Mod/Update_Boundary_Values.f90
+++ Sources/Process/Cpu/Process_Mod/Update_Boundary_Values.f90
@@ -271,16 +271,6 @@
 
         ! Wall temperature or heat fluxes for k-eps-zeta-f
         ! and high-re k-eps models.
-<<<<<<< HEAD
-        if(Turb % model .eq. K_EPS_ZETA_F    .or.  &
-           Turb % model .eq. HYBRID_LES_RANS .or.  &
-           Turb % model .eq. LES_DYNAMIC     .or.  &
-           Turb % model .eq. LES_WALE        .or.  &
-           Turb % model .eq. LES_SMAGORINSKY .or.  &
-           Turb % model .eq. SPALART_ALLMARAS.or.  &
-           Turb % model .eq. DES_SPALART     .or.  &
-           Turb % model .eq. K_OMEGA_SST     .or.  &
-=======
         if(Turb % model .eq. K_EPS_ZETA_F     .or.  &
            Turb % model .eq. HYBRID_LES_RANS  .or.  &
            Turb % model .eq. LES_DYNAMIC      .or.  &
@@ -289,20 +279,13 @@
            Turb % model .eq. SPALART_ALLMARAS .or.  &
            Turb % model .eq. DES_SPALART      .or.  &
            Turb % model .eq. K_OMEGA_SST      .or.  &
->>>>>>> origin/bojan_inserts_boundary_layers
            Turb % model .eq. K_EPS) then
           if(Var_Mod_Bnd_Cond_Type(t,c2) .eq. WALLFL) then
             t % n(c2) = t % n(c1) + t % q(c2) * Grid % wall_dist(c1)  &
                       / (Turb % con_w(c1) + TINY)
-<<<<<<< HEAD
-            if(Time % Curr_Dt() .lt. 100) then
-              t % n(c2) = t % n(c1) + t % q(c2) * Grid % wall_dist(c1)  &
-                        / Flow % conductivity(c1)
-=======
             if(Time % Curr_Dt() < BEGIN) then
               t % n(c2) = t % n(c1) + t % q(c2) * Grid % wall_dist(c1)  &
                         / (Flow % conductivity(c1))
->>>>>>> origin/bojan_inserts_boundary_layers
             end if
           else if(Var_Mod_Bnd_Cond_Type(t,c2) .eq. WALL) then
             t % q(c2) = ( t % n(c2) - t % n(c1) ) * Turb % con_w(c1)  &
