f39cd0f9894090cf487ff3bbc9ed49cf1e407d17
mhadziabdic <mhadziabdic@ius.edu.ba>
2026-09-15 12:40:20 +0200

    Two bugs are fixed, definition of y+ and buoyancy term sign.

--- Sources/Process/Cpu/Turb_Mod/Vis_T_Subgrid.f90
+++ Sources/Process/Cpu/Turb_Mod/Vis_T_Subgrid.f90
@@ -50,7 +50,7 @@
       ! Calculate u_tau, y+ and perform Van Driest damping
       u_tau = (u_tan/A_POW * (nu/Grid % wall_dist(c))**B_POW)   &
               ** (1.0/(1.0+B_POW))
-      Turb % y_plus(c) = Grid % wall_dist(c) * u_tau / Flow % viscosity(c)
+      Turb % y_plus(c) = Grid % wall_dist(c) * u_tau / nu 
       cs = Turb % c_smag * (1.0 - exp(-Turb % y_plus(c) / 25.0))
 
       Turb % vis_t(c) = Flow % density(c)  &
@@ -84,7 +84,7 @@
   !-------------------------------------------------!
   if(Flow % buoyancy .eq. THERMALLY_DRIVEN) then
     do c = Cells_In_Domain_And_Buffers()
-      nc2 = max(- Flow % beta * (  Flow % grav_x * t % x(c)   &
+      nc2 = max(  Flow % beta * (  Flow % grav_x * t % x(c)   &
                                  + Flow % grav_y * t % y(c)   &
                                  + Flow % grav_z * t % z(c)), 0.0)
       Turb % vis_t(c) = Turb % vis_t(c)            &
